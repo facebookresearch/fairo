b775b429428b66e7a24ccd43e8902567ff144569
Yuxuan Sun <yuxuans@fb.com>
2021-01-27 04:12:50 -0800

    Set up base agent unittest CI

---
+++ base_agent/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+
+
+cd $(dirname $0)
+coverage run --source . -m unittest discover
+status=$?
+coverage report
+coverage xml -o /shared/test_base_agent.xml
+exit $status
