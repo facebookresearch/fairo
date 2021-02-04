b9111727080e2c6d604792a7ac102844a87fe4d3
Yuxuan Sun <yuxuans@fb.com>
2021-02-04 05:46:24 -0800

    Fixed pointing feature in mc

--- craftassist/agent/dialogue_objects/point_target.py
+++ craftassist/agent/dialogue_objects/point_target.py
@@ -9,14 +9,9 @@ class PointTargetInterpreter:
         # TODO: We might want to specifically check for BETWEEN/INSIDE, I'm not sure
         # what the +1s are in the return value
         mems = interpreter.subinterpret["reference_locations"](interpreter, speaker, d["location"])
-        #    mems = interpret_reference_location(interpreter, speaker, d["location"])
-        steps, reldir = interpret_relative_direction(interpreter.agent, d)
-        loc, _ = interpreter.subinterpret["specify_locations"](
-            interpreter, speaker, mems, steps, reldir
-        )
-        #    loc, _ = compute_locations(interpreter, speaker, mems, steps, reldir)
-        return self.point_to_region(loc)
+        return self.point_to_region(mems[0].get_point_at_target())
 
-    def point_to_region(self, loc):
-        assert len(loc) == 3, "point_to_region expects a triple"
-        return (loc[0], loc[1]+1, loc[2], loc[0], loc[1]+1, loc[2])
+    def point_to_region(self, pointed_target):
+        # mc pointed target is always a region represented as [xmin, ymin, zmin, xmax, ymax, zmax]
+        assert len(pointed_target) == 6, "pointed target should be a region in mc"
+        return pointed_target
