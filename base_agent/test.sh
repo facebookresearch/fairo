aa7d8c223c34a89f7d86299ca27f787580f61bc4
Yuxuan Sun <yuxuans@fb.com>
2021-02-07 03:29:59 -0800

    Add files back

---
+++ base_agent/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+
+cd $(dirname $0)
+coverage run --source . -m unittest discover -s ./test/ -t ./
+status=$?
+coverage report
+coverage xml -o /shared/test_base_agent.xml
+exit $status
