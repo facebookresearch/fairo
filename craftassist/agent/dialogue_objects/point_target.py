fc7c11a6e3a002d4fab22dc5560c8867371d59eb
Yuxuan Sun <yuxuans@fb.com>
2021-02-06 23:55:54 -0800

    simple fix

--- craftassist/agent/dialogue_objects/point_target.py
+++ craftassist/agent/dialogue_objects/point_target.py
@@ -9,9 +9,19 @@ class PointTargetInterpreter:
         # TODO: We might want to specifically check for BETWEEN/INSIDE, I'm not sure
         # what the +1s are in the return value
         mems = interpreter.subinterpret["reference_locations"](interpreter, speaker, d["location"])
-        return self.point_to_region(mems[0].get_point_at_target())
+        steps, reldir = interpret_relative_direction(interpreter.agent, d)
+        # if directly point at a reference object, call built-in fn to get pointed target
+        if steps is None and reldir is None:
+            loc = mems[0].get_point_at_target()
+        else:
+            loc, _ = interpreter.subinterpret["specify_locations"](
+                interpreter, speaker, mems, steps, reldir
+            )
+        return self.point_to_region(loc)
 
     def point_to_region(self, pointed_target):
-        # mc pointed target is always a region represented as [xmin, ymin, zmin, xmax, ymax, zmax]
-        assert len(pointed_target) == 6, "pointed target should be a region in mc"
+        # mc pointed target is either a point (x, y, z) or a region represented as (xmin, ymin, zmin, xmax, ymax, zmax)
+        assert len(pointed_target) == 6 or len(pointed_target) == 3, "pointed target should either be (x, y, z) or (xmin, ymin, zmin, xmax, ymax, zmax)"
+        if len(pointed_target) == 3:
+            pointed_target = (pointed_target[0], pointed_target[1], pointed_target[2], pointed_target[0], pointed_target[1], pointed_target[2])
         return pointed_target
