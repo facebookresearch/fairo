656449c0a2dccc1279ac96dfb264005a7b33d61b
Soumith Chintala <soumith@gmail.com>
2021-02-10 00:51:44 -0800

    [dashboard] new binary build
    ghstack-source-id: e783ce7ef176ac6a636650553d110e75b6bccb46
    Pull Request resolved: https://github.com/facebookresearch/droidlet/pull/123

--- dldashboard/web/build/service-worker.js
+++ dldashboard/web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67d10cc34a903392db7d490b3e6ebf5c.js"
+  "/precache-manifest.62f3608cccc55c7041d54ce412cc76fb.js"
 );
 
 self.addEventListener('message', (event) => {
