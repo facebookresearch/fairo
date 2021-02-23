e45587c094d07b4debded55e23af0c4ed85a183e
Rebecca QIan <rebeccaqian@devfair0419.h2.fair>
2021-02-23 12:13:28 -0800

    Eval nsp model on any dataset

--- tools/nsp_scripts/eval_model_on_dataset.py
+++ tools/nsp_scripts/eval_model_on_dataset.py
@@ -1,7 +1,5 @@
 """
 Copyright (c) Facebook, Inc. and its affiliates.
-
-This script evaluates a NSP model on a given dataset.
 """
 # flake8: noqa
 
@@ -9,55 +7,49 @@ import json
 import math
 import pickle
 import torch
+import argparse
 from transformers import AutoModel, AutoTokenizer, BertConfig
-# import ipdb; ipdb.set_trace()
-# from base_agent.ttad.ttad_transformer_model.train_model import *
-# from utils_caip import *
-# from utils_parsing import *
-# from train_model import *
-import os
-import sys
+from base_agent.ttad.ttad_transformer_model.utils_caip import *
+from base_agent.ttad.ttad_transformer_model.utils_parsing import *
+from base_agent.ttad.ttad_transformer_model.train_model import *
 from pprint import pprint
 
-sys.path.append(os.path.dirname(__file__) + "/../../base_agent")
-sys.path.append(os.path.realpath(__file__))
 
-# sys.path.append()
-print(os.path.dirname(__file__) + "/../../base_agent")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        default="craftassist/agent/datasets/annotated_data/",
+        type=str,
+        help="train/valid/test data",
+    )
+    args = parser.parse_args()
 
-model = "craftassist/agent/models/semantic_parser/ttad_bert_updated/caip_test_model.pth"
-args_path = "craftassist/agent/models/semantic_parser/ttad_bert_updated/caip_test_model_args.pk"
-args = pickle.load(open(args_path, "rb"))
+    model = "craftassist/agent/models/semantic_parser/ttad_bert_updated/caip_test_model.pth"
+    args_path = "craftassist/agent/models/semantic_parser/ttad_bert_updated/caip_test_model_args.pk"
+    args = pickle.load(open(args_path, "rb"))
 
-tokenizer = AutoTokenizer.from_pretrained(args.pretrained_encoder_name)
-full_tree, tree_i2w = json.load(open(args.tree_voc_file))
-dataset = CAIPDataset(tokenizer, args, prefix="", full_tree_voc=(full_tree, tree_i2w))
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_encoder_name)
+    full_tree, tree_i2w = json.load(open(args.tree_voc_file))
+    full_tree_voc = (full_tree, tree_i2w)
+    dataset = CAIPDataset(tokenizer, args, prefix="", full_tree_voc=(full_tree, tree_i2w))
 
-enc_model = AutoModel.from_pretrained(args.pretrained_encoder_name)
-bert_config = BertConfig.from_pretrained("bert-base-uncased")
-bert_config.is_decoder = True
-bert_config.add_cross_attention = True
-bert_config.vocab_size = len(tree_i2w) + 8
-bert_config.num_hidden_layers = args.num_decoder_layers
-dec_with_loss = DecoderWithLoss(bert_config, args, tokenizer)
-encoder_decoder = EncoderDecoderWithLoss(enc_model, dec_with_loss, args)
-map_location = None if torch.cuda.is_available() else torch.device("cpu")
-encoder_decoder.load_state_dict(torch.load(model, map_location=map_location), strict=False)
-encoder_decoder = encoder_decoder.cuda()
-_ = encoder_decoder.eval()
+    enc_model = AutoModel.from_pretrained(args.pretrained_encoder_name)
+    bert_config = BertConfig.from_pretrained("bert-base-uncased")
+    bert_config.is_decoder = True
+    bert_config.add_cross_attention = True
+    bert_config.vocab_size = len(tree_i2w) + 8
+    bert_config.num_hidden_layers = args.num_decoder_layers
+    dec_with_loss = DecoderWithLoss(bert_config, args, tokenizer)
+    encoder_decoder = EncoderDecoderWithLoss(enc_model, dec_with_loss, args)
+    map_location = None if torch.cuda.is_available() else torch.device("cpu")
+    encoder_decoder.load_state_dict(torch.load(model, map_location=map_location), strict=False)
+    encoder_decoder = encoder_decoder.cuda()
+    _ = encoder_decoder.eval()
+
+    model_trainer = ModelTrainer(args)
+    model_trainer.eval_model_on_dataset(encoder_decoder, "annotated", full_tree_voc, tokenizer, split="test")
 
 
-def get_beam_tree(chat, noop_thres=0.95, beam_size=5, well_formed_pen=1e2):
-    """Given a chat, runs beam search with the pretrained model and returns a logical form.
-
-    Args:
-        chat (str): text input
-    Returns:
-        logical_form (dict)
-    """
-    btr = beam_search(chat, encoder_decoder, tokenizer, dataset, beam_size, well_formed_pen)
-    if btr[0][0].get("dialogue_type", "NONE") == "NOOP" and math.exp(btr[0][1]) < noop_thres:
-        tree = btr[1][0]
-    else:
-        tree = btr[0][0]
-    return tree
+if __name__ == "__main__":
+    main()
