f2437d051b0e57a9795a0497bf30c607b55dea90
Rebecca Qian <rebeccaqian@fb.com>
2021-02-23 12:41:24 -0800

    swap test and valid ratios in split

--- base_agent/ttad/ttad_transformer_model/data_scripts/create_annotated_split.py
+++ base_agent/ttad/ttad_transformer_model/data_scripts/create_annotated_split.py
@@ -28,7 +28,7 @@ def create_data_split(data_path, filename, output_path, split_ratios):
         num_samples = int(round(len(annotated_data) * ratio))
         end_idx = start_idx + num_samples
         chunk = annotated_data[start_idx:min(end_idx, len(annotated_data))]
-        print(chunk[0])
+         # print(chunk[0])
         write_data_split(output_path, "{}/".format(split_name), filename, chunk)
         start_idx = end_idx
 
@@ -49,10 +49,10 @@ if __name__ == "__main__":
     parser.add_argument("--raw_data_path", type=str, default="craftassist/agent/datasets/full_data/")
     parser.add_argument("--output_path", type=str, default="craftassist/agent/datasets/annotated_data/")
     parser.add_argument("--filename", type=str, default="annotated.txt")
-    parser.add_argument("--split_ratio", type=str, default="0.7:0.2:0.1", help="train:test:valid split of dataset")
+    parser.add_argument("--split_ratio", type=str, default="0.7:0.2:0.1", help="train:valid:test split of dataset")
     args = parser.parse_args()
 
-    split_names = ["train", "test", "valid"]
+    split_names = ["train", "valid", "test"]
     split_ratios = zip(split_names, [float(x) for x in args.split_ratio.split(":")])
 
     create_data_split(args.raw_data_path, args.filename, args.output_path, split_ratios)
