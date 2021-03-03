67a92ebbdc9fcf330e31f9c46dc62077c7783eea
Dhiraj Prakashchand Gandhi <dhirajgandhi@devfair0249.h2.fair>
2021-03-03 08:46:17 -0800

    added coco dataset creation

--- locobot/label_propogation/test_annotation.py
+++ locobot/label_propogation/test_annotation.py
@@ -2,14 +2,21 @@
 import cv2
 import numpy as np
 import json
+import os
 
-file_name = 6875
+file_name = 146
+root_folder = "/checkpoint/dhirajgandhi/active_vision/real_world_dhiraj_home_data"
+
+# make labele directory
+if not os.path.isdir(os.path.join(root_folder, 'label')):
+    os.makedirs(os.path.join(root_folder, 'label'))
+    
 src_img = cv2.imread(
-    "/checkpoint/dhirajgandhi/active_vision/habitat_data/rgb/{:05d}.jpg".format(file_name)
+    os.path.join(root_folder, "rgb/{:05d}.jpg".format(file_name))
 )
 label_img = np.zeros((src_img.shape[0], src_img.shape[1]))
 
-with open("{:05d}.json".format(file_name), "r") as f:
+with open(os.path.join(root_folder, "vgg_annot/{:05d}.json".format(file_name)), "r") as f:
     label_data = json.load(f)
 k = list(label_data["_via_img_metadata"].keys())
 x = label_data["_via_img_metadata"][k[0]]["regions"][0]["shape_attributes"]["all_points_x"]
@@ -26,7 +33,7 @@ temp[:, :, 2] = label_img
 
 dst = cv2.addWeighted(src_img, 0.5, temp, 0.5, 0.0)
 cv2.imwrite(
-    "/checkpoint/dhirajgandhi/active_vision/habitat_data/label/{:05d}.png".format(file_name),
+    os.path.join(root_folder, "label/{:05d}.png".format(file_name)),
     label_img,
 )
 cv2.imwrite("test_{:05d}.jpg".format(file_name), dst)
