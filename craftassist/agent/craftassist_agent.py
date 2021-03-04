87b1e0e22d7cf0d40a5936f5dd59b87f2f5ac0f9
Rebecca-Qian <rebeccaqian@fb.com>
2021-03-04 01:01:09 -0800

    Remove path printing in tests

--- craftassist/agent/craftassist_agent.py
+++ craftassist/agent/craftassist_agent.py
@@ -50,7 +50,6 @@ random.seed(0)
 log_formatter = logging.Formatter(
     "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() %(levelname)s]: %(message)s"
 )
-logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().handlers.clear()
 
 sentry_sdk.init()  # enabled if SENTRY_DSN set in env
