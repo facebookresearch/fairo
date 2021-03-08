254c19e9f73fcf896fa88ea54330f5195b92902e
Yuxuan Sun <yuxuans@fb.com>
2021-03-08 05:09:10 -0800

    Add terminateAgent handler in base agent

--- base_agent/loco_mc_agent.py
+++ base_agent/loco_mc_agent.py
@@ -139,6 +139,11 @@ class LocoMCAgent(BaseAgent):
                 "allChats": self.dashboard_memory["chats"],
             }
             sio.emit("setChatResponse", payload)
+        
+        @sio.on("terminateAgent")
+        def terminate_agent(sid, msg):
+            exit(0)
+
 
     def init_physical_interfaces(self):
         """
