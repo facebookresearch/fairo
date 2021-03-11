a383ab719786b7bbefed3af7f42219f557c385e2
Rebecca-Qian <rebeccaqian@fb.com>
2021-03-11 00:31:52 -0800

    Switch sha1sum to shasum for mac compatibility

--- tools/data_scripts/checksum_fn.sh
+++ tools/data_scripts/checksum_fn.sh
@@ -7,7 +7,7 @@ calculate_sha1sum() {
     echo $'\nCalculating checksum for' ${CHECKSUM_FOLDER}
     echo "Saving to ${SAVE_TO_PATH}"
     find $CHECKSUM_FOLDER -type f ! -name '*checksum*' ! -name '*MD*' \
-       -not -path '*/\.*' -printf "%P\n" | sort -z | sha1sum | tr -d '-' \
+       -not -path '*/\.*' -printf "%P\n" | sort -z | shasum | tr -d '-' \
        | xargs > $SAVE_TO_PATH
     echo "Saved checksum " $(cat $SAVE_TO_PATH) $'\n'
 }
