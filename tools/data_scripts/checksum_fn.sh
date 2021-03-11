783a03ea1db6077b8cda16fc19d8a8d8d207e354
Anurag <apratik@fb.com>
2021-03-11 18:15:31 -0500

    nit

--- tools/data_scripts/checksum_fn.sh
+++ tools/data_scripts/checksum_fn.sh
@@ -6,7 +6,7 @@ calculate_sha1sum() {
     SAVE_TO_PATH=$2
     echo $'\nCalculating checksum for' ${CHECKSUM_FOLDER}
     echo "Saving to ${SAVE_TO_PATH}"
-    # echo "Uncomment to see what the checksum is being computer for "
+    # "Uncomment below to see what the checksum is being computed for "
     # echo $(find $CHECKSUM_FOLDER -type f ! -name '*checksum*' ! -name '*MD*' \
     #    -not -path '*/\.*' -exec realpath --relative-to $CHECKSUM_FOLDER {} \; | sort -d)
     find $CHECKSUM_FOLDER -type f ! -name '*checksum*' ! -name '*MD*' \
