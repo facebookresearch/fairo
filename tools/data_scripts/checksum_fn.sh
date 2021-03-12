ec8480bc0a71172b60a4ef6d162734bae2454844
anuragprat1k <57542204+anuragprat1k@users.noreply.github.com>
2021-03-12 13:56:44 -0500

    shasum bugfix (#249)
    * shasum
    
    * nits
    
    * craft sums
    
    * dockerfile update
    
    * fixes
    
    * dataset update
    
    Co-authored-by: Anurag Pratik <apratik@devfair0228.h2.fair>

--- tools/data_scripts/checksum_fn.sh
+++ tools/data_scripts/checksum_fn.sh
@@ -5,12 +5,10 @@ calculate_sha1sum() {
     CHECKSUM_FOLDER=$1
     SAVE_TO_PATH=$2
     echo $'\nCalculating checksum for' ${CHECKSUM_FOLDER}
-    echo "Saving to ${SAVE_TO_PATH}"
-    # "Uncomment below to see what the checksum is being computed for "
-    # echo $(find $CHECKSUM_FOLDER -type f ! -name '*checksum*' ! -name '*MD*' \
-    #    -not -path '*/\.*' -exec realpath --relative-to $CHECKSUM_FOLDER {} \; | sort -d)
+    # -exec calculates shasum for each file
+    # cut trims the output of exec to exclude file names and finally pipes all shasums to the final shasum
     find $CHECKSUM_FOLDER -type f ! -name '*checksum*' ! -name '*MD*' \
-       -not -path '*/\.*' -exec realpath --relative-to=$CHECKSUM_FOLDER {} \; | sort -d | shasum | tr -d '-' \
-       | xargs > $SAVE_TO_PATH
+       -not -path '*/\.*' -exec shasum {} \; | cut -d" " -f1 | sort -d | shasum | tr -d '-' | xargs > $SAVE_TO_PATH
+    echo "Saving to ${SAVE_TO_PATH}"
     echo "Saved checksum " $(cat $SAVE_TO_PATH) $'\n'
 }
