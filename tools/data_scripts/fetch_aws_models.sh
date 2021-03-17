01ba0be196288a6e2bb6c7909c4473772557a777
kavyasrinet <ksrinet@fb.com>
2021-03-17 13:00:23 -0700

    Updating fetch datasets and model script with better logs (#261)
    * Updating fetch datasets and model script with better logs
    
    * Absolute path + error message on unsuccesful download
    
    * Updating correct checksum file path

--- tools/data_scripts/fetch_aws_models.sh
+++ tools/data_scripts/fetch_aws_models.sh
@@ -13,19 +13,28 @@ MODELS_DIRNAME=models_folder
 if [ -z $1 ]
 then
 	AGENT="craftassist"
+	echo "Defaulting to default agent: '$AGENT'"
 else
 	AGENT=$1
 fi
-CHKSUM=$2
 
-echo "Checksum" $CHKSUM
+if [ -z $2 ]
+then
+	CHECKSUM_FILE="${ROOTDIR}/tools/data_scripts/default_checksums/nsp.txt"
+	CHECKSUM=`cat $CHECKSUM_FILE`  
+	echo "Downloading model folder with default checksum from file: '$CHECKSUM_FILE'"
+else
+	CHECKSUM=$2
+fi
+
+echo "Checksum" $CHECKSUM
 
 MODELS_DIRNAME=models_folder
 
 cd $ROOTDIR
 
-echo "====== Downloading http://craftassist.s3-us-west-2.amazonaws.com/pubr/${MODELS_DIRNAME}_${CHKSUM}.tar.gz to $ROOTDIR/${MODELS_DIRNAME}_${CHKSUM}.tar.gz ======"
-curl http://craftassist.s3-us-west-2.amazonaws.com/pubr/${MODELS_DIRNAME}_${CHKSUM}.tar.gz -o $MODELS_DIRNAME.tar.gz
+echo "====== Downloading http://craftassist.s3-us-west-2.amazonaws.com/pubr/${MODELS_DIRNAME}_${CHECKSUM}.tar.gz to $ROOTDIR/${MODELS_DIRNAME}_${CHECKSUM}.tar.gz ======"
+curl http://craftassist.s3-us-west-2.amazonaws.com/pubr/${MODELS_DIRNAME}_${CHECKSUM}.tar.gz -o $MODELS_DIRNAME.tar.gz 
 
 if [ -d "${AGENT}/agent/models" ]
 then
@@ -35,7 +44,7 @@ fi
 
 mkdir -p $AGENT/agent/models/
 
-tar -xzvf $MODELS_DIRNAME.tar.gz -C $AGENT/agent/models/ --strip-components 1
+tar -xzvf $MODELS_DIRNAME.tar.gz -C $AGENT/agent/models/ --strip-components 1 || echo "Failed to download and unarchive. Please make sure the file: ${MODELS_DIRNAME}_${CHECKSUM}.tar.gz exists on S3." 
 
 if [ $AGENT == "locobot" ]; then
     curl https://locobot-bucket.s3-us-west-2.amazonaws.com/perception_models.tar.gz -o locobot_models.tar.gz
