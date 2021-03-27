ee82a3c64c852ab23b274f71d5f5a598043c9457
Rebecca-Qian <rebeccaqian@fb.com>
2021-03-26 18:57:23 -0700

    Make data validation unit test for ground truth and annotated

---
+++ craftassist/test/test_validate_dataset.py
@@ -0,0 +1,45 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+"""
+
+import os
+import unittest
+
+from base_craftassist_test_case import BaseCraftassistTestCase
+from fake_agent import MockOpt
+from validate_json import JSONValidator
+
+FULL_DATA_DIR = os.path.join(os.path.dirname(__file__), "../agent/datasets/full_data/")
+GROUND_TRUTH_DATA_DIR = os.path.join(os.path.dirname(__file__), "../agent/datasets/ground_truth/datasets/")
+SCHEMA_DIR = os.path.join(os.path.dirname(__file__), "../../base_agent/documents/json_schema/")
+
+"""This class tests safety checks using a preset list of blacklisted words.
+"""
+
+
+class DataValidationTest(BaseCraftassistTestCase):
+    def setUp(self):
+        # accept all spans
+        self.json_validator_all = JSONValidator(SCHEMA_DIR, "all")
+        # accept only array spans
+        self.json_validator_array = JSONValidator(SCHEMA_DIR, "array")
+
+    def test_locobot(self):
+        res = self.json_validator_all.validate_data(FULL_DATA_DIR + "locobot.txt", test_mode=True)
+        self.assertTrue(res)
+
+    def test_high_pri_commands(self):
+        res = self.json_validator_all.validate_data(FULL_DATA_DIR + "high_pri_commands.txt", test_mode=True)
+        self.assertTrue(res)
+
+    def test_short_commands(self):
+        res = self.json_validator_all.validate_data(FULL_DATA_DIR + "short_commands.txt", test_mode=True)
+        self.assertTrue(res)
+
+    def test_annotated(self):
+        res = self.json_validator_array.validate_data(FULL_DATA_DIR + "annotated.txt", test_mode=True)
+        self.assertTrue(res)
+
+
+if __name__ == "__main__":
+    unittest.main()
