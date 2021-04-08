581d8319ade8aca0ac0c9c983720e85824128690
Rebecca-Qian <rebeccaqian@fb.com>
2021-04-07 23:48:52 -0700

    Write to unique autocomplete annotations filepath

--- tools/data_processing/autocomplete_postprocess.py
+++ tools/data_processing/autocomplete_postprocess.py
@@ -5,6 +5,7 @@ import pprint as pp
 import argparse
 import json
 import pkg_resources
+from datetime import date
 
 """Applies updates to annotated dataset following grammar changes.
 """
@@ -52,7 +53,7 @@ def traverse_subtree(command, action_dict):
     return action_dict
 
 def write_file(dataset, file_path):
-    with open(file_path, "w") as fd:
+    with open(file_path, "w+") as fd:
         for line in dataset:
             fd.write(line + "\n")
 
@@ -66,7 +67,7 @@ if __name__ == "__main__":
         dataset = json.load(fd)
     autocomplete_annotations = {}
     updated_dataset = []
-    datasets_read_path = "{}/{}".format(pkg_resources.resource_filename('craftassist.agent', 'datasets'), "full_data/high_pri_commands.txt")
+    datasets_read_path = "{}/{}".format(pkg_resources.resource_filename('craftassist.agent', 'datasets'), "full_data/autocomplete_{}.txt".format(date.today().strftime("%d_%m_%Y")))
 
     # Read the file, update  
     for command in dataset:
@@ -74,12 +75,13 @@ if __name__ == "__main__":
         updated_tree = traverse_tree(command, action_dict)
         autocomplete_annotations[command] = updated_tree
     # Load all the existing action dictionaries
-    with open(datasets_read_path) as fd:
-        existing_annotations = fd.readlines()
-        for row in existing_annotations:
-            command, action_dict = row.strip().split("|")
-            if command not in autocomplete_annotations:
-                autocomplete_annotations[command] = json.loads(action_dict)
+    if os.path.isfile(datasets_read_path):
+        with open(datasets_read_path) as fd:
+            existing_annotations = fd.readlines()
+            for row in existing_annotations:
+                command, action_dict = row.strip().split("|")
+                if command not in autocomplete_annotations:
+                    autocomplete_annotations[command] = json.loads(action_dict)
 
     for command in autocomplete_annotations:
         updated_dataset.append("{}|{}".format(command, json.dumps(autocomplete_annotations[command])))
