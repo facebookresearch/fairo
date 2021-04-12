cd390cbe7caaa0768b1d3ac08272b24f228fc675
Rebecca-Qian <rebeccaqian@fb.com>
2021-04-11 23:00:01 -0700

    Fix bug in s3 download_file option

--- tools/data_scripts/fetch_internal_resources.py
+++ tools/data_scripts/fetch_internal_resources.py
@@ -10,8 +10,8 @@ def fetch_safety_words():
 
     Currently needs to be called with sys arg passing in output directory.
     """
-    s3 = boto3.client('s3')
     try:
+        s3 = boto3.client('s3')
         response = s3.head_bucket(Bucket='droidlet-internal')
         if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             print("Authenticated user, fetching safety words list.")
@@ -24,10 +24,14 @@ def fetch_safety_words():
         pass
     # If awscli is not setup, eg. in one time use containers, read access tokens from environment   
     try:
-        return s3.download_file(
-            's3://droidlet-internal/',
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        s3 = boto3.client('s3',
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
             aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
+        return s3.download_file(
+            'droidlet-internal',
+            'safety.txt',
+            os.path.realpath(sys.argv[1])
         )
     except Exception as e:
         print(e)
