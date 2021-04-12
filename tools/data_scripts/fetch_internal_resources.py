9d1534b55fa2746947d1b4275fc3e408767a5667
Rebecca-Qian <rebeccaqian@fb.com>
2021-04-11 22:10:46 -0700

    Add fallback for s3 read with boto3 download using access tokens

--- tools/data_scripts/fetch_internal_resources.py
+++ tools/data_scripts/fetch_internal_resources.py
@@ -3,21 +3,32 @@ import subprocess
 import os
 import sys
 
+
 def fetch_safety_words():
     """
     Fetch secure s3 resources for internal users and production systems, eg. safety keyword list.
 
     Currently needs to be called with sys arg passing in output directory.
     """
+    s3 = boto3.client('s3')
     try:
-        s3 = boto3.client('s3')
         response = s3.head_bucket(Bucket='droidlet-internal')
         if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             print("Authenticated user, fetching safety words list.")
             path_to_root = os.path.realpath(sys.argv[1])
-            subprocess.run(
+            return subprocess.run(
                 "aws s3 cp s3://droidlet-internal/safety.txt {}".format(path_to_root), shell=True
             )
+    except Exception as e:
+        print(e)
+        pass
+    # If awscli is not setup, eg. in one time use containers, read access tokens from environment   
+    try:
+        return s3.download_file(
+            's3://droidlet-internal/',
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
     except Exception as e:
         print(e)
 
