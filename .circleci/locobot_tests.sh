2cf7b328d95c5bb64f9fc6a9737f31096812ff09
Soumith Chintala <soumith@gmail.com>
2021-05-11 02:49:08 -0400

    fix circleci tests
    [ghstack-poisoned]

--- .circleci/locobot_tests.sh
+++ .circleci/locobot_tests.sh
@@ -22,24 +22,24 @@ SHARED_PATH=/shared
 python -m Pyro4.naming -n $LOCOBOT_IP &
 sleep 10
 
-python locobot/robot/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
+python droidlet/lowlevel/locobot/remote/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
 BGPID=$!
 sleep 30
-python locobot/test/smoke_test.py
+python droidlet/lowlevel/locobot/tests/smoke_test.py
 kill -9 $BGPID
 sleep 5
 
-python locobot/robot/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
+python droidlet/lowlevel/locobot/remote/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
 BGPID=$!
 sleep 30
-pushd locobot/test
+pushd droidlet/lowlevel/locobot/tests/
 pytest --cov-report=xml:$SHARED_PATH/test_habitat.xml --cov=../ test_habitat.py --disable-pytest-warnings
 popd
 kill -9 $BGPID
 sleep 5
 
 COV_RELATIVE=locobot
-python locobot/robot/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
+python droidlet/lowlevel/locobot/remote/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
 BGPID=$!
 sleep 30
 deactivate
@@ -47,22 +47,21 @@ source activate /root/miniconda3/envs/droidlet_env
 pip install -r locobot/requirements.txt
 python setup.py develop
 
-pytest --cov-report=xml:$SHARED_PATH/test_mover.xml --cov=$COV_RELATIVE locobot/test/test_mover.py --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_mover.xml --cov=$COV_RELATIVE droidlet/lowlevel/locobot/tests/test_mover.py --disable-pytest-warnings
 kill -9 $BGPID
 sleep 5
 
 deactivate
 source /root/pyenv_pyrobot_python3/bin/activate
-python locobot/robot/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
+python droidlet/lowlevel/locobot/remote/remote_locobot.py --ip $LOCOBOT_IP --backend habitat &
 BGPID=$!
 sleep 30
 deactivate
 source activate /root/miniconda3/envs/droidlet_env
 
-# pytest --cov-report=xml:$SHARED_PATH/test_locobot_agent.xml --cov=$COV_RELATIVE locobot/test/test_locobot_agent.py --disable-pytest-warnings
-pytest --cov-report=xml:$SHARED_PATH/test_handlers.xml --cov=$COV_RELATIVE locobot/test/test_handlers.py --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_handlers.xml --cov=$COV_RELATIVE droidlet/perception/tests/test_perception.py --disable-pytest-warnings
 
 kill -9 $BGPID
-pytest --cov-report=xml:$SHARED_PATH/test_memory.xml --cov=$COV_RELATIVE locobot/test/test_memory.py --disable-pytest-warnings
-pytest --cov-report=xml:$SHARED_PATH/test_interpreter_mock.xml --cov=$COV_RELATIVE locobot/test/test_interpreter_mock.py --disable-pytest-warnings
-pytest --cov-report=xml:$SHARED_PATH/test_utils.xml --cov=$COV_RELATIVE locobot/test/test_utils.py --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_memory.xml --cov=$COV_RELATIVE droidlet/memory/robot/tests/test_memory.py --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_interpreter_mock.xml --cov=$COV_RELATIVE droidlet/interpreter/robot/tests/test_interpreter_mock.py --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_utils.xml --cov=$COV_RELATIVE droidlet/lowlevel/locobot/tests/test_utils.py --disable-pytest-warnings
