effc73732d10a346909a4c352645b787cddc3772
Soumith Chintala <soumith@gmail.com>
2021-05-13 09:22:42 -0400

    fix pytest path in craftassist/test/test.sh
    [ghstack-poisoned]

--- craftassist/test/test.sh
+++ craftassist/test/test.sh
@@ -8,6 +8,6 @@ cd $(dirname $0)
 SHARED_PATH=/shared
 COV_RELATIVE=craftassist
 
-pytest --cov-report=xml:$SHARED_PATH/test_MC.xml --cov=$COV_RELATIVE craftassist/test/ --disable-pytest-warnings
+pytest --cov-report=xml:$SHARED_PATH/test_MC.xml --cov=$COV_RELATIVE . --disable-pytest-warnings
 status=$?
 exit $status
