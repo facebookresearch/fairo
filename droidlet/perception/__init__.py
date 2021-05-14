d5717bb05c56eb44f34551e87c3071b54f6db8be
Yuxuan Sun <yuxuans@fb.com>
2021-05-14 15:00:12 -0700

    Temporary fix for craftassist test by commenting out perception init content

--- droidlet/perception/__init__.py
+++ droidlet/perception/__init__.py
@@ -1,42 +1,42 @@
-from .perception_helpers import (
-    random_colors,
-    draw_xyz,
-    get_random_color,
-    get_coords,
-    get_color_tag,
-)
-from .handlers import (
-    InputHandler,
-    MemoryHandler,
-    DetectionHandler,
-    FaceRecognitionHandler,
-    ObjectDeduplicationHandler,
-    Detection,
-    WorldObject,
-    RGBDepth,
-    Human,
-    HumanKeypoints,
-)
-from .perception import Perception, SlowPerception
-from .self_perception import SelfPerception
+# from .perception_helpers import (
+#     random_colors,
+#     draw_xyz,
+#     get_random_color,
+#     get_coords,
+#     get_color_tag,
+# )
+# from .handlers import (
+#     InputHandler,
+#     MemoryHandler,
+#     DetectionHandler,
+#     FaceRecognitionHandler,
+#     ObjectDeduplicationHandler,
+#     Detection,
+#     WorldObject,
+#     RGBDepth,
+#     Human,
+#     HumanKeypoints,
+# )
+# from .perception import Perception, SlowPerception
+# from .self_perception import SelfPerception
 
-__all__ = [
-    Perception,
-    SlowPerception,
-    SelfPerception,
-    Detection,
-    WorldObject,
-    Human,
-    HumanKeypoints,
-    RGBDepth,
-    random_colors,
-    draw_xyz,
-    get_random_color,
-    get_coords,
-    get_color_tag,
-    InputHandler,
-    DetectionHandler,
-    MemoryHandler,
-    FaceRecognitionHandler,
-    ObjectDeduplicationHandler,
-]
+# __all__ = [
+#     Perception,
+#     SlowPerception,
+#     SelfPerception,
+#     Detection,
+#     WorldObject,
+#     Human,
+#     HumanKeypoints,
+#     RGBDepth,
+#     random_colors,
+#     draw_xyz,
+#     get_random_color,
+#     get_coords,
+#     get_color_tag,
+#     InputHandler,
+#     DetectionHandler,
+#     MemoryHandler,
+#     FaceRecognitionHandler,
+#     ObjectDeduplicationHandler,
+# ]
