20c68ac374a1d00ad21d31705d91cfd392a1dac2
Kavya Srinet <kavyasrinet@fb.com>
2021-05-26 15:53:51 -0700

    increase ci timeout

--- .circleci/craftassist_tests.sh
+++ .circleci/craftassist_tests.sh
@@ -7,7 +7,7 @@ SHARED_PATH=/shared
 pushd $script_dir/../
 
 pytest --cov-report=xml:$SHARED_PATH/test_full_craftassist_agent.xml --cov=$COV_RELATIVE agents/craftassist/tests/ --disable-pytest-warnings
-pytest --cov-report=xml:$SHARED_PATH/craftassist_tests.xml --cov=droidlet droidlet/ --disable-pytest-warnings \
+pytest --cov-report=xml:$SHARED_PATH/droidlet_craftassist_tests.xml --cov=droidlet droidlet/ --disable-pytest-warnings \
        --ignore droidlet/memory/robot/ \
        --ignore droidlet/perception/robot/ \
        --ignore droidlet/interpreter/robot/ \
