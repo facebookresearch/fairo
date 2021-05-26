d74c78b8b55f2c00228da0b1d12df7c1ee26c1ab
Kavya Srinet <kavyasrinet@fb.com>
2021-05-26 16:03:11 -0700

    Adding a test for craftassist agent with cuberite

--- agents/craftassist/tests/test_ca_agent_with_cuberite.py
+++ agents/craftassist/tests/test_ca_agent_with_cuberite.py
@@ -1,8 +1,14 @@
 import unittest
+import subprocess
+from ..craftassist_agent import CraftAssistAgent, opts
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_agent_with_cuberite(self):
+        cuberite_process = "droidlet/lowlevel/minecraft/cuberite_process.py --con"
+        p = subprocess.call("python", cuberite_process, "--config", "flat_world")
+        sa = CraftAssistAgent(opts)
+        sa.start()
+
 
 if __name__ == '__main__':
     unittest.main()
