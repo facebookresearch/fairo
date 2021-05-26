8e1b2e4958b829c4a0da06d81efac227624f0667
Yuxuan Sun <34047968+snyxan@users.noreply.github.com>
2021-05-26 14:20:50 -0700

    Fix submodule version to stable, working ones as in main branch (#438)
    * Fix cuberite submodule version to 999faa9
    
    * Fix rapidjson submodule version to 585042c
    
    * Fix pybind11 submodule version to 085a294
    
    * Fix argparse submodule verison to fb0fe8d
    
    * Fix lua-struct submodule version to ea38f81
    
    comment cleanup + cuberite process fix
    
    better cuberite tests
    
    final cub tests

---
+++ droidlet/lowlevel/minecraft/tests/test_cuberite.py
@@ -0,0 +1,47 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+"""
+import unittest
+from droidlet.lowlevel.minecraft.cuberite_process import CuberiteProcess, create_workdir, repo_home
+from os import path
+from droidlet.lowlevel.minecraft.craftassist_cuberite_utils.ping_cuberite import ping
+
+
+class CuberiteBasicTest(unittest.TestCase):
+
+    def test_create_workdir(self):
+        # diverse_world
+        plugins = ["debug", "chatlog", "point_blocks"]
+        workdir = create_workdir(config_name="flat_world",
+                                 seed=0, game_mode="creative",
+                                 port=25565,
+                                 plugins=plugins,
+                                 place_blocks_yzx=None,
+                                 workdir_root=None)
+        assert workdir is not None
+        assert "tmp" in workdir
+        assert "cuberite" in workdir
+
+    def test_check_cuberite_executable(self):
+        popen = [repo_home + "/server/cuberite/Server/Cuberite"]
+        assert path.exists(popen[0]) == True
+
+    def test_launch_cuberite_process(self):
+        plugins = ["debug", "chatlog", "point_blocks"]
+        p = CuberiteProcess(
+            config_name="flat_world",
+            seed=0,
+            game_mode="creative",
+            port=25565,
+            plugins=plugins)
+        try:
+            ping("localhost", 25565)
+            print("Successfully pinged an existing cuberite instance.")
+        except:
+            self.fail("ping cuberite raised exception unexpectedly!")
+        p.destroy()
+        # Assert that ping raises an exception.
+        self.assertRaises(ConnectionRefusedError, ping, **{"host": "localhost", "port": 25565})
+
+if __name__ == '__main__':
+    unittest.main()
