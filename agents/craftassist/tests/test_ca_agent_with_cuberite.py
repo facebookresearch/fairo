bc2a4a7989e56199e39a728d8ef0d0796919c913
Yuxuan Sun <yuxuans@fb.com>
2021-05-27 04:17:38 -0700

    python -> python3

--- agents/craftassist/tests/test_ca_agent_with_cuberite.py
+++ agents/craftassist/tests/test_ca_agent_with_cuberite.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
         opts = MockOpt()
         opts.port = 25565
         cuberite_process = "droidlet/lowlevel/minecraft/cuberite_process.py "
-        p = subprocess.Popen([f"python {cuberite_process} --config flat_world"], shell=True)
+        p = subprocess.Popen([f"python3 {cuberite_process} --config flat_world"], shell=True)
         time.sleep(1) # let cuberite fully start
         sa = CraftAssistAgent(opts)
 
