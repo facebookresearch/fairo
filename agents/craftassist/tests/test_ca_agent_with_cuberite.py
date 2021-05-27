aac12dc3c4608098de8337c579c3675f4fb76f2b
Yuxuan Sun <yuxuans@fb.com>
2021-05-27 01:04:44 -0700

    Make cuberite with ca agent connection unittest work

--- agents/craftassist/tests/test_ca_agent_with_cuberite.py
+++ agents/craftassist/tests/test_ca_agent_with_cuberite.py
@@ -1,14 +1,17 @@
 import unittest
 import subprocess
+import time
 from ..craftassist_agent import CraftAssistAgent
+from .fake_agent import MockOpt
 
 class MyTestCase(unittest.TestCase):
     def test_agent_with_cuberite(self):
-        # cuberite_process = "droidlet/lowlevel/minecraft/cuberite_process.py --con"
-        # p = subprocess.call("python", cuberite_process, "--config", "flat_world")
-        # sa = CraftAssistAgent(opts)
-        # sa.start()
-        pass
+        opts = MockOpt()
+        opts.port = 25565
+        cuberite_process = "droidlet/lowlevel/minecraft/cuberite_process.py "
+        p = subprocess.Popen([f"python {cuberite_process} --config flat_world"], shell=True)
+        time.sleep(1) # let cuberite fully start
+        sa = CraftAssistAgent(opts)
 
 
 if __name__ == '__main__':
