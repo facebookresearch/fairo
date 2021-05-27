02879119419bc733f566319dddb33ceabe09e314
Jerry Han <jhan6501@devfair025.maas>
2021-05-27 16:45:14 -0700

    added a navigation pane for teleop. Navbar can choose whether to display mane pane or navigation pane

--- dldashboard/web/src/MobileMainPane.js
+++ dldashboard/web/src/MobileMainPane.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
-import LiveImage from "./components/LiveImage";
-import LiveObjects from "./components/LiveObjects";
-import LiveHumans from "./components/LiveHumans";
-import stateManager from "./StateManager";
-import InteractApp from "./components/Interact/InteractApp";
-import Memory2D from "./components/Memory2D";
 import NavbarComponent from "./components/Navbar";
-
+import MobileHomePane from "./components/MobileHomePane";
+import MobileNavigationPane from "./components/MobileNavigationPane";
 class MobileMainPane extends React.Component {
   constructor(props) {
     let width = window.innerWidth;
@@ -28,67 +23,18 @@ class MobileMainPane extends React.Component {
   }
 
   render() {
+    let displayPane;
     if (this.state.screen === "home") {
-      return (
-        <Container fluid>
-          <Row>
-            <Col>
-              Video Feed 1
-              <LiveImage
-                type={"rgb"}
-                height={this.state.imageWidth}
-                width={this.state.imageWidth}
-                offsetH={0}
-                offsetW={0}
-                stateManager={stateManager}
-                isMobile={true}
-              />
-            </Col>
-            <Col>
-              Memory 2d
-              <Memory2D
-                stateManager={stateManager}
-                isMobile={true}
-                dimensions={this.state.imageWidth}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              Video Feed 2
-              <LiveImage
-                type={"depth"}
-                height={this.state.imageWidth}
-                width={this.state.imageWidth}
-                offsetH={0}
-                offsetW={0}
-                stateManager={stateManager}
-                isMobile={true}
-              />
-            </Col>
-
-            <Col>
-              Video Feed 3
-              <LiveHumans
-                type={"depth"}
-                height={this.state.imageWidth}
-                width={this.state.imageWidth}
-                offsetH={0}
-                offsetW={0}
-                stateManager={stateManager}
-                isMobile={true}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <InteractApp stateManager={stateManager} />
-          </Row>
-          <NavbarComponent paneHandler={this.paneHandler.bind(this)} />
-        </Container>
-      );
+      displayPane = <MobileHomePane imageWidth={this.state.imageWidth} />;
     } else {
-      return <p> hai </p>;
+      displayPane = <MobileNavigationPane imageWidth={this.state.imageWidth} />;
     }
+    return (
+      <Container fluid>
+        {displayPane}
+        <NavbarComponent paneHandler={this.paneHandler.bind(this)} />
+      </Container>
+    );
   }
 }
 
---
+++ dldashboard/web/src/components/MobileNavigationPane.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+import LiveImage from "./LiveImage";
+import LiveHumans from "./LiveHumans";
+import stateManager from ".././StateManager";
+import Memory2D from "./Memory2D";
+
+class MobileNavigationPane extends React.Component {
+  constructor(props) {
+    let width = window.innerWidth;
+    super(props);
+    console.log("the props are");
+    console.log(props);
+    let imageWidth = this.props.imageWidth;
+  }
+
+  render() {
+    console.log("image width is");
+    console.log(this.props.imageWidth);
+    return (
+      <div>
+        <Row>
+          <Col>
+            Video Feed 1
+            <LiveImage
+              type={"rgb"}
+              height={this.props.imageWidth}
+              width={this.props.imageWidth}
+              offsetH={0}
+              offsetW={0}
+              stateManager={stateManager}
+              isMobile={true}
+            />
+          </Col>
+          <Col>
+            Memory 2d
+            <Memory2D
+              stateManager={stateManager}
+              isMobile={true}
+              dimensions={this.props.imageWidth}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Video Feed 2
+            <LiveImage
+              type={"depth"}
+              height={this.props.imageWidth}
+              width={this.props.imageWidth}
+              offsetH={0}
+              offsetW={0}
+              stateManager={stateManager}
+              isMobile={true}
+            />
+          </Col>
+
+          <Col>
+            Video Feed 3
+            <LiveHumans
+              type={"depth"}
+              height={this.props.imageWidth}
+              width={this.props.imageWidth}
+              offsetH={0}
+              offsetW={0}
+              stateManager={stateManager}
+              isMobile={true}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default MobileNavigationPane;
