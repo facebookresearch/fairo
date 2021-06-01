0693fe303e53d239d213ae12197171276cf18632
Yuxuan Sun <yuxuans@fb.com>
2021-06-01 10:12:23 -0700

    Added unittest for cuberite agent connection

---
+++ droidlet/lowlevel/minecraft/tests/test_cuberite_agent_connection.py
@@ -0,0 +1,37 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+"""
+import os
+import signal
+import subprocess
+import time
+import unittest
+from agents.craftassist.craftassist_agent import CraftAssistAgent
+from agents.craftassist.tests.fake_agent import MockOpt
+
+
+class MyTestCase(unittest.TestCase):
+    def test_agent_cuberite_connection(self):
+        opts = MockOpt()
+        opts.port = 25565
+        cuberite_process_path = "droidlet/lowlevel/minecraft/cuberite_process.py"
+        with open("cuberite_log.txt", "w") as f:
+            proc = subprocess.Popen(
+                [f"python3 {cuberite_process_path} --config flat_world"],
+                shell=True,
+                stdout=f,
+                preexec_fn=os.setsid,
+            )
+            time.sleep(5)  # let cuberite fully starts
+            sa = CraftAssistAgent(opts)
+            os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+            time.sleep(5)  # let cuberite fully terminates
+
+        with open("cuberite_log.txt", "r") as f:
+            cuberite_log = f.read()
+            # check if agent has joined the game from cuberite log
+            self.assertIn("has joined the game", cuberite_log)
+
+
+if __name__ == "__main__":
+    unittest.main()
