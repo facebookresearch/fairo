6e608f56dec00a28346a442f3d9f2ba597a27837
Rebecca Qian <rebeccaqian@fb.com>
2021-06-04 00:23:01 -0700

    file name as command line arg

--- droidlet/tools/crowdsourcing/droidlet_static_html_task/process_s3_logs.py
+++ droidlet/tools/crowdsourcing/droidlet_static_html_task/process_s3_logs.py
@@ -8,16 +8,13 @@ import re
 from datetime import datetime
 import argparse
 
-# TURK_LOGS_DIRECTORY = "~/s3_logs"
-# TURK_OUTPUT_DIRECTORY = "~/s3_logs/processed_logs/"
-
 pd.set_option('display.max_rows', 10)
 
-def read_turk_logs(turk_logs_directory, turk_output_directory):
+def read_turk_logs(turk_logs_directory, turk_output_directory, filename):
     # Crawl turk logs directory
     all_turk_interactions = None
 
-    for csv_path in glob.glob('{turk_logs_dir}/**/{csv_filename}'.format(turk_logs_dir=turk_logs_directory, csv_filename='nsp_outputs.csv')):
+    for csv_path in glob.glob('{turk_logs_dir}/**/{csv_filename}'.format(turk_logs_dir=turk_logs_directory, csv_filename=filename + '.csv')):
         print(csv_path)
         with open(csv_path) as fd:
             # collect the NSP outputs CSV
@@ -36,7 +33,7 @@ def read_turk_logs(turk_logs_directory, turk_output_directory):
     print(all_turk_interactions.shape)
 
     # Pipe CSV outputs to another file
-    out_path = "{output_dir}/nsp_outputs_{timestamp}.csv".format(output_dir=turk_output_directory, timestamp=datetime.now().strftime("%Y-%m-%d"))
+    out_path = "{output_dir}/{filename}_{timestamp}.csv".format(output_dir=turk_output_directory, filename=filename, timestamp=datetime.now().strftime("%Y-%m-%d"))
     all_turk_interactions.to_csv(out_path)
 
 
@@ -44,7 +41,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--turk_logs_directory', help='where to read s3 logs from')
     parser.add_argument('--turk_output_directory', help='where to write the collated NSP outputs')
+    parser.add_argument('--filename', help='name of the CSV file we want to read, eg. nsp_outputs')
     args = parser.parse_args()
 
-    read_turk_logs(args.turk_logs_directory, args.turk_output_directory)
+    read_turk_logs(args.turk_logs_directory, args.turk_output_directory, args.filename)
     
