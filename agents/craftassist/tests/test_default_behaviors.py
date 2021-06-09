4c0f6d80f8e996a0a3b5cc89f5bf77d339900a3d
Kavya Srinet <kavyasrinet@fb.com>
2021-06-08 18:24:40 -0700

    Adding unit test for default behaviors
    call ut
    
    call ut

---
+++ agents/craftassist/tests/test_default_behaviors.py
@@ -0,0 +1,24 @@
+import unittest
+from agents.craftassist.tests.base_craftassist_test_case import BaseCraftassistTestCase
+from droidlet.interpreter.craftassist.default_behaviors import build_random_shape, come_to_player
+
+
+class TestDefaultBehavior(BaseCraftassistTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_build_random_shape(self):
+        schematic = build_random_shape(self.agent)
+        # Assert that some non-zero size schematic was built
+        self.assertTrue(len(schematic) > 0)
+        # Assert that build task was pushed and completed successfully
+        self.assertTrue(self.agent.memory.get_last_finished_root_task().action_name, "build")
+
+    def test_come_to_player(self):
+        come_to_player(self.agent)
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
