8d9c60ed5aa403a36dfdc2345400f55613033699
Yuxuan Sun <yuxuans@fb.com>
2021-06-11 04:34:00 -0700

    Added new entry point for dashboard in turk

--- droidlet/dashboard/web/config/webpackDevServer.config.js
+++ droidlet/dashboard/web/config/webpackDevServer.config.js
@@ -100,6 +100,7 @@ module.exports = function(proxy, allowedHost) {
       verbose: true,
       rewrites: [
         {from: /^\/mobile/, to: '/mobile.html'},
+        {from: /^\/turk/, to: '/turk.html'},
       ],
       index: paths.publicUrlOrPath,
     },
