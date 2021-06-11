c0df55adbbb13e49ffb2129b1ee056dc05737e2a
Kavya Srinet <kavyasrinet@fb.com>
2021-06-10 18:43:16 -0700

    Adding back safety test
    fix test

--- droidlet/dialog/tests/test_safety.py
+++ droidlet/dialog/tests/test_safety.py
@@ -2,37 +2,34 @@
 Copyright (c) Facebook, Inc. and its affiliates.
 """
 
-import os
 import unittest
-
-# from droidlet.dialog.map_to_dialogue_object import is_safe
-from droidlet.perception.semantic_parsing.droidlet_nsp_model_wrapper import DroidletNSPModelWrapper
+import os
 from droidlet.shared_data_structs import MockOpt
+from droidlet.dialog.map_to_dialogue_object import DialogueObjectMapper
 
-TTAD_BERT_DATA_DIR = os.path.join(os.path.dirname(__file__), "../../../../agents/craftassist/datasets/annotated_data/")
-TTAD_BERT_MODEL_DIR = os.path.join(os.path.dirname(__file__), "../../../../agents/craftassist/models/semantic_parser/")
+GROUND_TRUTH_DATA_DIR = os.path.join(os.path.dirname(__file__), "../../../agents/craftassist/datasets/ground_truth/")
 
 """This class tests safety checks using a preset list of blacklisted words.
 """
 
 
 class SafetyTest(unittest.TestCase):
-    pass
-    # def setUp(self):
-    #     opts = MockOpt()
-    #     opts.nsp_data_dir = TTAD_BERT_DATA_DIR
-    #     opts.nsp_models_dir = TTAD_BERT_MODEL_DIR
-    #     opts.no_ground_truth = False
-    #     self.chat_parser = DroidletNSPModelWrapper(opts)
-    #
-    #
-    # def test_unsafe_word(self):
-    #     is_safe = is_safe(self.chat_parser.safety_words, "bad Clinton")
-    #     self.assertFalse(is_safe)
-    #
-    # def test_safe_word(self):
-    #     is_safe = is_safe(self.chat_parser.safety_words, "build a house")
-    #     self.assertTrue(is_safe)
+    def setUp(self):
+        opts = MockOpt()
+        opts.ground_truth_data_dir = GROUND_TRUTH_DATA_DIR
+        self.dialogue_object_mapper = DialogueObjectMapper(
+            dialogue_object_classes={},
+            opts=opts,
+            dialogue_manager=None
+        )
+
+    def test_unsafe_word(self):
+        is_safe = self.dialogue_object_mapper.is_safe("bad Clinton")
+        self.assertFalse(is_safe)
+
+    def test_safe_word(self):
+        is_safe = self.dialogue_object_mapper.is_safe("build a house")
+        self.assertTrue(is_safe)
 
 
 if __name__ == "__main__":
