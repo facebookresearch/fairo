7141115e9063597f2a3f84ee460a07ff951cd5f0
Rebecca-Qian <rebeccaqian@fb.com>
2021-06-17 00:37:16 -0700

    only run tasks if the input is prepared

--- tools/annotation_tools/turk_with_s3/run_all_tasks.py
+++ tools/annotation_tools/turk_with_s3/run_all_tasks.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import sys
+import os
 
 """
 Kicks off a pipeline that schedules Turk jobs for tool 1A,
@@ -23,24 +24,33 @@ if rc != 0:
     print("Error preprocessing. Exiting.")
     sys.exit()
 
-# Load input commands and create a separate HIT for each row
-rc = subprocess.call(["python3 run_tool_1B.py"], shell=True)
-if rc != 0:
-    print("Error creating HIT jobs. Exiting.")
-    sys.exit()
-# Wait for results to be ready
-print("Turk jobs created at : %s \n Waiting for results..." % time.ctime())
+# If the tool B input file is not empty, kick off a job
+filesize = os.path.getsize("B/input.txt")
+if filesize > 0:
+    # Load input commands and create a separate HIT for each row
+    rc = subprocess.call(["python3 run_tool_1B.py"], shell=True)
+    if rc != 0:
+        print("Error creating HIT jobs. Exiting.")
+        sys.exit()
+    # Wait for results to be ready
+    print("Turk jobs created at : %s \n Waiting for results..." % time.ctime())
 
 time.sleep(100)
-# Check if results are ready
-rc = subprocess.call(["python3 run_tool_1C.py"], shell=True)
-if rc != 0:
-    print("Error fetching HIT results. Exiting.")
-    sys.exit()
+# If the tool C input file is not empty, kick off a job
+filesize = os.path.getsize("C/input.txt")
+if filesize > 0:
+    # Check if results are ready
+    rc = subprocess.call(["python3 run_tool_1C.py"], shell=True)
+    if rc != 0:
+        print("Error fetching HIT results. Exiting.")
+        sys.exit()
 
-# Collate datasets
-print("*** Collating turk outputs and input job specs ***")
-rc = subprocess.call(["python3 run_tool_1D.py"], shell=True)
-if rc != 0:
-    print("Error collating answers. Exiting.")
-    sys.exit()
+# If the tool B input file is not empty, kick off a job
+filesize = os.path.getsize("D/input.txt")
+if filesize > 0:
+    # Collate datasets
+    print("*** Collating turk outputs and input job specs ***")
+    rc = subprocess.call(["python3 run_tool_1D.py"], shell=True)
+    if rc != 0:
+        print("Error collating answers. Exiting.")
+        sys.exit()
