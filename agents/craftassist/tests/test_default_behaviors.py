04ead88e1a6a4dd4322efff23e0fba3e48925583
Arthur Szlam <aszlam@fb.com>
2021-06-19 21:48:22 -0700

    flush longer, some of the default builds are big

--- agents/craftassist/tests/test_default_behaviors.py
+++ agents/craftassist/tests/test_default_behaviors.py
@@ -12,7 +12,7 @@ class TestDefaultBehavior(BaseCraftassistTestCase):
         schematic = build_random_shape(self.agent)
         # Assert that some non-zero size schematic was built
         self.assertTrue(len(schematic) > 0)
-        changes = self.flush(500)
+        changes = self.flush(10000)
         # Assert that build task was pushed and completed successfully
         self.assertTrue(self.agent.memory.get_last_finished_root_task().action_name, "build")
         self.assertTrue(len(changes) == len(schematic))
