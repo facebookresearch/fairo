a4d6279e5e438ccb66d389827519f2ceaed0219f
Hollis Ma <hollism@princeton.edu>
2021-06-23 17:07:00 -0400

    Hollis/correct labels squash (#517)
    Revamped annotation tool
    
    Toolbox: 
    * Create new masks within an object mask
    * Delete masks within an object mask
    * Insert points into an existing mask
    * Remove points from an existing mask
    * Zoom in and out
    * Save the edited mask
    QoL changes: 
    * Keyboard shortcuts for all toolbox functionalities
    * "Focusing" on certain masks to make them easier to edit
    * Revamped color scheme that's consistent with the object detection feed
    * Color-coded list of labels, allowing users to find and focus on certain masks
    * More intuitive flow for adding new masks
    * Dynamically capping points in detected masks

--- droidlet/dashboard/web/src/components/AnnotationComponents/DataEntry.jsx
+++ droidlet/dashboard/web/src/components/AnnotationComponents/DataEntry.jsx
@@ -7,19 +7,28 @@ import TagSelector from "./TagSelector";
 
 /*
 Props:
-draw_location: 
-    where to draw the overlay
-    {x: x location, y: y location}
 
-objectId:
-    Object indentifier (int)
+x (number): 
+  x-coodinate for where to draw the overlay
+y (number): 
+  y-coodinate for where to draw the overlay
+label: (string): 
+  object label name
+tags ([string]): 
+  text tags to describe the object
+includeSubmitButton (bool): 
+  whether or not to include a submit button (only for new objects)
+deleteCallback (func): 
+  callback to delete the object
+onSubmit (func): 
+  callback to update label name and tags of object
 */
 class DataEntry extends React.Component {
   constructor(props) {
     super(props);
 
-    this.labels = [];
-    this.tags = [];
+    this.tags = this.props.tags || [];
+    this.submit = this.submit.bind(this);
 
     this.nameRef = React.createRef();
   }
@@ -51,11 +60,25 @@ class DataEntry extends React.Component {
           position: "fixed",
         }}
       >
-        <input placeholder="Object Name" ref={this.nameRef}></input>
-        <TagSelector tags={this.labels} update={(tags) => (this.tags = tags)} />
-        <button className="data-entry-submit" onClick={this.submit.bind(this)}>
-          Submit
-        </button>
+        <input
+          placeholder="Object Name"
+          ref={this.nameRef}
+          defaultValue={this.props.label || ""}
+        />
+        <TagSelector tags={this.tags} update={(tags) => (this.tags = tags)} />
+        {this.props.includeSubmitButton ? (
+          <button className="data-entry-submit" onClick={this.submit}>
+            Submit
+          </button>
+        ) : null}
+        {this.props.deleteCallback ? (
+          <button
+            className="data-entry-delete"
+            onClick={this.props.deleteCallback}
+          >
+            Delete object (⌫)
+          </button>
+        ) : null}
       </div>
     );
   }
