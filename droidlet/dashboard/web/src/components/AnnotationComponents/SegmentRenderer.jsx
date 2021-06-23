631f3241c915bd3bfeb9eb7eb13ed27436c16cbe
Hollis Ma <hollisma@fb.com>
2021-06-23 06:18:30 -0700

    revamped annotation tool

--- droidlet/dashboard/web/src/components/AnnotationComponents/SegmentRenderer.jsx
+++ droidlet/dashboard/web/src/components/AnnotationComponents/SegmentRenderer.jsx
@@ -13,15 +13,14 @@ import React from "react";
 
 img (Javascript Image Object)
     Image to draw segments on
-
 objects (List of object names to draw [keys to pointMap])
-
 pointMap (Map from object names to arrays of points)
     Defines the borders of objects to be drawn
-
 colors (List of colors to use to draw the segments)
     list of colors to use. If the length of the colors array is less than 
     the objects to be drawn, colors will be recycled (harder to read)
+onClick (func)
+    Handle click by starting a new polygon or displaying the clicked one
 
 */
 
@@ -31,6 +30,8 @@ class SegmentRenderer extends React.Component {
 
     this.update = this.update.bind(this);
     this.drawLine = this.drawLine.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.regions = {}; // array of mask sets
 
     this.canvasRef = React.createRef();
     this.imgRef = React.createRef();
@@ -61,7 +62,6 @@ class SegmentRenderer extends React.Component {
           width={imageSize}
           height={imageSize}
           tabIndex="0"
-          onClick={this.props.onClick}
           onMouseMove={this.onMouseMove}
           onKeyDown={this.keyDown}
         ></canvas>
@@ -70,7 +70,7 @@ class SegmentRenderer extends React.Component {
   }
 
   update() {
-    //clear and transform
+    // Draw image scaled and repostioned
     this.ctx.resetTransform();
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.setTransform(
@@ -81,20 +81,49 @@ class SegmentRenderer extends React.Component {
       this.Offset.x,
       this.Offset.y
     );
-    //Draw image scaled and repostioned
     this.ctx.drawImage(this.props.img, 0, 0);
-    //Draw segments
+
+    // Draw regions
     for (let i = 0; i < this.props.objects.length; i++) {
-      let pts = this.props.pointMap[this.props.objects[i]];
-      if (pts) {
-        let color = this.props.colors[i % this.props.colors.length];
-        if (color) {
-          this.drawRegion(pts, color);
-        } else {
-          this.drawRegion(pts, "rgba(0,200,0,.5)");
+      let curId = this.props.objects[i];
+      let pts_arr = this.props.pointMap[curId];
+      if (pts_arr.length === 0) {
+        continue;
+      }
+
+      this.regions[curId] = [];
+      let color =
+        this.props.colors[i % this.props.colors.length] || "rgba(0,200,0,.5)";
+      // Go through masks in label
+      for (let j in pts_arr) {
+        // Denormalize points
+        let points = pts_arr[j].map((pt) => ({
+          x: pt.x * this.canvas.width,
+          y: pt.y * this.canvas.height,
+        }));
+        let region = this.drawRegion(points, color);
+        this.regions[curId].push(region);
+      }
+    }
+
+    this.canvas.addEventListener("mousedown", this.onClick);
+  }
+
+  onClick(e) {
+    // Run through regions and if click is in a region, display only that region
+    let regionId = -1;
+    for (let i in this.regions) {
+      for (let j in this.regions[i]) {
+        if (
+          this.regions[i][j] &&
+          this.ctx.isPointInPath(this.regions[i][j], e.offsetX, e.offsetY)
+        ) {
+          regionId = i;
         }
       }
     }
+    let regionFound = regionId !== -1;
+    this.props.onClick(e.clientX, e.clientY, regionFound, regionId);
   }
 
   drawLine(pt1, pt2) {
@@ -116,6 +145,11 @@ class SegmentRenderer extends React.Component {
     region.closePath();
     this.ctx.fillStyle = color;
     this.ctx.fill(region, "evenodd");
+    return region;
+  }
+
+  getCanvasBoundingBox() {
+    return this.canvas.getBoundingClientRect();
   }
 }
 
