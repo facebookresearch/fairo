fe708c432d6981823721970016c764a521679c69
Yuxuan Sun <34047968+snyxan@users.noreply.github.com>
2021-06-30 19:56:18 -0700

    Fixed servermgr issues introduced by refactor (#544)
    * Added script to allocate ecs instances programatically
    
    * Update paths in servermgr agent start script
    
    * Fixed more path issue
    
    * More path fix
    
    * Another fix
    
    * Debug
    
    * Debug 2
    
    * Sync servermgr code with droidlet subtree

--- droidlet/tools/servermgr/run.withagent.sh
+++ droidlet/tools/servermgr/run.withagent.sh
@@ -6,11 +6,16 @@
 S3_DEST=s3://craftassist/turk_interactions_with_agent
 
 function background_agent() (
-    python3 /droidlet/craftassist/wait_for_cuberite.py --host localhost --port 25565
-    python3 /droidlet/craftassist/agent/craftassist_agent.py --no_default_behavior --dev 1>agent.log 2>agent.log
+    echo "Running craftassist agent"
+    python3 /droidlet/droidlet/lowlevel/minecraft/craftassist_cuberite_utils/wait_for_cuberite.py --host localhost --port 25565
+    python3 /droidlet/agents/craftassist/craftassist_agent.py --no_default_behavior --dev 1>agent.log 2>agent.log
 )
 
-python3 /droidlet/craftassist/cuberite_process.py \
+echo "Installing Droidlet as a module"
+
+cd /droidlet && python3 setup.py develop && cd /
+
+python3 /droidlet/droidlet/lowlevel/minecraft/cuberite_process.py \
     --mode creative \
     --workdir . \
     --config flat_world \
@@ -36,6 +41,7 @@ TARBALL=logs.tar.gz
 find -name "*.log" -o -name "*.csv" -o -name "job_metadata.json"|tar czf $TARBALL --force-local -T -
 
 if [ -z "$CRAFTASSIST_NO_UPLOAD" ]; then
+    echo "Uploading data to S3"
     # expects $AWS_ACCESS_KEY_ID and $AWS_SECRET_ACCESS_KEY to exist
     aws s3 cp $TARBALL $S3_DEST/$TARBALL
 fi
