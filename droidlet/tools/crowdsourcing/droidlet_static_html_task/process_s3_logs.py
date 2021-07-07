e1f27ad9cc42aa7cdc513b27afe2f72e48a4f3f2
Rebecca-Qian <rebeccaqian@fb.com>
2021-07-07 16:24:42 -0700

    read S3 bucket

--- droidlet/tools/crowdsourcing/droidlet_static_html_task/process_s3_logs.py
+++ droidlet/tools/crowdsourcing/droidlet_static_html_task/process_s3_logs.py
@@ -7,8 +7,31 @@ import pandas as pd
 import re
 from datetime import datetime
 import argparse
+import boto3
+import os
 
 pd.set_option('display.max_rows', 10)
+
+def read_s3_bucket(s3_logs_dir):
+    print('{s3_logs_dir}/**/{csv_filename}'.format(s3_logs_dir=s3_logs_dir, csv_filename='logs.tar.gz'))
+    for csv_path in glob.glob('{s3_logs_dir}/**/{csv_filename}'.format(s3_logs_dir=s3_logs_dir, csv_filename='logs.tar.gz')):
+        print(csv_path)
+#     # session = boto3.Session(profile_name=profile_name)
+#     # s3 = session.client('s3')
+#     # bucket_name, key = mpu.aws._s3_path_split(source)
+#     # s3_object = s3.get_object(Bucket=bucket_name, Key=key)
+#     # body = s3_object['Body']
+#     s3 = boto3.client('s3',
+#         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+#         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+#     )
+#     # print("Set up boto3 s3 client, attempting to download internal resources.")
+
+#     s3.download_file(
+#         'craftassist/turk_interactions_with_agent/turk',
+#         'private.txt',
+#         'private.txt'
+#     )
 
 def read_turk_logs(turk_logs_directory, turk_output_directory, filename):
     # Crawl turk logs directory
@@ -43,6 +66,6 @@ if __name__ == "__main__":
     parser.add_argument('--turk_output_directory', help='where to write the collated NSP outputs')
     parser.add_argument('--filename', help='name of the CSV file we want to read, eg. nsp_outputs')
     args = parser.parse_args()
-
-    read_turk_logs(args.turk_logs_directory, args.turk_output_directory, args.filename)
+    read_s3_bucket(args.turk_logs_directory)
+    # read_turk_logs(args.turk_logs_directory, args.turk_output_directory, args.filename)
     
