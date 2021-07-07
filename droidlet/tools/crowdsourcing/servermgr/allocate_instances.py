837c4c607323d4e8e40c15de8a80601acba58489
Rebecca Qian <rebeccaqian@fb.com>
2021-07-07 16:47:36 -0700

    [Dashboard in Turk] Fully Automate Dashboard Instance Pool set up (#557)
    * Register IP instances on cloudflare
    
    * Write the allocated instances info to input CSV for Mephisto
    
    * Remove hard coded IPs
    
    * Add Heroku params to onboarding flow
    
    * Make cloudflare email configurable
    
    * Add exception handling to cloudflare post request

--- droidlet/tools/crowdsourcing/servermgr/allocate_instances.py
+++ droidlet/tools/crowdsourcing/servermgr/allocate_instances.py
@@ -11,6 +11,8 @@ import os
 import random
 import socket
 import time
+import CloudFlare
+import csv
 
 from base64 import b64encode, b64decode
 from datetime import datetime, timezone
@@ -146,8 +148,56 @@ def request_instance(instance_num):
     logging.info(f"All {instance_num} are up, ip list: {instance_ips}")
     return instance_ips
 
+
+def register_dashboard_subdomain(cf, zone_id, ip, subdomain):
+    """Registers a unique subdomain for craftassist.io
+    that serves proxied HTTP content using cloudflare.
+    Args:
+    cf -- CloudFlare context with R/W permissions.
+    zone_id -- zone ID used to locate DNS records.
+    ip -- IP of the ECS container that runs dashboard.
+    subdomain -- subdomain contains a unique identifier for this task run, 
+    which is the batch ID concatenated with the run number.
+    """
+    # Check that DNS record does not already exist
+    dns_record_exists = cf.zones.dns_records.get(
+        zone_id, params={"name": "{}.craftassist.io".format(subdomain)}
+    )
+    if dns_record_exists:
+        print("DNS record already exists for {}".format(subdomain))
+        return
+
+    dns_record = {"name": subdomain, "type": "A", "content": ip, "proxied": True}
+    try:
+        r = cf.zones.dns_records.post(zone_id, data=dns_record)   
+        print("Registered IP {} at subdomain {}".format(ip, subdomain))
+    except Exception as e:
+        raise e 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--instance_num", type=int, default=1, help="number of instances requested")
+    parser.add_argument("--batch_id", type=int, default=0, help="ID of the current batch, used to track which group of runs the task was run in")
+    parser.add_argument("--user", type=str, default="rebeccaqian@fb.com", help="Email of the CloudFlare account")
     args = parser.parse_args()
     instance_ips = request_instance(args.instance_num)
+    # register subdomain to proxy instance IP
+    if os.getenv("CLOUDFLARE_TOKEN") and os.getenv("CLOUDFLARE_ZONE_ID"):
+        logging.info("registering subdomains on craftassist.io")
+        cloudflare_token = os.getenv("CLOUDFLARE_TOKEN")
+        zone_id = os.getenv("CLOUDFLARE_ZONE_ID")
+        cf = CloudFlare.CloudFlare(email=args.user, token=cloudflare_token)
+        dns_records = cf.zones.dns_records.get(zone_id)
+
+        # Write the subdomains and batch IDs to input CSV for Mephisto
+        # CSV file headers
+        headers = ["subdomain", "batch"]
+        with open("../droidlet_static_html_task/data.csv", "w") as fd:
+            csv_writer = csv.writer(fd, delimiter=",")
+            csv_writer.writerow(headers)
+            for x in range(len(instance_ips)):
+                ip = instance_ips[x]
+                subdomain = "dashboard-{}-{}".format(args.batch_id, x)
+                register_dashboard_subdomain(cf, zone_id, ip, subdomain)
+                # Write record to Mephisto task input CSV
+                csv_writer.writerow([subdomain, args.batch_id])
