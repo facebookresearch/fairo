2614b73fbd2b8d093aec376a72149c17e8a05d55
Jerry Han <jhan6501@devfair025.maas>
2021-07-07 14:40:43 -0700

    finihsed bash script

--- droidlet/tools/crowdsourcing/servermgr/run.locobotagent.sh
+++ droidlet/tools/crowdsourcing/servermgr/run.locobotagent.sh
@@ -5,22 +5,10 @@ S3_DEST=s3://craftassist/turk_interactions_with_agent
 
 function background_agent() (
     echo "Running locobot agent"
-    export LOCOBOT_IP="172.17.0.2"
-    ls
-    conda init bash
-    conda activate droidlet_env
     python3 agents/locobot/locobot_agent.py --no_default_behavior
 )
 
 echo "Installing Droidlet as a module"
-ls
-cd droidlet
-# conda create -n droidlet_env python=3.7 \
-#    pytorch==1.7.1 torchvision==0.8.2 \
-#    cudatoolkit=11.0 -c pytorch
-conda activate droidlet_env
-# pip install -r \
-#     agents/locobot/requirements.txt
 cd /droidlet && python3 setup.py develop && cd ..
 ls
 
