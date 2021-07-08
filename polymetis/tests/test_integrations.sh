825ea8e23fdd3cc48dda9db8e07ccb411f9dafe9
ExhAustin <aswang@andrew.cmu.edu>
2021-07-08 16:33:56 -0700

    Add a mocked hardware test to the integration tests

--- polymetis/tests/test_integrations.sh
+++ polymetis/tests/test_integrations.sh
@@ -8,6 +8,9 @@
 set -eo pipefail
 trap "kill -- -$$" ERR # cleanup all child processes if error
 
+######################
+# Test empty statistics client
+######################
 echo "=== Starting server and testing empty statistics client... ==="
 launch_robot.py robot_client=empty_statistics_client use_real_time=false num_requests=5000 &
 server_pid=$!
@@ -22,8 +25,11 @@ fi
 
 echo "=== Success. ==="
 
-echo "=== Starting server and franka simulation...==="
+######################
+# Test simulation client
+######################
 # Start server & robot client
+echo "=== Starting server and franka simulation...==="
 launch_robot.py robot_client=franka_sim gui=false use_real_time=false &
 server_pid=$!
 echo "=== Server PID: $server_pid ==="
@@ -56,3 +62,31 @@ then
     echo "Killing server_pid $server_pid"
     kill $server_pid
 fi
+
+######################
+# Test mocked hardware client
+######################
+# Start server & robot client
+echo "=== Starting server and mocked franka hardware client... ==="
+launch_robot.py robot_client=franka_hardware use_real_time=false robot_client.executable_cfg.mock=true &
+server_pid=$!
+echo "=== Server PID: $server_pid ==="
+
+sleep 4
+
+# Run examples
+echo "=== Running controllers in examples/ ... ==="
+for entry in "examples"/*; do
+    if [ ${entry: -3} == ".py" ]; then
+        echo $entry
+        python $entry
+    fi
+done
+
+# Kill server
+if ps -p $server_pid > /dev/null
+then
+    kill $server_pid
+fi
+
+echo "=== Success. ==="
