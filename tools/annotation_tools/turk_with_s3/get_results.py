f82e7e8c719d54cde15d8e74d46ca3977e23959e
Rebecca-Qian <rebeccaqian@fb.com>
2021-07-12 01:17:41 -0700

    Add argparse to get results script

--- tools/annotation_tools/turk_with_s3/get_results.py
+++ tools/annotation_tools/turk_with_s3/get_results.py
@@ -4,6 +4,7 @@ import xmltodict
 import os
 import sys
 import time
+import argparse
 
 access_key = os.getenv("AWS_ACCESS_KEY_ID")
 secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -45,72 +46,80 @@ def get_hit_list_status(mturk):
     return hit_status
 
 
-# This will contain the answers
-# delete_hits(mturk)
-# print("deleted all HITs")
-if os.path.exists("turk_output.csv"):
-    res = pd.read_csv("turk_output.csv")
-else:
-    res = pd.DataFrame()
-NUM_TRIES_REMAINING = 5
-curr_hit_status = get_hit_list_status(mturk)
+def get_results(output_csv: str):
+    # This will contain the answers
+    # delete_hits(mturk)
+    # print("deleted all HITs")
+    if os.path.exists(output_csv):
+        res = pd.read_csv(output_csv)
+    else:
+        res = pd.DataFrame()
+    NUM_TRIES_REMAINING = 5
+    curr_hit_status = get_hit_list_status(mturk)
 
-while curr_hit_status["assignable"] or curr_hit_status["reviewable"]:
-    if NUM_TRIES_REMAINING == 0:
-        break
-    print("*** Fetching results ***".format(NUM_TRIES_REMAINING))
-    # get reviewable hits, ie hits that have been completed
-    # hits = [x['HITId'] for x in mturk.list_reviewable_hits()['HITs']]
-    # If there are no reviewable HITs currently, wait 2 mins in between tries.
-    if len(curr_hit_status["reviewable"]) == 0:
-        NUM_TRIES_REMAINING -= 1
-        time.sleep(30)
-        curr_hit_status = get_hit_list_status(mturk)
-        continue
+    while curr_hit_status["assignable"] or curr_hit_status["reviewable"]:
+        if NUM_TRIES_REMAINING == 0:
+            break
+        print("*** Fetching results ***".format(NUM_TRIES_REMAINING))
+        # get reviewable hits, ie hits that have been completed
+        # hits = [x['HITId'] for x in mturk.list_reviewable_hits()['HITs']]
+        # If there are no reviewable HITs currently, wait 2 mins in between tries.
+        if len(curr_hit_status["reviewable"]) == 0:
+            NUM_TRIES_REMAINING -= 1
+            time.sleep(30)
+            curr_hit_status = get_hit_list_status(mturk)
+            continue
 
-    # If there are no assignable or reviewable HITs, the job is done!
-    if len(curr_hit_status["reviewable"]) == 0 and len(curr_hit_status["assignable"]) == 0:
-        print("*** No HITs pending or awaiting review. Exiting.")
-        sys.exit()
+        # If there are no assignable or reviewable HITs, the job is done!
+        if len(curr_hit_status["reviewable"]) == 0 and len(curr_hit_status["assignable"]) == 0:
+            print("*** No HITs pending or awaiting review. Exiting.")
+            sys.exit()
 
-    # Parse responses from each reviewable HIT
-    for hit_id in curr_hit_status["reviewable"]:
-        new_row = {"HITId": hit_id}
-        worker_results = mturk.list_assignments_for_hit(
-            HITId=hit_id, AssignmentStatuses=["Submitted"]
-        )
-        if worker_results["NumResults"] > 0:
-            for assignment in worker_results["Assignments"]:
-                new_row["WorkerId"] = assignment["WorkerId"]
-                xml_doc = xmltodict.parse(assignment["Answer"])
+        # Parse responses from each reviewable HIT
+        for hit_id in curr_hit_status["reviewable"]:
+            new_row = {"HITId": hit_id}
+            worker_results = mturk.list_assignments_for_hit(
+                HITId=hit_id, AssignmentStatuses=["Submitted"]
+            )
+            if worker_results["NumResults"] > 0:
+                for assignment in worker_results["Assignments"]:
+                    new_row["WorkerId"] = assignment["WorkerId"]
+                    xml_doc = xmltodict.parse(assignment["Answer"])
 
-                print("Worker's answer was:")
-                if type(xml_doc["QuestionFormAnswers"]["Answer"]) is list:
-                    # Multiple fields in HIT layout
-                    for answer_field in xml_doc["QuestionFormAnswers"]["Answer"]:
-                        input_field = answer_field["QuestionIdentifier"]
-                        answer = answer_field["FreeText"]
+                    print("Worker's answer was:")
+                    if type(xml_doc["QuestionFormAnswers"]["Answer"]) is list:
+                        # Multiple fields in HIT layout
+                        for answer_field in xml_doc["QuestionFormAnswers"]["Answer"]:
+                            input_field = answer_field["QuestionIdentifier"]
+                            answer = answer_field["FreeText"]
+                            print("For input field: " + input_field)
+                            print("Submitted answer: " + answer)
+                            new_row["Answer.{}".format(input_field)] = answer
+
+                        res = res.append(new_row, ignore_index=True)
+                        res.to_csv(output_csv, index=False)
+                    else:
+                        # One field found in HIT layout
+                        answer = xml_doc["QuestionFormAnswers"]["Answer"]["FreeText"]
+                        input_field = xml_doc["QuestionFormAnswers"]["Answer"]["QuestionIdentifier"]
                         print("For input field: " + input_field)
                         print("Submitted answer: " + answer)
                         new_row["Answer.{}".format(input_field)] = answer
+                        res = res.append(new_row, ignore_index=True)
+                        res.to_csv(output_csv, index=False)
 
-                    res = res.append(new_row, ignore_index=True)
-                    res.to_csv("turk_output.csv", index=False)
-                else:
-                    # One field found in HIT layout
-                    answer = xml_doc["QuestionFormAnswers"]["Answer"]["FreeText"]
-                    input_field = xml_doc["QuestionFormAnswers"]["Answer"]["QuestionIdentifier"]
-                    print("For input field: " + input_field)
-                    print("Submitted answer: " + answer)
-                    new_row["Answer.{}".format(input_field)] = answer
-                    res = res.append(new_row, ignore_index=True)
-                    res.to_csv("turk_output.csv", index=False)
-
-                mturk.approve_assignment(AssignmentId=assignment["AssignmentId"])
+                    mturk.approve_assignment(AssignmentId=assignment["AssignmentId"])
+                    mturk.delete_hit(HITId=hit_id)
+            else:
+                print("No results ready yet")
+                # if returned assignment is empty,reject
                 mturk.delete_hit(HITId=hit_id)
-        else:
-            print("No results ready yet")
-            # if returned assignment is empty,reject
-            mturk.delete_hit(HITId=hit_id)
-    curr_hit_status = get_hit_list_status(mturk)
-    print(curr_hit_status)
+        curr_hit_status = get_hit_list_status(mturk)
+        print(curr_hit_status)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_csv", type=str, required=True)
+
+    args = parser.parse_args()
+    get_results(args.output_csv)
