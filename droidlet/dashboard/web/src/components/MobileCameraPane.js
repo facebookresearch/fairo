2d9fa36584ce1ee416d65da43b5de1575e31e91d
Jerry Han <jhan6501@devfair025.maas>
2021-07-14 11:40:42 -0700

    added logic to switch between user and environment camera if environment camera does not exist

--- droidlet/dashboard/web/src/components/MobileCameraPane.js
+++ droidlet/dashboard/web/src/components/MobileCameraPane.js
@@ -9,12 +9,19 @@ class MobileCameraPane extends React.Component {
     this.state = {
       currentMode: "camera",
       img: null,
+      videoConstraints: {
+        facingMode: { exact: "environment" },
+        height: this.props.imageWidth,
+        width: this.props.imageWidth,
+      },
     };
     this.webcamRef = React.createRef();
   }
 
   screenshot() {
     var screenshot = this.webcamRef.current.getScreenshot();
+    console.log("screenshot");
+    console.log(screenshot);
     let asImage = new Image(this.props.imageWidth, this.props.imageWidth);
     asImage.src = screenshot;
     this.setState({
@@ -23,20 +30,34 @@ class MobileCameraPane extends React.Component {
     });
   }
 
-  render() {
-    if (this.state.currentMode === "camera") {
-      const videoConstraints = {
-        // facingMode: { exact: "environment" },
-        height: this.props.imageWidth,
-        width: this.props.imageWidth,
-      };
+  componentDidMount() {
+    console.log("post mount");
+    if (this.webcamRef.current) {
+      // if device has no environment camera, use the selfie camera
+      if (!this.webcamRef.current.getScreenshot()) {
+        console.log("switching camera");
+        this.setState({
+          videoConstraints: {
+            facingMode: "user",
+            height: this.props.imageWidth,
+            width: this.props.imageWidth,
+          },
+        });
+        console.log("this.state");
+        console.log(this.state);
+      }
+    }
+  }
 
+  render() {
+    console.log("camera render");
+    if (this.state.currentMode === "camera") {
       return (
         <div>
           <Webcam
             height={this.props.imageWidth}
             width={this.props.imageWidth}
-            videoConstraints={videoConstraints}
+            videoConstraints={this.state.videoConstraints}
             ref={this.webcamRef}
           />
           <button onClick={this.screenshot.bind(this)}> Capture </button>
