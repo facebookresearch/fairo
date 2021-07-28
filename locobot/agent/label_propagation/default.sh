e32a19596a1f91356494849f6a0b3f0d887bc77a
Anurag <apratik@fb.com>
2021-07-28 11:44:46 -0700

    nits

--- locobot/agent/label_propagation/default.sh
+++ locobot/agent/label_propagation/default.sh
@@ -3,12 +3,12 @@ export EXPPREFIX=_h1
 echo $SCENE_ROOT 
 
 export FRAMES=5
-for scene in straightline default
+for heu in straightline default
 do 
     for p in 2 4 6 8
     do 
-        export OUTDIR=/checkpoint/apratik/finals/$scene/apartment_0/pred_label_gt5p$p$EXPPREFIX
+        export OUTDIR=/checkpoint/apratik/finals/noisy/$heu/apartment_0/pred_label_gt5p$p$EXPPREFIX
         echo $OUTDIR
-        python label_propagation.py --scene_path /checkpoint/apratik/finals/$scene --gtframes $FRAMES --propogation_step $p --out_dir $OUTDIR
+        python label_propagation.py --scene_path /checkpoint/apratik/finals/noisy/$heu --gtframes $FRAMES --propogation_step $p --out_dir $OUTDIR
     done
 done
