999d744f89684e51f0864db117bd3d72dc480870
ExhAustin <aswang@andrew.cmu.edu>
2021-08-03 15:38:07 -0700

    Update test

--- polymetis/tests/scripts/1_ee_pose.py
+++ polymetis/tests/scripts/1_ee_pose.py
@@ -19,7 +19,7 @@ def test_new_ee_pose(robot, ee_pos_desired, ee_quat_desired):
     assert torch.allclose(ee_pos, ee_pos_desired, atol=0.005)
     assert (
         R.from_quat(ee_quat).inv() * R.from_quat(ee_quat_desired)
-    ).magnitude() < 0.0175  # 1 degree
+    ).magnitude() < 0.0174  # 1 degree
 
     return ee_pos, ee_quat
 
@@ -42,34 +42,43 @@ if __name__ == "__main__":
     print("=== RobotInterface.set_ee_pose ===")
     ee_pos_desired = ee_pos + torch.Tensor([0.0, 0.05, -0.05])
     ee_quat_desired = torch.Tensor([1, 0, 0, 0])  # pointing straight down
+    time_to_go = 4.0
 
-    state_log = robot.set_ee_pose(ee_pos_desired, ee_quat_desired)
+    state_log = robot.set_ee_pose(
+        ee_pos_desired, ee_quat_desired, time_to_go=time_to_go
+    )
     time.sleep(0.5)
 
     ee_pos, ee_quat = test_new_ee_pose(robot, ee_pos_desired, ee_quat_desired)
-    check_episode_log(state_log, int(robot.time_to_go_default * hz))
+    check_episode_log(state_log, int(time_to_go * hz))
 
-    # Go to ee_pose (larger orientation movement)
+    # Go to ee_pose (larger movement)
     print("=== RobotInterface.set_ee_pose ===")
-    ee_pos_desired = ee_pos + torch.Tensor([0.0, 0.05, -0.05])
+    ee_pos_desired = ee_pos + torch.Tensor([0.1, -0.1, 0.2])
     ee_quat_desired = torch.Tensor(
         [0.7071, 0, 0, -0.7071]
     )  # rotate by 90 degrees around x axis
+    time_to_go = 6.0
 
-    state_log = robot.set_ee_pose(ee_pos_desired, ee_quat_desired)
+    state_log = robot.set_ee_pose(
+        ee_pos_desired, ee_quat_desired, time_to_go=time_to_go
+    )
     time.sleep(0.5)
 
     ee_pos, ee_quat = test_new_ee_pose(robot, ee_pos_desired, ee_quat_desired)
-    check_episode_log(state_log, int(robot.time_to_go_default * hz))
+    check_episode_log(state_log, int(time_to_go * hz))
 
     # Move by delta ee pose
     print("=== RobotInterface.move_ee_xyz ===")
-    delta_ee_pos_desired = torch.Tensor([-0.05, 0.0, 0.05])
+    delta_ee_pos_desired = torch.Tensor([-0.1, 0.0, -0.1])
     ee_pos_desired = ee_pos + delta_ee_pos_desired
     ee_quat_desired = ee_quat
+    time_to_go = 6.0
 
-    state_log = robot.move_ee_xyz(delta_ee_pos_desired, use_orient=False)
+    state_log = robot.move_ee_xyz(
+        delta_ee_pos_desired, time_to_go=time_to_go, use_orient=False
+    )
     time.sleep(0.5)
 
     ee_pos, ee_quat = test_new_ee_pose(robot, ee_pos_desired, ee_quat_desired)
-    check_episode_log(state_log, int(robot.time_to_go_default * hz))
+    check_episode_log(state_log, int(time_to_go * hz))
