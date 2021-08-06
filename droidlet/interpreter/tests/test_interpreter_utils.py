dce14eceb7c411015d030596db04c7f6d67b1321
kavyasrinet <ksrinet@fb.com>
2021-08-06 13:55:04 -0700

    Adding test for validating location inside selectors

--- droidlet/interpreter/tests/test_interpreter_utils.py
+++ droidlet/interpreter/tests/test_interpreter_utils.py
@@ -9,6 +9,7 @@ from droidlet.perception.semantic_parsing.tests.test_y_print_parsing_report impo
     common_functional_commands,
     compare_full_dictionaries,
 )
+from all_test_commands import INTERPRETER_POSSIBLE_ACTIONS, FILTERS, REFERENCE_OBJECTS
 
 logical_form_before_processing = {
     "turn right": common_functional_commands["turn right"],
@@ -55,6 +56,23 @@ class TestProcessSpans(unittest.TestCase):
                 processed, original_words, lemmatized_words
             )  # process spans and fixed_values. Implemented in: interpreter_utils.
             assert compare_full_dictionaries(processed, logical_form_post_processing[k])
+
+class TestLocationInSelector(unittest.TestCase):
+    def check_location_in_filters(self, action_dict):
+        for key, value in action_dict.items():
+            if key == "filters" and "location" in value:
+                return False
+            elif type(value) == dict:
+                return self.check_location_in_filters(value)
+        return True
+
+    def test_location_reference_object(self):
+        all_dicts = INTERPRETER_POSSIBLE_ACTIONS
+        all_dicts.update(FILTERS)
+        all_dicts.update(REFERENCE_OBJECTS)
+        for key, action_dict in all_dicts.items():
+            self.assertTrue(self.check_location_in_filters(action_dict))
+
 
 
 if __name__ == "__main__":
