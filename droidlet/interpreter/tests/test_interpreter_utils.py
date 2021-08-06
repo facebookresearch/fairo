56d1273641f8f1c43c9c06417f892bfe686681b9
Kavya Srinet <ksrinet@fb.com>
2021-08-06 14:46:07 -0700

    Fix unit test

--- droidlet/interpreter/tests/test_interpreter_utils.py
+++ droidlet/interpreter/tests/test_interpreter_utils.py
@@ -9,7 +9,7 @@ from droidlet.perception.semantic_parsing.tests.test_y_print_parsing_report impo
     common_functional_commands,
     compare_full_dictionaries,
 )
-from all_test_commands import INTERPRETER_POSSIBLE_ACTIONS, FILTERS, REFERENCE_OBJECTS
+from .all_test_commands import INTERPRETER_POSSIBLE_ACTIONS, FILTERS, REFERENCE_OBJECTS
 
 logical_form_before_processing = {
     "turn right": common_functional_commands["turn right"],
@@ -46,7 +46,7 @@ logical_form_post_processing = {
 }
 
 
-class TestProcessSpans(unittest.TestCase):
+class TestInterpreterUtils(unittest.TestCase):
     def test_process_spans(self):
         for k, v in logical_form_before_processing.items():
             processed = deepcopy(v)
@@ -57,21 +57,20 @@ class TestProcessSpans(unittest.TestCase):
             )  # process spans and fixed_values. Implemented in: interpreter_utils.
             assert compare_full_dictionaries(processed, logical_form_post_processing[k])
 
-class TestLocationInSelector(unittest.TestCase):
-    def check_location_in_filters(self, action_dict):
-        for key, value in action_dict.items():
-            if key == "filters" and "location" in value:
-                return False
-            elif type(value) == dict:
-                return self.check_location_in_filters(value)
-        return True
-
     def test_location_reference_object(self):
+        def check_location_in_filters(action_dict):
+            for key, value in action_dict.items():
+                if key == "filters" and "location" in value:
+                    return False
+                elif type(value) == dict:
+                    return check_location_in_filters(value)
+            return True
+
         all_dicts = INTERPRETER_POSSIBLE_ACTIONS
         all_dicts.update(FILTERS)
         all_dicts.update(REFERENCE_OBJECTS)
         for key, action_dict in all_dicts.items():
-            self.assertTrue(self.check_location_in_filters(action_dict))
+            self.assertTrue(check_location_in_filters(action_dict))
 
 
 
