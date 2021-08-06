0bc66ce3b7a7a20104489e5268ab7ff50fcba0c0
kavyasrinet <ksrinet@fb.com>
2021-08-06 13:37:16 -0700

    remove convert_location_to_selector

--- droidlet/interpreter/__init__.py
+++ droidlet/interpreter/__init__.py
@@ -8,7 +8,6 @@ from .interpreter_utils import (
     tags_from_dict,
     strip_prefix,
     ref_obj_lf_to_selector,
-    convert_location_to_selector,
 )
 
 from .reference_object_helpers import (
@@ -34,7 +33,6 @@ __all__ = [
     SPEAKERPOS,
     AGENTPOS,
     ref_obj_lf_to_selector,
-    convert_location_to_selector,
     is_loc_speakerlook,
     coref_resolve,
     process_spans_and_remove_fixed_value,
