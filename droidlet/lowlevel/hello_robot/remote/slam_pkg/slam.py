[hello-remote] rotation fix, to not do needless rotation

droidlet/lowlevel/hello_robot/remote/slam_pkg/slam.py
@@ -30,7 +30,7 @@ class Slam(object):
         agent_min_z=5,
         agent_max_z=70,
         vis=False,
-        save_vis=False,
+        save_vis=True,
         save_folder="./slam_logs",
     ):
         """
@@ -94,6 +94,7 @@ class Slam(object):
         if self.save_vis:
             self.save_folder = os.path.join(save_folder, 'l' + str(int(time.time())))
             if not os.path.isdir(self.save_folder):
+                print(f'making save_folder {self.save_folder}')
                 os.makedirs(self.save_folder)
         self.start_vis = False
         self.vis_count = 0
@@ -173,12 +174,12 @@ class Slam(object):
         selem = disk(self.robot_rad / self.map_builder.resolution)
         traversable = binary_dilation(obstacle, selem) != True
 
-        """
+        
         # add robot collision map to traversable area
         unknown_region = self.map_builder.map.sum(axis=-1) < 1
         col_map_unknown = np.logical_and(self.col_map > 0.1, unknown_region)
         traversable = np.logical_and(traversable, np.logical_not(col_map_unknown))
-        """
+        
 
         # call the planner
         self.planner = FMMPlanner(
@@ -208,14 +209,14 @@ class Slam(object):
         print('orienting robot')
         delta = np.arctan2(
                     stg_real[1] - self.prev_bot_state[1], stg_real[0] - self.prev_bot_state[0]
-                ) % radians(360)
+                )
         print(f'delta in deg {degrees(delta), delta}, robot state {degrees(robot_state[2]), robot_state[2]}')
         exec = goto(
             self.robot._robot,
             (
                 0,
                 0,
-                (delta - (robot_state[2] % radians(360))) % radians(360),
+                delta - robot_state[2],
             ),
             dryrun=False
         )

droidlet/lowlevel/hello_robot/remote/utils.py
@@ -147,11 +147,11 @@ def goto(robot, xyt_position=None, translation_threshold=0.1, dryrun=False, dept
             xyt_position = [0.0, 0.0, 0.0]
         x = xyt_position[0]    # in meters
         y = xyt_position[1]    # in meters
-        rot = xyt_position[2] % radians(360)  # in radians
+        rot = xyt_position[2]  # in radians
 
         if sqrt(x * x + y * y) < translation_threshold:
             print("translation distance ", sqrt(x * x + y * y))
-            print("rotate by ", rot)
+            print(f'rotate by {xyt_position[2], rot}')
             if not dryrun:
                 robot.base.rotate_by(rot)
                 robot.push_command()
@@ -172,9 +172,10 @@ def goto(robot, xyt_position=None, translation_threshold=0.1, dryrun=False, dept
         theta_2 = -theta_1 + rot
 
         # first rotate by theta1
-        print("rotate by ", theta_1)
+        theta_1 = np.sign(theta_1) * (abs(theta_1) % radians(360))
+        print("rotate by theta_1", theta_1)
         if not dryrun:
-            robot.base.rotate_by(theta_1, v_r=0.1)
+            robot.base.rotate_by(theta_1)
             robot.push_command()
             time.sleep(0.2)
             while(abs(robot.base.left_wheel.status['vel']) >= 0.1 or abs(robot.base.right_wheel.status['vel']) >= 0.1):
@@ -196,7 +197,8 @@ def goto(robot, xyt_position=None, translation_threshold=0.1, dryrun=False, dept
             #     print(robot.base.left_wheel.status['vel'], robot.base.right_wheel.status['vel'])
             #     time.sleep(0.05)
         # second rotate by theta2
-        print("rotate by ", theta_2)
+        theta_2 = np.sign(theta_2) * (abs(theta_2) % radians(360))
+        print("rotate by theta_2", theta_2)
         if not dryrun:
             robot.base.rotate_by(theta_2)
             robot.push_command()