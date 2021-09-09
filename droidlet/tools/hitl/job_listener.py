175317f839cf1234562959f3f8f0373dbe81f35f
Yuxuan Sun <yuxuans@fb.com>
2021-09-09 11:36:58 -0700

    Implemented basic infrastructure for HiTL

---
+++ droidlet/tools/hitl/job_listener.py
@@ -0,0 +1,45 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+"""
+
+import logging
+import threading
+
+from typing import List
+
+from .data_generator import DataGenerator
+
+
+class JobListener:
+    def __init__(self):
+        self._finished = False
+        self._started = False
+        self._parent_jobs = []
+    
+    def run(self):
+        pass
+    
+    def start(self, *args):
+        if not self._started:
+            thread = threading.Thread(target=self.run, args=(args))
+            thread.daemon = True
+            thread.start()
+            self._started = True
+    
+    def set_finished(self, finished=True):
+        self._finished = finished
+
+    def check_is_finished(self) -> bool:
+        return self._finished
+
+    def check_parent_finished(self) -> bool:
+        finished = True
+        for job in self._parent_jobs:
+            if not job.check_is_finished():
+                finished = False
+        return finished
+
+    def add_parent_jobs(self, jobs: List[DataGenerator]):
+        self._parent_jobs.extend(jobs)
+
+    
---
+++ droidlet/tools/hitl/task_runner.py
@@ -0,0 +1,59 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+"""
+
+import logging
+import time
+
+from typing import List
+
+from .data_generator import DataGenerator
+from .job_listener import JobListener
+
+RUN_POLL_TIME = 5
+
+log_formatter = logging.Formatter(
+    "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() %(levelname)s]: %(message)s"
+)
+logger = logging.getLogger()
+logger.handlers.clear()
+logger.setLevel("INFO")
+sh = logging.StreamHandler()
+sh.setFormatter(log_formatter)
+logger.addHandler(sh)
+
+class TaskRunner:
+    def __init__(self):
+        self._data_generators = []
+        self._job_listeners = []
+        self._finished = False
+
+    def register_data_generators(self, data_generators: List[DataGenerator]):
+        for data_generator in data_generators:
+            self._data_generators.append(data_generator)
+        
+    def register_job_listeners(self, job_listeners: List[JobListener]):
+        for job_listener in job_listeners:
+            self._job_listeners.append(job_listener)
+
+    def run(self):
+        while not self._finished:
+            for data_generator in self._data_generators:
+                data_generator.start()
+
+            for job_listener in self._job_listeners:
+                job_listener.start(self)
+
+            self._finished = self._check_is_finished()
+            logging.info(f"Task is running...")
+            time.sleep(RUN_POLL_TIME)
+            
+    def _check_is_finished(self) -> bool:
+        finished = True
+        if any([data_generator.check_is_finished() == False for data_generator in self._data_generators]):
+            logging.debug("Remaining data generator jobs, not finished...")
+            finished = False
+        if any([job_listener.check_is_finished() == False for job_listener in self._job_listeners]):
+            logging.debug("Remaining job listener jobs, not finished...")
+            finished = False
+        return finished
