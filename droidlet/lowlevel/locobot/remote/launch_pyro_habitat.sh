aa3b7d78433c42422901ea749ad0a247208f9a47
Soumith Chintala <soumith@gmail.com>
2021-10-12 16:44:58 -0400

    modify launch script

--- droidlet/lowlevel/locobot/remote/launch_pyro_habitat.sh
+++ droidlet/lowlevel/locobot/remote/launch_pyro_habitat.sh
@@ -5,8 +5,9 @@ export PYRO_SERIALIZERS_ACCEPTED='pickle'
 export PYRO_SOCK_REUSE=True
 
 echo "Ensuring clean slate (kills roscore, rosmaster processes)..."
-killall -9 roscore &
-killall -9 rosmaster &
+killall -9 roscore
+killall -9 rosmaster
+killall python
 sleep 5
 
 echo "Launching environment ..."
@@ -15,6 +16,8 @@ source /root/pyenv_pyrobot_python3/bin/activate && source /root/pyrobot_catkin_w
 
 default_ip=$(hostname -I)
 ip=${LOCOBOT_IP:-$default_ip}
+export LOCAL_IP=$ip
+export LOCOBOT_IP=$ip
 echo "Binding to Host IP" $ip
 
 python -m Pyro4.naming -n $ip &
@@ -23,5 +26,23 @@ sleep 4
 
 pushd $(dirname ${BASH_SOURCE[0]})
 echo $ip
-python remote_locobot.py --ip $ip --backend habitat
+python remote_locobot.py --ip $ip --backend habitat &
 BGPID2=$!
+
+sleep 10
+
+python slam_service.py &
+BGPID3=$!
+
+sleep 1
+
+python planning_service.py &
+BGPID3=$!
+
+sleep 1
+
+python navigation_service.py
+BGPID3=$!
+
+
+
