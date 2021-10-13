3a815d4c6f33a70c6fe60c44d7414a4190f6dcc3
kavyasrinet <ksrinet@fb.com>
2021-10-13 16:23:35 -0700

    extra unit test wrong dir
    fix

--- droidlet/perception/semantic_parsing/tests/test_nsp_loading.py
+++ droidlet/perception/semantic_parsing/tests/test_nsp_loading.py
@@ -12,20 +12,26 @@ NLU_DATA_DIR = os.path.join(
 
 class TestNSPModel(unittest.TestCase):
     def setUp(self):
-        nsp_data_dir = NLU_DATA_DIR
-        nsp_model_dir = os.path.join(NLU_MODEL_DIR, "ttad_bert_updated")
-        self.model = Model(model_dir=nsp_model_dir, data_dir=nsp_data_dir)
+        self.nsp_model_dir = os.path.join(NLU_MODEL_DIR, "ttad_bert_updated")
+        self.model = Model(model_dir=self.nsp_model_dir, data_dir=NLU_DATA_DIR)
 
     def test_model_parse(self):
         chat = 'come here'
         logical_form = self.model.parse(chat=chat)
         self.assertEqual(type(logical_form), dict)
+        self.assertTrue("dialogue_type" in logical_form)
         chat = 'hello'
         logical_form = self.model.parse(chat=chat)
         self.assertEqual(type(logical_form), dict)
+        self.assertTrue("dialogue_type" in logical_form)
         chat = 'dance'
         logical_form = self.model.parse(chat=chat)
         self.assertEqual(type(logical_form), dict)
+        self.assertTrue("dialogue_type" in logical_form)
+
+    def test_model_dir(self):
+        # change the model directory and assert model doesn't load
+        self.assertRaises(Exception, Model, self.nsp_model_dir + "wert", NLU_DATA_DIR)
 
 if __name__ == '__main__':
     unittest.main()
