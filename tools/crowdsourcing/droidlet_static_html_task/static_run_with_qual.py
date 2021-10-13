45e164c75c291534a6956abd40f890060a187c5c
Yuxuan Sun <yuxuans@fb.com>
2021-10-13 03:05:26 -0700

    Move remaining files from droidlet/tools to tools

--- tools/crowdsourcing/droidlet_static_html_task/static_run_with_qual.py
+++ tools/crowdsourcing/droidlet_static_html_task/static_run_with_qual.py
@@ -26,7 +26,7 @@ TASK_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 defaults = [
     {"mephisto/blueprint": BLUEPRINT_TYPE},
     {"mephisto/architect": "heroku"},
-    {"mephisto/provider": "mturk"},
+    {"mephisto/provider": "mock"},
     {"conf": "run_with_qual"},
 ]
 
