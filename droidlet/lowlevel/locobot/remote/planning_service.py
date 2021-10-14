fully working refactored slam, with trackback, rotation fixes, etc.

droidlet/lowlevel/locobot/remote/planning_service.py
@@ -3,17 +3,15 @@ import numpy as np
 import Pyro4
 from slam_pkg.utils.fmm_planner import FMMPlanner
 
-
 Pyro4.config.SERIALIZER = "pickle"
 Pyro4.config.SERIALIZERS_ACCEPTED.add("pickle")
-Pyro4.config.ITER_STREAMING = True
 Pyro4.config.PICKLE_PROTOCOL_VERSION = 4
 
-
-# Pyro4.config.SERVERTYPE = "multiplex" # habitat
+def is_traversable(location, traversable):
+    return traversable[round(location[1]), round(location[0])]
 
 @Pyro4.expose
-class Planning(object):
+class Planner(object):
     def __init__(self, slam):
         self.slam = slam
         self.map_resolution = self.slam.get_map_resolution()
@@ -31,11 +29,15 @@ class Planning(object):
         robot_map_location = self.slam.robot2map(robot_location)
 
         # get occupancy map
-        traversable = self.slam.get_traversable_map()
+        traversable_map = self.slam.get_traversable_map()
+
+        # if the goal is an obstacle, you can't go there. Return
+        if not is_traversable(goal_map_location, traversable_map):
+            return False
 
         # construct a planner
         self.planner = FMMPlanner(
-            traversable,
+            traversable_map,
             step_size=int(step_size / self.map_resolution)
         )
 
@@ -43,21 +45,28 @@ class Planning(object):
         self.planner.set_goal(goal_map_location)
         stg = self.planner.get_short_term_goal(robot_map_location)
 
+        # if the goal is an obstacle, you can't go there. Return
+        if not is_traversable(stg, traversable_map):
+            return False
+
         # convert short-term-goal to real co-ordinates, and normalize
         # against robot initial state (if it wasn't zeros)
         stg_real = self.slam.map2robot(stg)
-        return stg_real
 
-    def goal_reached(self, robot_location, goal):
-        goal_map_location = self.slam.real2map(goal[:2])
-        robot_map_location = self.slam.robot2map(robot_location)
+        if self.goal_within_threshold(stg_real, goal):
+            # is it the final goal? if so, use the rotation from the goal
+            rotation_angle = goal[2]
+        else:
+            rotation_angle = np.arctan2(
+                stg_real[1] - robot_location[1],
+                stg_real[0] - robot_location[0]
+            )
+        return (stg_real[0], stg_real[1], rotation_angle)
 
-        distance = np.linalg.norm(np.array(robot_map_location)
-                                  - np.array(goal_map_location)) * 100.0
-        threshold = np.sqrt(2) * self.map_resolution
-
+    def goal_within_threshold(self, robot_location, goal):
+        distance = np.linalg.norm(np.array(robot_location[:2]) - np.array(goal[:2]))
+        threshold = 0.1 # in metres
         return distance < threshold
-    
 
 
 robot_ip = os.getenv('LOCOBOT_IP')
@@ -65,11 +74,11 @@ ip = os.getenv('LOCAL_IP')
     
 with Pyro4.Daemon(ip) as daemon:
     slam = Pyro4.Proxy("PYRONAME:slam@" + robot_ip)
-    obj = Planning(slam)
+    obj = Planner(slam)
     obj_uri = daemon.register(obj)
     with Pyro4.locateNS() as ns:
-        ns.register("planning", obj_uri)
+        ns.register("planner", obj_uri)
 
-    print("Planning Server is started...")
+    print("Planner Server is started...")
     daemon.requestLoop()
 