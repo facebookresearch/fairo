c3b46982cd8e1d682ebb5e52f2b87a7cedfb7c78
Soumith Chintala <soumith@gmail.com>
2021-10-23 23:48:54 -0400

    refactored what used to be called as remote_locobot + slam, into four services: 1. Navigation 2. Planning 3. SLAM 4. remote_locobot
    All unit tests pass without any changes.
    All previously known functionality or bug fixes are integrated into the refactored code except slam.py's visualization / data dumping stuff. For ex.: trackback, etc. are integrated and working.

--- droidlet/lowlevel/locobot/remote/launch_pyro_habitat.sh
+++ droidlet/lowlevel/locobot/remote/launch_pyro_habitat.sh
@@ -5,16 +5,16 @@ export PYRO_SERIALIZERS_ACCEPTED='pickle'
 export PYRO_SOCK_REUSE=True
 
 echo "Ensuring clean slate (kills roscore, rosmaster processes)..."
-killall -9 roscore &
-killall -9 rosmaster &
-sleep 5
+ps -elf|grep remote_locobot.py | grep -v grep | tr -s " " | cut -f 4 -d" " | xargs kill -9 >/dev/null 2>&1
+ps -elf|grep Pyro4.naming | grep -v grep | tr -s " " | cut -f 4 -d" " | xargs kill -9 >/dev/null 2>&1
+sleep 1
 
 echo "Launching environment ..."
-roscore &
-source /root/pyenv_pyrobot_python3/bin/activate && source /root/pyrobot_catkin_ws/devel/setup.bash
 
 default_ip=$(hostname -I)
 ip=${LOCOBOT_IP:-$default_ip}
+export LOCAL_IP=$ip
+export LOCOBOT_IP=$ip
 echo "Binding to Host IP" $ip
 
 python -m Pyro4.naming -n $ip &
@@ -23,5 +23,9 @@ sleep 4
 
 pushd $(dirname ${BASH_SOURCE[0]})
 echo $ip
-python remote_locobot.py --ip $ip --backend habitat
+python remote_locobot.py --ip $ip --backend habitat &
 BGPID2=$!
+
+sleep 45
+
+./launch_navigation.sh &
