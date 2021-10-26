8549d2f38c319c2867b4e6fcaf9e9359662ae4d8
Soumith Chintala <soumith@gmail.com>
2021-10-26 03:12:47 -0400

    upgrade to the latest pytorch and cleanup build scripts

--- droidlet/lowlevel/locobot/remote/launch_navigation.sh
+++ droidlet/lowlevel/locobot/remote/launch_navigation.sh
@@ -3,10 +3,10 @@ set -ex
 
 
 python slam_service.py &
-timeout 10s bash -c "until python check_connected.py slam; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py slam; do sleep 0.5; done;"
 
 python planning_service.py &
-timeout 10s bash -c "until python check_connected.py planner; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py planner; do sleep 0.5; done;"
 
 python navigation_service.py
-timeout 10s bash -c "until python check_connected.py navigation; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py navigation; do sleep 0.5; done;"
