2187eb7e8a00c1c4246ca5bea3b8d0410db9a776
Soumith Chintala <soumith@gmail.com>
2021-10-26 13:06:11 -0400

    add a create_conda script

---
+++ create_conda.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
+conda activate base
+
+conda env remove -n droidlet -y
+
+# conda install mamba -c conda-forge -y
+
+mamba create -n droidlet python=3.7 --file conda.txt --file agents/locobot/conda.txt \
+      -c pytorch -c aihabitat -c open3d-admin -c conda-forge  -y
+
+conda activate droidlet
+mamba install https://anaconda.org/aihabitat/habitat-sim/0.2.1/download/linux-64/habitat-sim-0.2.1-py3.7_headless_linux_fc7fb11ccec407753a73ab810d1dbb5f57d0f9b9.tar.bz2
+
+python setup.py develop
+
+pip install -r agents/locobot/requirements.txt
