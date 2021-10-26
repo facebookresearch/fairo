7fc09a732de1ac8bb37bb7d158f89ab2e480e37c
ExhAustin <aswang@andrew.cmu.edu>
2021-10-25 22:21:51 -0700

    Add comments

--- polymetis/polymetis/python/polymetis/utils/get_version.py
+++ polymetis/polymetis/python/polymetis/utils/get_version.py
@@ -5,24 +5,31 @@ import polymetis
 
 polymetis_version = ""
 
+# Conda installed
 if "CONDA_PREFIX" in os.environ and os.environ["CONDA_PREFIX"] in polymetis.__file__:
-    # Conda installed
+    # Search conda pkgs for polymetis & extract version number
     stream = os.popen("conda list | grep polymetis")
     for line in stream:
-        line_ls = [s for s in line.strip("\n").split(" ") if len(s) > 0]
-        if line_ls[0] == "polymetis":
-            polymetis_version = line_ls[1]
+        info_fields = [s for s in line.strip("\n").split(" ") if len(s) > 0]
+        if info_fields[0] == "polymetis":  # pkg name == polymetis
+            polymetis_version = info_fields[1]
             break
 
+# Built locally
 else:
-    # Built locally
+    # Navigate to polymetis pkg dir, which should be within the git repo
     original_cwd = os.getcwd()
     os.chdir(os.path.dirname(polymetis.__file__))
 
+    # Git describe output
     stream = os.popen("git describe --tags")
-    version_strings = [line for line in stream][0].strip("\n").split("-")
-    polymetis_version = f"{version_strings[-2]}_{version_strings[-1]}"
+    version_string = [line for line in stream][0]
 
+    # Modify to same format as conda env variable GIT_DESCRIBE_NUMBER
+    version_items = version_string.strip("\n").split("-")
+    polymetis_version = f"{version_items[-2]}_{version_items[-1]}"
+
+    # Reset cwd
     os.chdir(original_cwd)
 
 if not polymetis_version:
