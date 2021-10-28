7188bdb1093237b4761d79671c18dbbca231605c
kavyasrinet <ksrinet@fb.com>
2021-10-27 18:15:17 -0700

    cleanup compute_checksum - better structure, add comments
    rebase

--- tools/data_scripts/compute_checksum.py
+++ tools/data_scripts/compute_checksum.py
@@ -1,54 +1,61 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
 """
-This script computes hashes for local directories and saves them to data_scripts/default_checksums/
-# ./compute_checksum.sh craftassist # hash for semantic parser models
-# ./compute_checksum.sh craftassist datasets # hash for datasets folder
-# ./compute_checksum.sh locobot # hash for locobot models
-
+This script computes hashes for the given local artifact directories and saves them to data_scripts/default_checksums/
 """
 import os
 import subprocess
 
 
-def compute_checksum_for_directory(agent=None, artifact_type=None, artifact_name=None):
+def compute_checksum_for_directory(
+        agent=None,
+        artifact_type=None,
+        model_name=None):
+    """
+    Computes checksum for a given artifact directory and tracks it.
+    :param agent: Name of agent.
+    :param artifact_type: datasets or models artifact
+    :param model_name: name of model (nlu or perception)
+    """
     ROOTDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../')
     print("Rootdir : %r" % ROOTDIR)
 
     if not agent:
-        print("Agent name not specified, defaulting to craftassist")
+        print("Agent name not specified, defaulting to craftassist agent")
         agent = "craftassist"
 
     if not artifact_type:
         print("Artifact not specified, defaulting to models/")
         artifact_type = "models"
 
-    if artifact_type and not artifact_name:
+    if artifact_type == "models" and not model_name:
         print("Model name not given, defaulting to nlu model")
-        artifact_name = "nlu"
+        model_name = "nlu"
 
     agent_path = os.path.join(ROOTDIR, 'agents/'+agent)
 
     print("Now computing hashes ...")
     compute_shasum_script_path = os.path.join(ROOTDIR, 'tools/data_scripts/checksum_fn.sh')
+    checksum_name = ''
+    artifact_folder_name = ''
     if artifact_type == "models":
-        # TODO: rename the following folder and checksum files
-        # TODO: rename and cleanup this file
-        if artifact_name == "nlu":
+        artifact_folder_name = 'models/' + model_name
+        if model_name == "nlu":
             # compute for NLU model
-            artifact_path = os.path.join(agent_path, 'models/' + artifact_name)
-            checksum_write_path = os.path.join(ROOTDIR, 'tools/data_scripts/default_checksums/nlu.txt')
-        elif artifact_name == "perception":
+            checksum_name = 'nlu.txt'
+        elif model_name == "perception":
             # perception models
-            artifact_path = os.path.join(agent_path, 'models/' + artifact_name)
             if agent == "locobot":
-                checksum_write_path = os.path.join(ROOTDIR, 'tools/data_scripts/default_checksums/locobot.txt')
+                checksum_name = 'locobot_perception.txt'
             elif agent == "craftassist":
-                checksum_write_path = os.path.join(ROOTDIR, 'tools/data_scripts/default_checksums/craftassist_perception.txt')
+                checksum_name = 'craftassist_perception.txt'
     else:
         # datasets
-        artifact_path = os.path.join(agent_path, 'datasets/')
-        checksum_write_path = os.path.join(ROOTDIR, 'tools/data_scripts/default_checksums/datasets.txt')
+        artifact_folder_name = 'datasets/'
+        checksum_name = 'datasets.txt'
+
+    artifact_path = os.path.join(agent_path, artifact_folder_name)
+    checksum_write_path = os.path.join(ROOTDIR, 'tools/data_scripts/default_checksums/' + checksum_name)
     result = subprocess.check_output([compute_shasum_script_path, artifact_path, checksum_write_path],
                                      text=True)
     print(result)
