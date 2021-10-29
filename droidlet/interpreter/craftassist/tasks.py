00aafe3696d57e991cd48fe869208e08176c022e
Yuxuan Sun <34047968+snyxan@users.noreply.github.com>
2021-10-28 21:05:55 -0700

    Revert "Added move finished confirmation message (#765)"
    This reverts commit c643cf6243c9be7c1014b3ced0cf0ea0279962fb.

--- droidlet/interpreter/craftassist/tasks.py
+++ droidlet/interpreter/craftassist/tasks.py
@@ -226,7 +226,7 @@ class Move(BaseMovementTask):
         if manhat_dist(tuple(agent.pos), self.target) <= self.approx:
             if len(self.replace) > 0:
                 logging.error("Move finished with non-empty replace set: {}".format(self.replace))
-            self.finish(agent)
+            self.finished = True
             return
 
         # get path
@@ -259,10 +259,6 @@ class Move(BaseMovementTask):
                 step_fn = getattr(agent, step_fn_name)
                 step_fn()
                 break
-
-    def finish(self, agent):
-        agent.send_chat("I finished my movement.")
-        self.finished = True
 
     def __repr__(self):
         return "<Move {} ±{}>".format(self.target, self.approx)
