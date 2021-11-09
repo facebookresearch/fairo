7ab1bdd44fa0634ff9964b147513c208877f9840
ExhAustin <aswang@andrew.cmu.edu>
2021-11-09 09:37:25 -0800

    Bring back cartesian space joint move to

--- polymetis/polymetis/python/torchcontrol/policies/move_to.py
+++ polymetis/polymetis/python/torchcontrol/policies/move_to.py
@@ -12,30 +12,26 @@ from torchcontrol.transform import Transformation as T
 from torchcontrol.utils import to_tensor
 
 
-class JointSpaceMoveTo(toco.PolicyModule):
+class JointPlanExecutor(toco.PolicyModule):
     """
     Plans and executes a trajectory to a desired joint position
     """
 
     def __init__(
         self,
-        joint_pos_current,
-        joint_pos_desired,
+        plan: torch.nn.Module,
+        steps: int,
         Kp,
         Kd,
-        time_to_go: float,
-        hz: float,
         robot_model: torch.nn.Module,
         ignore_gravity: bool = True,
     ):
         """
         Args:
-            joint_pos_current: Current joint positions
-            joint_pos_desired: Desired joint positions
+            plan: A plan module from torchcontrol.modules.planning
+            steps: Length of plan in number of steps
             Kp: P gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
             Kd: D gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
-            time_to_go: Duration of trajectory in seconds
-            hz: Frequency of controller
             robot_model: A robot model from torchcontrol.models
             ignore_gravity: `True` if the robot is already gravity compensated, `False` otherwise
 
@@ -43,17 +39,8 @@ class JointSpaceMoveTo(toco.PolicyModule):
         """
         super().__init__()
 
-        joint_pos_current = to_tensor(joint_pos_current)
-        joint_pos_desired = to_tensor(joint_pos_desired)
-
-        # Planning
-        self.N = int(time_to_go * hz)
-        self.plan = toco.modules.planning.JointSpaceMinJerkPlanner(
-            start=joint_pos_current,
-            goal=joint_pos_desired,
-            steps=self.N,
-            time_to_go=time_to_go,
-        )
+        self.plan = plan
+        self.N = steps
 
         # Control
         self.control = toco.policies.JointImpedanceControl(
@@ -83,6 +70,100 @@ class JointSpaceMoveTo(toco.PolicyModule):
             self.set_terminated()
 
         return output
+
+
+class JointSpaceMoveTo(JointPlanExecutor):
+    def __init__(
+        joint_pos_current,
+        joint_pos_desired,
+        time_to_go: float,
+        hz: float,
+        *args,
+        **kwargs,
+    ):
+        """
+        Args:
+            joint_pos_current: Current joint positions
+            joint_pos_desired: Desired joint positions
+            time_to_go: Duration of trajectory in seconds
+            hz: Frequency of controller
+            Kp: P gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
+            Kd: D gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
+            robot_model: A robot model from torchcontrol.models
+            ignore_gravity: `True` if the robot is already gravity compensated, `False` otherwise
+
+        (Note: nA is the action dimension and N is the number of degrees of freedom)
+        """
+        # Parse inputs
+        joint_pos_current = to_tensor(joint_pos_current)
+        joint_pos_desired = to_tensor(joint_pos_desired)
+
+        # Plan
+        steps = int(time_to_go * hz)
+        plan = toco.modules.planning.JointSpaceMinJerkPlanner(
+            start=joint_pos_current,
+            goal=joint_pos_desired,
+            steps=steps,
+            time_to_go=time_to_go,
+        )
+
+        # Initialize joint plan executor
+        super().__init__(plan, steps, *args, **kwargs)
+
+
+class CartesianTargetJointMoveTo(JointPlanExecutor):
+    def __init__(
+        joint_pos_current,
+        ee_pos_desired,
+        time_to_go: float,
+        hz: float,
+        robot_model: torch.nn.Module,
+        ee_quat_desired=None,
+        *args,
+        **kwargs,
+    ):
+        """
+        Args:
+            joint_pos_current: Current joint positions
+            ee_pos_desired: Desired end-effector position (3D)
+            ee_quat_desired: Desired end-effector orientation (None if current orientation is desired)
+            time_to_go: Duration of trajectory in seconds
+            hz: Frequency of controller
+            Kp: P gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
+            Kd: D gain matrix of shape (nA, N) or shape (N,) representing a N-by-N diagonal matrix (if nA=N)
+            robot_model: A robot model from torchcontrol.models
+            ignore_gravity: `True` if the robot is already gravity compensated, `False` otherwise
+
+        (Note: nA is the action dimension and N is the number of degrees of freedom)
+        """
+        # Parse inputs
+        joint_pos_current = to_tensor(joint_pos_current)
+        ee_pos_current, ee_quat_current = robot_model.forward_kinematics(
+            joint_pos_current
+        )
+
+        ee_pos_desired = to_tensor(ee_pos_desired)
+        if ee_quat_desired is None:
+            ee_quat_desired = ee_quat_current
+        else:
+            ee_quat_desired = to_tensor(ee_quat_desired)
+
+        ee_pose_desired = T.from_rot_xyz(
+            rotation=R.from_quat(ee_quat_desired), translation=ee_pos_desired
+        )
+
+        # Plan
+        steps = int(time_to_go * hz)
+        plan = toco.modules.planning.CartesianSpaceMinJerkJointPlanner(
+            joint_pos_start=joint_pos_current,
+            ee_pose_goal=ee_pose_desired,
+            steps=steps,
+            time_to_go=time_to_go,
+            robot_model=robot_model,
+        )
+
+        # Initialize joint plan executor
+        super().__init__(plan, steps, robot_model=robot_model, *args, **kwargs)
 
 
 class CartesianSpaceMoveTo(toco.PolicyModule):
