8c37996f27d2f9afb401f7f2581a6dbdfa109dad
ExhAustin <aswang@andrew.cmu.edu>
2021-11-17 15:46:54 -0800

    Add async test

--- polymetis/polymetis/tests/python/polymetis/test_gripper_interface.py
+++ polymetis/polymetis/tests/python/polymetis/test_gripper_interface.py
@@ -1,4 +1,5 @@
 import time
+import random
 import pytest
 import unittest
 from unittest.mock import MagicMock
@@ -32,3 +33,19 @@ def test_gripper_interface(mocked_gripper, blocking):
     mocked_gripper.grpc_connection.GetState.assert_called_once()
     mocked_gripper.grpc_connection.Goto.assert_called_once()
     mocked_gripper.grpc_connection.Grasp.assert_called_once()
+
+
+@pytest.mark.parametrize("blocking", [True, False])
+def test_async_gripper_commands(mocked_gripper, blocking):
+    # Overload gripper with commands
+    for _ in range(10):
+        width = random.random()
+        speed = random.random()
+        force = random.random()
+        mocked_gripper.goto(width=width, speed=speed, force=force, blocking=blocking)
+
+    time.sleep(0.1)
+
+    # Check if last goto is being executed
+    last_cmd = polymetis_pb2.GripperCommand(width=width, speed=speed, force=force)
+    mocked_gripper.grpc_connection.Goto.assert_called_with(last_cmd)
