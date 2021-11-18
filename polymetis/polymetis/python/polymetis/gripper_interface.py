f11c397abd8778b42df4850916aa3d70e977d623
Austin Wang <austinswang@gmail.com>
2021-11-17 17:25:52 -0800

    [Polymetis] Franka gripper fix (#816)
    * Add franka gripper fix
    
    * Change gripper to ignore new commands
    
    * Cache gripper commands in Python
    
    * Use events
    
    * Debug
    
    * Debug
    
    * Fix async issues
    
    * Fix test
    
    * Switch to queues
    
    * Add async test
    
    * Update polymetis/polymetis/python/polymetis/gripper_interface.py
    
    Co-authored-by: Yixin Lin <1heart@users.noreply.github.com>
    
    Co-authored-by: Yixin Lin <1heart@users.noreply.github.com>

--- polymetis/polymetis/python/polymetis/gripper_interface.py
+++ polymetis/polymetis/python/polymetis/gripper_interface.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import threading
+import queue
 
 import grpc
 
@@ -25,11 +26,26 @@ class GripperInterface:
         self.channel = grpc.insecure_channel(f"{ip_address}:{port}")
         self.grpc_connection = polymetis_pb2_grpc.GripperServerStub(self.channel)
 
-    def _send_gripper_command(self, command, msg, blocking: bool = True) -> None:
-        if blocking:
+        # Execute commands from cache in separate thread
+        self._command_thr = threading.Thread(
+            target=self._command_executor,
+            daemon=True,
+        )
+
+        self._command_queue = queue.Queue(maxsize=1)
+        self._command_thr.start()
+
+    def _command_executor(self):
+        while True:
+            command, msg = self._command_queue.get()
             command(msg)
-        else:
-            threading.Thread(target=command, args=(msg,), daemon=True).start()
+            self._command_queue.task_done()
+
+    def _send_gripper_command(self, command, msg, blocking: bool = True) -> None:
+        self._command_queue.put((command, msg))
+
+        if blocking:
+            self._command_queue.join()
 
     def get_state(self) -> polymetis_pb2.GripperState:
         """Returns the state of the gripper
