18b92ff5ea90e889574bd4b9ba863607de9b4de9
Giri Anantharaman <giriman@devfair0439.h2.fair>
2021-11-21 06:43:41 -0800

    Extending tests

--- fbrp/tests/fbrp/runtime/test_base.py
+++ fbrp/tests/fbrp/runtime/test_base.py
@@ -41,7 +41,7 @@ class TestBaseLauncher(IsolatedAsyncioTestCase):
 
 
     @patch("fbrp.life_cycle.aio_proc_info_watcher")
-    async def test_down_watcher(self, mock_proc_info_watcher):
+    async def test_down_watcher_1(self, mock_proc_info_watcher):
         mock_ondown = MagicMock(return_value=async_return("on down called"))
         proc_info = ProcInfo(Ask.DOWN, State.STARTED, 0, True)
         mock_proc_info_watcher.return_value = AsyncIter([proc_info])
@@ -50,7 +50,30 @@ class TestBaseLauncher(IsolatedAsyncioTestCase):
         await base_launcher.down_watcher(mock_ondown)
         mock_proc_info_watcher.assert_called_once()
         mock_ondown.assert_called_once()
+
+    @patch("fbrp.life_cycle.aio_proc_info_watcher")
+    async def test_down_watcher_2(self, mock_proc_info_watcher):
+        mock_ondown = MagicMock(return_value=async_return("on down called"))
+        proc_info = ProcInfo(Ask.UP, State.STOPPED, 0, False)
+        mock_proc_info_watcher.return_value = AsyncIter([proc_info])
+        base_launcher = BaseLauncher()
+        base_launcher.name = 'TEST_BASE'
+        await base_launcher.down_watcher(mock_ondown)
+        mock_proc_info_watcher.assert_called_once()
+        mock_ondown.assert_not_called()
         
+    @patch("fbrp.life_cycle.aio_proc_info_watcher")
+    async def test_down_watcher_3(self, mock_proc_info_watcher):
+        mock_ondown = MagicMock(return_value=async_return("on down called"))
+        proc_info_1 = ProcInfo(Ask.UP, State.STOPPED, 0, False)
+        proc_info_2 = ProcInfo(Ask.DOWN, State.STARTED, 0, True)
+        mock_proc_info_watcher.return_value = AsyncIter([proc_info_1, proc_info_2])
+        base_launcher = BaseLauncher()
+        base_launcher.name = 'TEST_BASE'
+        await base_launcher.down_watcher(mock_ondown)
+        mock_proc_info_watcher.assert_called_once()
+        mock_ondown.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
