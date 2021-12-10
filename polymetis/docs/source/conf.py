ec36ac3c186ed8cad1975c5e7594fec69a902b21
Yixin Lin <1heart@users.noreply.github.com>
2021-12-10 16:59:43 -0500

    Update docs (#883)
    * Add logo
    
    * Add clear demarcation between your machine & NUC in usage.md

--- polymetis/docs/source/conf.py
+++ polymetis/docs/source/conf.py
@@ -62,6 +62,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # html_theme = "alabaster"
 html_theme = "sphinx_book_theme"
 
+html_favicon = "img/polymetis-favicon.svg"
+html_logo = "img/polymetis-logo.svg"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
