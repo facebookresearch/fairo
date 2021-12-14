38afc66855820c8752f09f7116b87fa4028de46c
Soumith Chintala <soumith@gmail.com>
2021-12-15 04:54:35 +0530

    [lowlevel] fix bug on launcher script around non-standard ip address

--- droidlet/lowlevel/locobot/remote/launch_navigation.sh
+++ droidlet/lowlevel/locobot/remote/launch_navigation.sh
@@ -2,10 +2,10 @@
 set -ex
 
 python slam_service.py &
-timeout 10s bash -c "until python check_connected.py slam; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py slam $LOCOBOT_IP; do sleep 0.5; done;" || true
 
 python planning_service.py &
-timeout 10s bash -c "until python check_connected.py planner; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py planner $LOCOBOT_IP; do sleep 0.5; done;" || true
 
 python navigation_service.py &
-timeout 10s bash -c "until python check_connected.py navigation; do sleep 0.5; done;" || true
+timeout 10s bash -c "until python check_connected.py navigation $LOCOBOT_IP; do sleep 0.5; done;" || true
