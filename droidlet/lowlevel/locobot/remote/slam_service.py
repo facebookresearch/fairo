7553f8a9210becdc7c8f48b25afe6ac17e7c1b0f
Soumith Chintala <soumith@gmail.com>
2021-12-15 05:07:46 +0530

    [lowlevel] add pcd grabbers in the low-level interface, and hook up the obstacle detection in low-level

--- droidlet/lowlevel/locobot/remote/slam_service.py
+++ droidlet/lowlevel/locobot/remote/slam_service.py
@@ -89,7 +89,7 @@ class SLAM(object):
         robot_relative_pos = du.get_relative_state(
             self.robot.get_base_state(),
             self.init_state)
-        pcd = self.robot.get_current_pcd(in_cam=False)[0]
+        pcd = self.robot.get_current_pcd2(in_cam=False)[0]
         
         self.map_builder.update_map(pcd, robot_relative_pos)
 
