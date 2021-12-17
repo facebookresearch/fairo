4dd6745f4b6a678a559a271502d6b5e17782ddb9
Ethan Carlson <ethancarlson@fb.com>
2021-12-16 19:50:54 -0800

    two scenes with locked cameras, one editable

--- droidlet/tools/crowdsourcing/vision_annotation_task/server_files/extra_refs/voxel_painter.mjs
+++ droidlet/tools/crowdsourcing/vision_annotation_task/server_files/extra_refs/voxel_painter.mjs
@@ -1,72 +1,133 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from './OrbitControls.mjs';
 
-let camera, controls, scene, renderer;
-let plane;
-let pointer, raycaster, isShiftDown = false;
+let camera1, controls1, scene1, renderer1;
+let plane1;
+let camera2, controls2, scene2, renderer2;
+let plane2;
 
-let rollOverMesh, rollOverMaterial;
-let cubeGeo, cubeMaterial;
+let pointer2, raycaster2; 
+let isShiftDown = false;
+
+let rollOverMesh2, rollOverMaterial2;
+let cubeGeo1, cubeMaterial1;
+let cubeGeo2, cubeMaterial2;
 
 const objects = [];
 
-init();
+init1();
+init2();
 render();
 
-function init() {
+var canvii = document.getElementsByTagName("canvas");
+Array.from(canvii).forEach(canv => canv.style.display = "inline");
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set( 500, 800, 1300 );
-    camera.lookAt( 0, 0, 0 );
+function init1() {
+    // This is the left scene, uneditable
+    camera1 = new THREE.PerspectiveCamera( 45, (window.innerWidth/2) / window.innerHeight, 1, 10000 );
+    camera1.position.set( 500, 800, 1300 );
+    camera1.lookAt( 0, 0, 0 );
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xf0f0f0 );
-
-    // roll-over helpers
-    const rollOverGeo = new THREE.BoxGeometry( 50, 50, 50 );
-    rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
-    rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
-    scene.add( rollOverMesh );
+    scene1 = new THREE.Scene();
+    scene1.background = new THREE.Color( 0xf0f0f0 );
 
     // cubes
-    cubeGeo = new THREE.BoxGeometry( 50, 50, 50 );
-    cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, map: new THREE.TextureLoader().load( 'square-outline-textured.png' ) } );
+    cubeGeo1 = new THREE.BoxGeometry( 50, 50, 50 );
+    cubeMaterial1 = new THREE.MeshLambertMaterial( { color: 0xfeb74c, map: new THREE.TextureLoader().load( 'square-outline-textured.png' ) } );
 
     // grid
     const gridHelper = new THREE.GridHelper( 1000, 20 );
-    scene.add( gridHelper );
-    raycaster = new THREE.Raycaster();
-    pointer = new THREE.Vector2();
+    scene1.add( gridHelper );
 
     const geometry = new THREE.PlaneGeometry( 1000, 1000 );
     geometry.rotateX( - Math.PI / 2 );
 
-    plane = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
-    scene.add( plane );
+    plane1 = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
+    scene1.add( plane1 );
 
-    objects.push( plane );
+    objects.push( plane1 );
 
     // lights
     const ambientLight = new THREE.AmbientLight( 0x606060 );
-    scene.add( ambientLight );
+    scene1.add( ambientLight );
 
     const directionalLight = new THREE.DirectionalLight( 0xffffff );
     directionalLight.position.set( 1, 0.75, 0.5 ).normalize();
-    scene.add( directionalLight );
+    scene1.add( directionalLight );
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    var cont = document.getElementById("voxel_painter");
-    cont.appendChild( renderer.domElement );
+    renderer1 = new THREE.WebGLRenderer( { antialias: true } );
+    renderer1.setPixelRatio( window.devicePixelRatio );
+    renderer1.setSize( (window.innerWidth/2.1), window.innerHeight );
+    let cont = document.getElementById("voxel_painter");
+    cont.appendChild( renderer1.domElement );
 
-    controls = new OrbitControls( camera, renderer.domElement );
-    controls.listenToKeyEvents( window ); // optional
-    controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+    controls1 = new OrbitControls( camera1, renderer1.domElement );
+    controls1.listenToKeyEvents( window ); // optional
+    controls1.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
 
-    controls.enableZoom = false;
-    controls.minPolarAngle = (1.2 * Math.PI) / 4;
-    controls.maxPolarAngle = (1.2 * Math.PI) / 4;
+    controls1.enableZoom = false;
+    controls1.minPolarAngle = (1.2 * Math.PI) / 4;
+    controls1.maxPolarAngle = (1.2 * Math.PI) / 4;
+
+    document.addEventListener( 'keydown', onDocumentKeyDown );
+    document.addEventListener( 'keyup', onDocumentKeyUp );
+    window.addEventListener( 'resize', onWindowResize );
+}
+
+function init2() {
+    // This is the right scene, uneditable
+    camera2 = new THREE.PerspectiveCamera( 45, (window.innerWidth/2) / window.innerHeight, 1, 10000 );
+    camera2.position.set( 500, 800, 1300 );
+    camera2.lookAt( 0, 0, 0 );
+
+    scene2 = new THREE.Scene();
+    scene2.background = new THREE.Color( 0xf0f0f0 );
+
+    // roll-over helpers
+    const rollOverGeo = new THREE.BoxGeometry( 50, 50, 50 );
+    rollOverMaterial2 = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+    rollOverMesh2 = new THREE.Mesh( rollOverGeo, rollOverMaterial2 );
+    scene2.add( rollOverMesh2 );
+
+    // cubes
+    cubeGeo2 = new THREE.BoxGeometry( 50, 50, 50 );
+    cubeMaterial2 = new THREE.MeshLambertMaterial( { color: 0xfeb74c, map: new THREE.TextureLoader().load( 'square-outline-textured.png' ) } );
+
+    // grid
+    const gridHelper = new THREE.GridHelper( 1000, 20 );
+    scene2.add( gridHelper );
+    raycaster2 = new THREE.Raycaster();
+    pointer2 = new THREE.Vector2();
+
+    const geometry = new THREE.PlaneGeometry( 1000, 1000 );
+    geometry.rotateX( - Math.PI / 2 );
+
+    plane2 = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
+    scene2.add( plane2 );
+
+    objects.push( plane2 );
+
+    // lights
+    const ambientLight = new THREE.AmbientLight( 0x606060 );
+    scene2.add( ambientLight );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff );
+    directionalLight.position.set( 1, 0.75, 0.5 ).normalize();
+    scene2.add( directionalLight );
+
+    renderer2 = new THREE.WebGLRenderer( { antialias: true } );
+    renderer2.setPixelRatio( window.devicePixelRatio );
+    renderer2.setSize( (window.innerWidth/2.1), window.innerHeight );
+    let cont = document.getElementById("voxel_painter");
+    cont.appendChild( renderer2.domElement );
+
+    controls2 = new OrbitControls( camera2, renderer2.domElement );
+    controls2.listenToKeyEvents( window ); // optional
+    controls2.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+    controls2.enableZoom = false;
+    controls2.minPolarAngle = (1.2 * Math.PI) / 4;
+    controls2.maxPolarAngle = (1.2 * Math.PI) / 4;
 
     document.addEventListener( 'pointermove', onPointerMove );
     document.addEventListener( 'pointerdown', onPointerDown );
@@ -76,29 +137,32 @@ function init() {
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera1.aspect = (window.innerWidth/2) / window.innerHeight;
+    camera1.updateProjectionMatrix();
+    renderer1.setSize( (window.innerWidth/2.1), window.innerHeight );
+    camera2.aspect = (window.innerWidth/2) / window.innerHeight;
+    camera2.updateProjectionMatrix();
+    renderer2.setSize( (window.innerWidth/2.1), window.innerHeight );
     render();
 }
 
 function onPointerMove( event ) {
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
-    raycaster.setFromCamera( pointer, camera );
-    const intersects = raycaster.intersectObjects( objects, false );
+    pointer2.set( ( (event.clientX - (window.innerWidth/2.1)) / (window.innerWidth/2.1) ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+    raycaster2.setFromCamera( pointer2, camera2 );
+    const intersects = raycaster2.intersectObjects( objects, false );
 
     if ( intersects.length > 0 ) {
         const intersect = intersects[ 0 ];
-        rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal );
-        rollOverMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+        rollOverMesh2.position.copy( intersect.point ).add( intersect.face.normal );
+        rollOverMesh2.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
     }
     render();
 }
 
 function onPointerDown( event ) {
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
-    raycaster.setFromCamera( pointer, camera );
-    const intersects = raycaster.intersectObjects( objects, false );
+    pointer2.set( ( (event.clientX - (window.innerWidth/2.1)) / (window.innerWidth/2.1) ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+    raycaster2.setFromCamera( pointer2, camera2 );
+    const intersects = raycaster2.intersectObjects( objects, false );
 
     if ( intersects.length > 0 ) {
         const intersect = intersects[ 0 ];
@@ -106,16 +170,16 @@ function onPointerDown( event ) {
         // delete cube
         if ( isShiftDown ) {
         if ( intersect.object !== plane ) {
-            scene.remove( intersect.object );
+            scene2.remove( intersect.object );
             objects.splice( objects.indexOf( intersect.object ), 1 );
         }
 
         // create cube
         } else {
-        const voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+        const voxel = new THREE.Mesh( cubeGeo2, cubeMaterial2 );
         voxel.position.copy( intersect.point ).add( intersect.face.normal );
         voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
-        scene.add( voxel );
+        scene2.add( voxel );
         objects.push( voxel );
         }
         render();
@@ -135,5 +199,6 @@ function onDocumentKeyUp( event ) {
 }
 
 function render() {
-    renderer.render( scene, camera );
+    renderer1.render( scene1, camera1 );
+    renderer2.render( scene2, camera2 );
 }
