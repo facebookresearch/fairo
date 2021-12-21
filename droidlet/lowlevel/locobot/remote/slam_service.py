a1ca4da85980d239d42ea77836b905b0c2dee060
Soumith Chintala <soumith@gmail.com>
2021-12-21 05:34:59 +0530

    [hello-remote] integrate navigation, integrate obstacle detection into goto

--- droidlet/lowlevel/locobot/remote/slam_service.py
+++ droidlet/lowlevel/locobot/remote/slam_service.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import numpy as np
 import Pyro4
@@ -115,11 +116,14 @@ class SLAM(object):
         self.map_builder.reset_map(self.map_size)
 
 
-robot_ip = os.getenv("LOCOBOT_IP")
-ip = os.getenv("LOCAL_IP")
+robot_ip = os.getenv('LOCOBOT_IP')
+ip = os.getenv('LOCAL_IP')
+robot_name = "remotelocobot"
+if len(sys.argv) > 1:
+    robot_name = sys.argv[1]
 with Pyro4.Daemon(ip) as daemon:
-    robot = Pyro4.Proxy("PYRONAME:remotelocobot@" + robot_ip)
-    obj = SLAM(robot)
+    robot = Pyro4.Proxy("PYRONAME:" + robot_name + "@" + robot_ip)
+    obj = SLAM(robot)    
     obj_uri = daemon.register(obj)
     with Pyro4.locateNS(robot_ip) as ns:
         ns.register("slam", obj_uri)
