f47772131b4708a001cd7fafa4b9b187b42ca431
Ali Senguel <ali.senguel@fb.com>
2021-12-21 19:54:28 +0000

    Dashboard Build

--- droidlet/dashboard/web/build/service-worker.js
+++ droidlet/dashboard/web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51375c607d5c00983ba3c05acc460ccd.js"
+  "/precache-manifest.c24a52c446d759613b7fb7bee42e2d46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
