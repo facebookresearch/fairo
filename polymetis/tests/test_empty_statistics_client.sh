30548806152ea1f84543526a5be37c9d3a96bc33
1heart <yixin1996@gmail.com>
2021-12-28 12:27:31 -0800

    Refactor integration tests & circleci

---
+++ polymetis/tests/test_empty_statistics_client.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -eo pipefail
+trap "kill -- -$$" ERR # cleanup all child processes if error
+
+export HYDRA_FULL_ERROR=1
+
+######################
+# Test empty statistics client
+######################
+# Start server & robot client
+echo "=== Starting server and testing empty statistics client... ==="
+launch_robot.py robot_client=empty_statistics_client use_real_time=false num_requests=5000 &
+server_pid=$!
+echo "=== Server PID: $server_pid ==="
+
+# Wait
+sleep 8
+
+echo "=== Success. ==="
+
+# Kill server
+if ps -p $server_pid > /dev/null
+then
+    kill $server_pid
+fi
---
+++ polymetis/tests/test_mocked_hardware_client.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -eo pipefail
+trap "kill -- -$$" ERR # cleanup all child processes if error
+
+export HYDRA_FULL_ERROR=1
+
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+######################
+# Test mocked hardware client
+######################
+# Start server & robot client
+echo "=== Starting server and mocked franka hardware client... ==="
+launch_robot.py robot_client=franka_hardware use_real_time=false robot_client.executable_cfg.mock=true &
+server_pid=$!
+echo "=== Server PID: $server_pid ==="
+
+sleep 4
+
+# Run examples
+echo "=== Running controllers in examples/ ... ==="
+for entry in "$PROJECT_ROOT_DIR/polymetis/examples"/*; do
+    if [ ${entry: -3} == ".py" ]; then
+        echo "Running `python $entry`"
+        python $entry
+    fi
+done
+
+echo "=== Success. ==="
+
+# Kill server
+if ps -p $server_pid > /dev/null
+then
+    kill $server_pid
+fi
