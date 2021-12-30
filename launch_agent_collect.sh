9e09bde84df7e0d9993e9ef110a098457a955104
anuragprat1k <57542204+anuragprat1k@users.noreply.github.com>
2021-12-30 13:40:23 -0500

    nits

--- launch_agent_collect.sh
+++ launch_agent_collect.sh
@@ -1,8 +1,8 @@
 export LOCOBOT_IP=$1
 export SAVE_EXPLORATION=True
-export DATA_PATH=straightline_noisy
-export HEURISTIC=straightlinet
+export DATA_PATH=straightline
+export HEURISTIC=straightline
 export VISUALIZE_EXAMINE=True
-export CONTINUOUS_EXPLORE=True
+export CONTINUOUS_EXPLORE=False
 source activate /private/home/apratik/miniconda3/envs/droidlet
 python agents/locobot/locobot_agent.py --dev
