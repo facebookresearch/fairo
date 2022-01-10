366860b6e4a73d077de1ee0cb4d0918b926826a9
Ali Senguel <ali.senguel@fb.com>
2022-01-10 18:19:21 +0000

    Black formatted, updated franka_remote and roboarm_agent

--- polymetis/examples/franka_remote.py
+++ polymetis/examples/franka_remote.py
@@ -6,43 +6,46 @@ Pyro4.config.SERIALIZERS_ACCEPTED.add("pickle")
 Pyro4.config.ITER_STREAMING = True
 Pyro4.config.PICKLE_PROTOCOL_VERSION = 4
 
+
 @Pyro4.expose
 class RemoteFranka(object):
     def __init__(self):
-        self.robot = RobotInterface ( ip_address = "localhost")
+        self.robot = RobotInterface(ip_address="localhost")
 
     def connect(self):
         if self.robot == None:
-            self.robot = RobotInterface ( ip_address = "localhost")
-        
+            self.robot = RobotInterface(ip_address="localhost")
+
     def go_home(self):
-        #Reset
+        # Reset
         self.robot.go_home()
         joint_pos = self.robot.get_joint_angles()
 
-    def move(self, joint,vel):
+    def move(self, joint, vel):
         # Get joint positions
         joint_pos = self.robot.get_joint_angles()
         if joint == 1:
-        	delta_joint_pos_desired = torch.Tensor([0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])	
-        elif joint == 2:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0])
-        elif joint == 3:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0])
-        elif joint == 4:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0])
-        elif joint == 5:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0])
-        elif joint == 6:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0])
-        else:	
-        	delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        	        	        	        	        	        	
+            delta_joint_pos_desired = torch.Tensor([0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        elif joint == 2:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0])
+        elif joint == 3:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0])
+        elif joint == 4:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0])
+        elif joint == 5:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0])
+        elif joint == 6:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0])
+        else:
+            delta_joint_pos_desired = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
         joint_pos_desired = joint_pos + delta_joint_pos_desired
-        state_log = self.robot.set_joint_positions(joint_pos_desired, time_to_go=(4.0-vel))  
+        state_log = self.robot.set_joint_positions(
+            joint_pos_desired, time_to_go=(4.0 - vel)
+        )
         # Get updated joint positions
         joint_pos = self.robot.get_joint_angles()
-                                     
+
     def get_ee_pos(self):
         # Get ee pose
         ee_pos, ee_quat = self.robot.pose_ee()
@@ -54,16 +57,19 @@ class RemoteFranka(object):
         # Command robot to ee pose (move ee downwards)
         # note: can also be done with robot.move_ee_xyz
         ee_pos, ee_quat = self.robot.pose_ee()
-        axis =torch.Tensor([0.0, 0.0, -1])
+        axis = torch.Tensor([0.0, 0.0, -1])
         const = float(pos)
-        #print (const)
-        delta_ee_pos_desired = torch.mul(const,axis)
-        #print(delta_ee_pos_desired)
+        # print (const)
+        delta_ee_pos_desired = torch.mul(const, axis)
+        # print(delta_ee_pos_desired)
         ee_pos_desired = ee_pos + delta_ee_pos_desired
-        self.robot.set_ee_pose( position=ee_pos_desired, orientation=None, time_to_go=2.0)
-    
+        self.robot.set_ee_pose(
+            position=ee_pos_desired, orientation=None, time_to_go=2.0
+        )
+
     def print_franka(self):
-        print ("Connected to Franka")
+        print("Connected to Franka")
+
 
 if __name__ == "__main__":
     import argparse
@@ -77,8 +83,8 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    
-    print (f'IP:: >  {args.ip}')
+
+    print(f"IP:: >  {args.ip}")
 
     # GLContexts in general are thread local
     # The PyRobot <-> Habitat integration is not thread-aware / thread-configurable,
@@ -86,25 +92,19 @@ if __name__ == "__main__":
     # multiplexing (which isn't too bad)
     Pyro4.config.SERVERTYPE = "multiplex"
     robot = RemoteFranka()
-    
-    
-    
-    #daemon = Pyro4.Daemon(args.ip)
-    #daemon = Pyro4.Daemon()
-    print ("Deamen server created")
-    #ns = Pyro4.locateNS()
-    
-    #robot_uri = daemon.register(robot)
-    
-    daemon = Pyro4.Daemon.serveSimple({robot: 'remotefranka',}, host=args.ip, port=9090, ns=False, verbose=True)
+    print("Deamen server created")
+    daemon = Pyro4.Daemon.serveSimple(
+        {
+            robot: "remotefranka",
+        },
+        host=args.ip,
+        port=9090,
+        ns=False,
+        verbose=True,
+    )
 
     daemon.close()
-    print('daemon closed')
-    
-    #ns.register("remotelocobot", robot_uri)
-
-    #print("Server is started...")
-    #daemon.requestLoop()
+    print("daemon closed")
 
 
 # Below is client code to run in a separate Python shell...
--- polymetis/examples/roboarm_agent.py
+++ polymetis/examples/roboarm_agent.py
@@ -9,20 +9,12 @@ import logging
 import faulthandler
 from multiprocessing import set_start_method
 import shutil
-
 import numpy as np
-from PIL import Image
-import base64
-import cv2
-
-import matplotlib.pyplot as plt
-import imageio
-from PIL import Image
-
 import Pyro4
 
 from droidlet import dashboard
-#from droidlet.tools.data_scripts.try_download import try_download_artifacts
+
+# from droidlet.tools.data_scripts.try_download import try_download_artifacts
 
 if __name__ == "__main__":
     # this line has to go before any imports that contain @sio.on functions
@@ -32,12 +24,11 @@ from droidlet.base_util import to_player_struct, Pos, Look, Player
 from agents.droidlet_agent import DroidletAgent
 from agents.argument_parser import ArgumentParser
 from droidlet.memory.robot.loco_memory import LocoAgentMemory, DetectedObjectNode
-from droidlet.perception.semantic_parsing.utils.interaction_logger import InteractionLogger
-
-from droidlet.interpreter.robot import (
-    dance,
-    default_behaviors
+from droidlet.perception.semantic_parsing.utils.interaction_logger import (
+    InteractionLogger,
 )
+
+from droidlet.interpreter.robot import dance, default_behaviors
 from droidlet.dialog.robot import LocoBotCapabilities
 import droidlet.lowlevel.rotation as rotation
 from droidlet.event import sio
@@ -90,30 +81,26 @@ class RoboarmAgent(DroidletAgent):
         self.point_targets = []
         self.init_event_handlers()
 
-        #uri = "PYRO:obj_599bf1e3a7db4d818cd9b328d6c55b92@localhost:36829"            
-        #self.simEnv = Pyro4.Proxy(uri)
-        self.simEnv = Pyro4.core.Proxy('PYRO:remoteEnv@' + "127.0.1.1" + ':9000')
+        # uri = "PYRO:obj_599bf1e3a7db4d818cd9b328d6c55b92@localhost:36829"
+        # self.simEnv = Pyro4.Proxy(uri)
+        self.simEnv = Pyro4.core.Proxy("PYRO:remoteEnv@" + "127.0.1.1" + ":9000")
         self.sim_image = []
-        
 
-        
         # list of (prob, default function) pairs
-        if self.backend == 'habitat':
+        if self.backend == "habitat":
             self.visible_defaults = [(1.0, default_behaviors.explore)]
-        elif self.backend == 'polysim':
-            self.visible_defaults = [(1.0, default_behaviors.explore)] 
+        elif self.backend == "polysim":
+            self.visible_defaults = [(1.0, default_behaviors.explore)]
         else:
-            raise RuntimeError("Unknown backend specified {}" % (self.backend, ))
+            raise RuntimeError("Unknown backend specified {}" % (self.backend,))
         self.interaction_logger = InteractionLogger()
         if os.path.exists("annotation_data/rgb"):
             shutil.rmtree("annotation_data/rgb")
         if os.path.exists("annotation_data/seg"):
             shutil.rmtree("annotation_data/seg")
-            
 
     def init_event_handlers(self):
         super().init_event_handlers()
-
 
         @sio.on("movement command")
         def test_command(sid, commands, movement_values):
@@ -121,9 +108,8 @@ class RoboarmAgent(DroidletAgent):
                 movement_values["yaw"] = 0.01
                 movement_values["velocity"] = 0.1
 
-            #Get velocity    
+            # Get velocity
             vel = movement_values["velocity"]
-
 
             for command in commands:
                 if command == "PAN_LEFT":
@@ -135,62 +121,25 @@ class RoboarmAgent(DroidletAgent):
                 elif command == "TILT_DOWN":
                     self.mover.bot.set_tilt(self.mover.bot.get_tilt() + 0.08)
                 elif command == "MOVE_JOINT_1":
-                    #print("JOINT_1 BUTTON PRESSED")
-                    self.mover.move(1,vel)
+                    self.mover.move(1, vel)
                 elif command == "MOVE_JOINT_2":
-                    #print("JOINT_2 BUTTON PRESSED")
-                    self.mover.move(2,vel)
+                    self.mover.move(2, vel)
                 elif command == "MOVE_JOINT_3":
-                    #print("JOINT_3 BUTTON PRESSED")
-                    self.mover.move(3,vel)    
+                    self.mover.move(3, vel)
                 elif command == "MOVE_JOINT_4":
-                    #print("JOINT_4 BUTTON PRESSED")
-                    self.mover.move(4,vel)
+                    self.mover.move(4, vel)
                 elif command == "MOVE_JOINT_5":
-                    #print("JOINT_5 BUTTON PRESSED")
-                    self.mover.move(5,vel)
+                    self.mover.move(5, vel)
                 elif command == "MOVE_JOINT_6":
-                    #print("JOINT_6 BUTTON PRESSED")
-                    self.mover.move(6,vel) 
+                    self.mover.move(6, vel)
                 elif command == "GO_HOME":
                     self.mover.go_home()
-
-
-
-                    #print (self.env.get_state())
-                #elif command == "GO_HOME":
-                    #self.mover.go_home()                       
                 elif command == "GET_POS":
                     pos = self.mover.get_ee_pos()
                     movement_values["ee_pos"] = pos
                 elif command == "GET_IMAGE":
                     rgb = self.update_sim_image()
                     sio.emit("updateImage", rgb)
-
-
-                    ##arrayList = self.simEnv.get_image()
-                    ##npArray = np.asarray(arrayList)
-                    ##npArray = npArray.astype(np.uint8)
-                    #print(type(npArray))
-                    #pil_image = Image.fromarray(npArray)
-                    #pil_image.show()
-                    #im = np.array(imageio.imread('Lenna_(test_image).png'))
-                    ##im = npArray[:,:,0:3]
-                    
-                    ##print(im.shape)
-                    ##print(type(im))
-
-                    ##print(npArray.shape)
-                    ##print(type(npArray))                    
-                    ##rgb_im = self.createRGB(im)
-                    #print(type(rgb_im))  
-                    ##serialized_image = rgb_im.to_struct(224,10)
-                    ##rgb = serialized_image["rgb"]
-                    ##sio.emit("updateImage", rgb)
-                    #sio.emit ("rgb", rgb) 
-              
-            #print(movement_values)
-            #sio.emit("updatePosState", movement_values)
 
         @sio.on("shutdown")
         def _shutdown(sid, data):
@@ -214,14 +163,14 @@ class RoboarmAgent(DroidletAgent):
             objects = LP.label_propagation(postData)
             sio.emit("labelPropagationReturn", objects)
 
-#@sio.on.emit("movement command", commands, movementValues);
+    # @sio.on.emit("movement command", commands, movementValues);
     def update_sim_image(self):
         arrayList = self.simEnv.get_image()
         npArray = np.asarray(arrayList)
         npArray = npArray.astype(np.uint8)
-        im = npArray[:,:,0:3]                  
+        im = npArray[:, :, 0:3]
         rgb_im = self.createRGB(im)
-        serialized_image = rgb_im.to_struct(224,10)
+        serialized_image = rgb_im.to_struct(224, 10)
         rgb = serialized_image["rgb"]
         return rgb
 
@@ -239,7 +188,6 @@ class RoboarmAgent(DroidletAgent):
         dance.add_default_dances(self.memory)
         logging.info("Initialized agent memory")
 
-
     def init_perception(self):
         """Instantiates all perceptual modules.
 
@@ -248,62 +196,61 @@ class RoboarmAgent(DroidletAgent):
         """
         if not hasattr(self, "perception_modules"):
             self.perception_modules = {}
-        #self.perception_modules["language_understanding"] = NSPQuerier(self.opts, self)
-        #self.perception_modules["self"] = SelfPerception(self)
-        #self.perception_modules["vision"] = Perception(self.opts.perception_model_dir)
-	
+        # self.perception_modules["language_understanding"] = NSPQuerier(self.opts, self)
+        # self.perception_modules["self"] = SelfPerception(self)
+        # self.perception_modules["vision"] = Perception(self.opts.perception_model_dir)
 
-	
     def perceive(self, force=False):
         # 1. perceive from NLU parser
         print("PERCIEVE_DEBUG")
         super().perceive(force=force)
         # 2. perceive from robot perception modules
-        #self.perception_modules["self"].perceive(force=force)
-        #rgb_depth = self.mover.get_rgb_depth()
-        #xyz = self.mover.get_base_pos_in_canonical_coords()
-        #x, y, yaw = xyz
-        #if self.backend == 'habitat':
+        # self.perception_modules["self"].perceive(force=force)
+        # rgb_depth = self.mover.get_rgb_depth()
+        # xyz = self.mover.get_base_pos_in_canonical_coords()
+        # x, y, yaw = xyz
+        # if self.backend == 'habitat':
         #    sio.emit(
         #        "map",
         #        {"x": x, "y": y, "yaw": yaw, "map": self.mover.get_obstacles_in_canonical_coords()},
         #    )
 
-        #previous_objects = DetectedObjectNode.get_all(self.memory)
+        # previous_objects = DetectedObjectNode.get_all(self.memory)
         # perception_output is a namedtuple of : new_detections, updated_detections, humans
-        #perception_output = self.perception_modules["vision"].perceive(rgb_depth,
+        # perception_output = self.perception_modules["vision"].perceive(rgb_depth,
         #                                                       xyz,
         #                                                       previous_objects,
         #                                                       force=force)
-        #self.memory.update(perception_output)
-
+        # self.memory.update(perception_output)
 
     def init_controller(self):
         """Instantiates controllers - the components that convert a text chat to task(s)."""
         dialogue_object_classes = {}
         print("CONTROLLER_DEBUG")
-        dialogue_object_classes["bot_capabilities"] = {"task": LocoBotCapabilities, "data": {}}
-        #dialogue_object_classes["interpreter"] = LocoInterpreter
-        #dialogue_object_classes["get_memory"] = LocoGetMemoryHandler
-        #dialogue_object_classes["put_memory"] = PutMemoryHandler
-        #self.dialogue_manager = DialogueManager(
+        dialogue_object_classes["bot_capabilities"] = {
+            "task": LocoBotCapabilities,
+            "data": {},
+        }
+        # dialogue_object_classes["interpreter"] = LocoInterpreter
+        # dialogue_object_classes["get_memory"] = LocoGetMemoryHandler
+        # dialogue_object_classes["put_memory"] = PutMemoryHandler
+        # self.dialogue_manager = DialogueManager(
         #    memory=self.memory,
         #    dialogue_object_classes=dialogue_object_classes,
         #    dialogue_object_mapper=DialogueObjectMapper,
         #    opts=self.opts,
-        #)
+        # )
 
     def init_physical_interfaces(self):
         """Instantiates the interface to physically move the robot."""
         print(f"IP::: PHYSICAL INTERFACE {self.opts.ip}")
-        self.mover = Pyro4.core.Proxy('PYRO:remotefranka@' + self.opts.ip + ':9090')
+        self.mover = Pyro4.core.Proxy("PYRO:remotefranka@" + self.opts.ip + ":9090")
 
     def step(self):
-        #super().step()
+        # super().step()
         time.sleep(0)
         rgb = self.update_sim_image()
         sio.emit("updateImage", rgb)
-        
 
     def task_step(self, sleep_time=0.0):
         super().task_step(sleep_time=sleep_time)
@@ -321,23 +268,29 @@ class RoboarmAgent(DroidletAgent):
             """
             pass
         time.sleep(5)  # let the other threads die
-        os._exit(0)  # TODO: remove and figure out why multiprocess sometimes hangs on exit
+        os._exit(
+            0
+        )  # TODO: remove and figure out why multiprocess sometimes hangs on exit
 
-    def createRGB (self,im):
+    def createRGB(self, im):
         p = im.shape[0]
         xs = np.sort(np.random.uniform(0, 10, p))
         xs = np.tile(xs, (p, 1))
 
         ys = np.sort(np.random.uniform(0, 10, p))
         ys[::-1].sort()
-        ys = np.transpose(np.tile(ys, (p,1)))
+        ys = np.transpose(np.tile(ys, (p, 1)))
 
         # (x,y,z=1) in row-major form, in locobot coords
-        pts = np.asarray([xyz_canonical_coords_to_pyrobot_coords((x,y,1))
-            for x,y in zip(xs.ravel(), ys.ravel())])
-        
+        pts = np.asarray(
+            [
+                xyz_canonical_coords_to_pyrobot_coords((x, y, 1))
+                for x, y in zip(xs.ravel(), ys.ravel())
+            ]
+        )
+
         depth = np.ones((p, p))
-        #rgb = np.float32(np.random.rand(p, p, 3) * 255)
+        # rgb = np.float32(np.random.rand(p, p, 3) * 255)
         rgb = im
 
         rgb_d = RGBDepth(rgb, depth, pts)
@@ -347,10 +300,10 @@ class RoboarmAgent(DroidletAgent):
 if __name__ == "__main__":
     base_path = os.path.dirname(__file__)
     parser = ArgumentParser("Roboarm", base_path)
-    
+
     opts = parser.parse()
-    print (opts.ip, opts.backend)
-    #opts.ip = "172.23.42.96"
+    print(opts.ip, opts.backend)
+    # opts.ip = "172.23.42.96"
     print(f"IP::: Roboarm AGENT {opts.ip}")
 
     logging.basicConfig(level=opts.log_level.upper())
@@ -362,11 +315,10 @@ if __name__ == "__main__":
     logging.info("LOG LEVEL: {}".format(logger.level))
 
     # Check that models and datasets are up to date
-    #if not opts.dev:
-     #   try_download_artifacts(agent="locobot")
+    # if not opts.dev:
+    #   try_download_artifacts(agent="locobot")
 
-    #set_start_method("spawn", force=True)
+    # set_start_method("spawn", force=True)
 
     sa = RoboarmAgent(opts)
     sa.start()
-
