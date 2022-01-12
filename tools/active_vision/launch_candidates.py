34151380c726c4a0b34bd54ead6ccccbdf2d1340
Anurag <apratik@fb.com>
2022-01-11 16:05:34 -0800

    functional pipeline

---
+++ tools/active_vision/launch_candidates.py
@@ -0,0 +1,25 @@
+#!/bin/env bash
+set -ex
+
+if ! source activate /private/home/apratik/miniconda3/envs/droidlet; then
+    echo "source activate not working, trying conda activate"
+    source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh || true
+    conda activate /private/home/apratik/miniconda3/envs/droidlet
+fi
+
+data_dir=$1
+# Base dir for all jobs
+base_dir=/checkpoint/${USER}/jobs/reexplore
+mkdir -p $base_dir
+dt=$(date '+%d-%m-%Y/%H:%M:%S');
+
+out_dir=$base_dir/respawnv3
+# job_dir=$base_dir/$dt
+# echo """"""""""""""""""""""""""""""
+# echo Job Directory $job_dir
+# mkdir -p $job_dir
+# echo """"""""""""""""""""""""""""""
+
+cd /private/home/apratik/fairo/tools/active_vision
+chmod +x find_respawn_loc.py
+python3.7 find_respawn_loc.py --data_dir $data_dir --out_dir $out_dir --slurm 
---
+++ tools/active_vision/launch_lp_train.py
@@ -0,0 +1,30 @@
+#!/bin/env bash
+set -ex
+
+if ! source activate /private/home/apratik/miniconda3/envs/droidlet; then
+    echo "source activate not working, trying conda activate"
+    source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh || true
+    conda activate /private/home/apratik/miniconda3/envs/droidlet
+fi
+
+# /checkpoint/apratik/jobs/reexplore/respawnv1/baselinev3_noisy
+# /checkpoint/apratik/jobs/reexplore/respawnv1/baselinev3
+
+data_dir=/checkpoint/apratik/jobs/reexplore/respawnv1/baselinev3
+# Base dir for all jobs
+base_dir=/checkpoint/${USER}/jobs/reexplore/labelprop
+
+dt=$(date '+%d-%m-%Y/%H:%M:%S');
+job_dir=$base_dir/$dt
+echo """"""""""""""""""""""""""""""
+echo Job Directory $job_dir
+mkdir -p $job_dir
+echo """"""""""""""""""""""""""""""
+
+cd /private/home/apratik/fairo/tools/active_vision
+
+chmod +x run_label_prop.py
+# python run_label_prop.py --data_dir $data_dir --job_dir $job_dir
+
+chmod +x prep_and_run_training.py
+python prep_and_run_training.py --data_dir $data_dir --job_dir $job_dir
