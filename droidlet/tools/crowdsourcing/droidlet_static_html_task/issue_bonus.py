21d26d3052ab24af9971a541af102de448509458
Ethan Carlson <ethancarlson@fb.com>
2022-01-21 13:54:53 -0800

    fix bonus script to not rely on external files

--- droidlet/tools/crowdsourcing/droidlet_static_html_task/issue_bonus.py
+++ droidlet/tools/crowdsourcing/droidlet_static_html_task/issue_bonus.py
@@ -5,53 +5,61 @@
 # LICENSE file in the root directory of this source tree.
 
 import argparse
-import os
+import logging
+import json
 
 from mephisto.abstractions.databases.local_database import LocalMephistoDB
 from mephisto.tools.data_browser import DataBrowser
 from mephisto.data_model.worker import Worker
 
-def main(run_id, performance_dir) -> list:
-    # Import bonus list, formatted "$Worker $Bonus" each line, and convert to dict
-    bonus_path = os.path.join(performance_dir, "performance_bonuses.txt")
-    with open(bonus_path, "r") as f:
-            bonus_list = f.readlines()
-    bonus_dict = {}
-    for bonus in bonus_list:
-        turker = bonus.split(' ')[0]
-        amount = float(bonus.split(' ')[1][:-1])
-        if turker in bonus_dict:
-            bonus_dict[turker].append(amount)
-        else: bonus_dict[turker] = [amount]
-    print(bonus_dict)
+logging.basicConfig(level="INFO")
+
+db = LocalMephistoDB()
+data_browser = DataBrowser(db=db)
+
+def main(run_id) -> list:
+    logging.info(f"Initializing bonus script for Mephisto job {run_id}")
 
     # Get completed units from the run_id
-    db = LocalMephistoDB()
     units = db.find_units(task_run_id=run_id)
     completed_units = []
     for unit in units:
         if unit.db_status == "completed":
             completed_units.append(unit)
 
-    #Issue bonuses and return the results
-    data_browser = DataBrowser(db=db)
+    logging.info(f"Completed units for job {run_id} retrieved")
+
+    # Retrieve bonus info from DB and issue
     bonus_results = []
+    total_bonus = 0
     for unit in completed_units:
         data = data_browser.get_data_from_unit(unit)
         worker = Worker(db, data["worker_id"])
-        if worker.worker_name in bonus_dict:  #Edge case that a worker completed a HIT with no score reported
-            amount = bonus_dict[worker.worker_name].pop(0) # Order doesn't really matter here, but they should be filled and popped the same
-            bonus_result, _ = worker.bonus_worker(amount, "Virtual assistant interaction quality bonus", unit)
-            bonus_results.append(bonus_result)
+        outputs = data["data"]["outputs"]
+        clean_click_string = outputs["clickedElements"].replace("'", "")
+        clicks = json.loads(clean_click_string)
+        if clicks:
+            for click in clicks:
+                if "interactionScores" in click["id"]:
+                    amount = float(f'{(click["id"]["interactionScores"]["stoplight"] * 0.30):.2f}')
+                    total_bonus += amount
+                    bonus_result, _ = worker.bonus_worker(amount, "Virtual assistant interaction quality bonus", unit)
+                    bonus_results.append(bonus_result)
+                    if not bonus_result:
+                        logging.info(f"Bonus NOT successfully issued for worker {worker.worker_name} , debug")
+        else:
+            logging.info(f'Recorded click data not found for {worker.worker_name}, no bonus will be issued')
 
-    print(bonus_results)
+    logging.info(f"Num completed units: {len(completed_units)}")
+    logging.info(f"Num bonuses issued: {len([x for x in bonus_results if x])}")
+    logging.info(f"Total bonus amount issued: {total_bonus}")
+
     return bonus_results
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--run_id", type=int, help="integer mephisto run ID", required=True)
-    parser.add_argument("--performance_dir", type=str, help="directory containing 'performance_bonuses.txt'", required=True)
     args = parser.parse_args()
 
-    main(args.run_id, args.performance_dir)
+    main(args.run_id)
