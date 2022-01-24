fe44a7b11c15d645c88216af9138015c832409b2
ExhAustin <aswang@andrew.cmu.edu>
2022-01-24 13:27:31 -0800

    Remove timestamp tests, check timestamp in joint test

--- polymetis/tests/scripts/utils/test_utils.py
+++ polymetis/tests/scripts/utils/test_utils.py
@@ -5,9 +5,15 @@
 import polymetis_pb2
 
 
-def check_episode_log(episode_log, timesteps):
+def check_episode_log(episode_log, timesteps, check_timestamps=False):
     assert len(episode_log) == (
         timesteps
     ), f"episode length={len(episode_log)}, but expected={timesteps}"
     assert type(episode_log) is list
     assert type(episode_log[0]) is polymetis_pb2.RobotState
+
+    if check_timestamps:
+        for i in range(len(episode_log) - 1):
+            timestamp_prev = episode_log[i].timestamp.ToDatetime()
+            timestamp_curr = episode_log[i + 1].timestamp.ToDatetime()
+            assert (timestamp_curr - timestamp_prev).microseconds > 0
