92eea6e8168c021c3b4e66a6d0823601f0ab84e8
aszlam <aszlam@fb.com>
2022-01-26 15:13:02 -0500

    allow holes in data gen (#953)
    * allow holes in data gen
    
    * bugfixes, move holes around to make them more visible

--- droidlet/lowlevel/minecraft/small_scenes_with_shapes.py
+++ droidlet/lowlevel/minecraft/small_scenes_with_shapes.py
@@ -14,6 +14,8 @@ SL = 17
 GROUND_DEPTH = 5
 H = 13
 
+HOLE_NAMES = ["RECTANGULOID", "ELLIPSOID", "SPHERE"]
+
 
 def bid():
     return (35, np.random.randint(16))
@@ -45,7 +47,7 @@ def agent_look(args, blocks):
 
 
 def build_base_world(sl, h, g, fence=False):
-    W = []
+    W = {}
     for i in range(sl):
         for j in range(g):
             for k in range(sl):
@@ -53,7 +55,7 @@ def build_base_world(sl, h, g, fence=False):
                     idm = red()
                 else:
                     idm = white()
-                W.append(((i, j, k), idm))
+                W[(i, j, k)] = idm
     return W
 
 
@@ -81,7 +83,7 @@ def build_shape_scene(args):
     """
     fence = getattr(args, "fence", False)
     blocks = build_base_world(args.SL, args.H, args.GROUND_DEPTH, fence=fence)
-    num_shapes = np.random.randint(1, args.MAX_NUM_SHAPES + 1)
+    num_shapes = np.random.randint(0, args.MAX_NUM_SHAPES + 1)
     occupied_by_shapes = {}
     inst_segs = []
     for t in range(num_shapes):
@@ -98,14 +100,44 @@ def build_shape_scene(args):
                 if ln[0] < args.SL and ln[1] < args.H and ln[2] < args.SL:
                     ln = tuple(ln.tolist())
                     if not occupied_by_shapes.get(ln):
-                        blocks.append((ln, idm))
+                        blocks[ln] = idm
                         inst_seg.append(ln)
                         occupied_by_shapes[ln] = True
         inst_segs.append({"tags": [shape], "locs": inst_seg})
-
+    num_holes = np.random.randint(0, args.MAX_NUM_GROUND_HOLES)
+    # TODO merge contiguous holes
+    ML = args.SL
+    mL = 0
+    if args.fence:
+        ML -= 1
+        mL = 1
+    for t in range(num_holes):
+        shape = random.choice(HOLE_NAMES)
+        opts = SHAPE_OPTION_FUNCTION_MAP[shape]()
+        S = SHAPE_FNS[shape](**opts)
+        m = np.round(np.mean([l for l, idm in S], axis=0)).astype("int32")
+        miny = min([l[1] for l, idm in S])
+        maxy = max([l[1] for l, idm in S])
+        offsets = np.random.randint((args.SL, args.H, args.SL))
+        offsets[0] -= m[0]
+        offsets[2] -= m[2]
+        # offset miny to GROUND_DEPTH - radius of shape
+        offsets[1] = args.GROUND_DEPTH - maxy // 2 - 1
+        inst_seg = []
+        for l, idm in S:
+            ln = np.add(l, offsets)
+            if ln[0] >= mL and ln[1] >= 0 and ln[2] >= mL:
+                if ln[0] < ML and ln[1] < args.GROUND_DEPTH and ln[2] < ML:
+                    ln = tuple(ln.tolist())
+                    if not occupied_by_shapes.get(ln):
+                        blocks[ln] = (0, 0)
+                        inst_seg.append(ln)
+                        occupied_by_shapes[ln] = True
+        inst_segs.append({"tags": ["hole"], "locs": inst_seg})
     J = {}
     # not shifting y for gridworld
     o = (args.cuberite_x_offset, 0, args.cuberite_z_offset)
+    blocks = [(l, idm) for l, idm in blocks.items()]
     blocks = shift(blocks, o)
     for i in inst_segs:
         i["locs"] = shift(i["locs"], o)
@@ -134,6 +166,7 @@ if __name__ == "__main__":
     parser.add_argument("--GROUND_DEPTH", type=int, default=GROUND_DEPTH)
     parser.add_argument("--MAX_NUM_SHAPES", type=int, default=3)
     parser.add_argument("--NUM_SCENES", type=int, default=3)
+    parser.add_argument("--MAX_NUM_GROUND_HOLES", type=int, default=0)
     parser.add_argument("--fence", action="store_true", default=False)
     parser.add_argument("--cuberite_x_offset", type=int, default=-SL // 2)
     parser.add_argument("--cuberite_y_offset", type=int, default=63 - GROUND_DEPTH)
