9bbe6254a7f49a6eedcccfd9c655e4bb2be4fd53
aszlam <aszlam@fb.com>
2022-01-28 23:22:00 -0500

    Spatial map memory (#937)
    * shallow refactor, move ExaminedDetection from global to member of memory
    
    * forgot file
    
    * skeleton of how maps should look in place_field
    
    * fill out skeleton some
    
    * forgot some fields
    
    * fix bugs
    
    * more functional place_field, first (incomplete) integration into craftassist agent
    
    * remove obstacle from place_field map when all blocks in column removed in mc
    
    * integrate in robot
    
    * test scripts (#954)
    
    * tests, bugfixes, change format of self.memid2locs
    
    * bugfix
    
    * grr
    
    Co-authored-by: anuragprat1k <57542204+anuragprat1k@users.noreply.github.com>

--- droidlet/interpreter/robot/default_behaviors.py
+++ droidlet/interpreter/robot/default_behaviors.py
@@ -4,36 +4,40 @@ Copyright (c) Facebook, Inc. and its affiliates.
 import logging
 from droidlet.interpreter.robot import tasks
 from droidlet.memory.robot.loco_memory_nodes import DetectedObjectNode
-from droidlet.lowlevel.robot_mover_utils import ExaminedMap
 import os
 import random
 import numpy as np
 import shutil
 import json
 
-random.seed(2021) # fixing a random seed to fix default exploration goal
+random.seed(2021)  # fixing a random seed to fix default exploration goal
+
 
 def get_distant_goal(x, y, t, l1_thresh=35):
     # Get a distant goal for the slam exploration
-    # Pick a random quadrant, get 
+    # Pick a random quadrant, get
     while True:
         xt = random.randint(-19, 19)
         yt = random.randint(-19, 19)
-        d = np.linalg.norm(np.asarray([x,y]) - np.asarray([xt,yt]), ord=1)
+        d = np.linalg.norm(np.asarray([x, y]) - np.asarray([xt, yt]), ord=1)
         if d > l1_thresh:
             return (xt, yt, 0)
 
+
 def init_logger():
-    logger = logging.getLogger('default_behavior')
+    logger = logging.getLogger("default_behavior")
     logger.setLevel(logging.INFO)
-    fh = logging.FileHandler('default_behavior.log', 'w')
+    fh = logging.FileHandler("default_behavior.log", "w")
     fh.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)s - %(funcName)s(): %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s %(filename)s:%(lineno)s - %(funcName)s(): %(message)s"
+    )
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     logger.addHandler(ch)
+
 
 init_logger()
 
@@ -66,46 +70,49 @@ def start_explore(agent, goal):
         agent.mover.slam.reset_map()
         agent.mover.nav.reset_explore()
 
-        logger = logging.getLogger('default_behavior')
+        logger = logging.getLogger("default_behavior")
         logger.info(
             f"Starting exploration {explore_count} \
             first_exploration_done {first_exploration_done} \
             os.getenv('CONTINUOUS_EXPLORE') {os.getenv('CONTINUOUS_EXPLORE', 'False')}"
         )
 
+        # FIXME, don't clear the memory, place_field, etc.  explore more reasonably
         # Clear memory
         objects = DetectedObjectNode.get_all(agent.memory)
-        logger.info(f'Clearing {len(objects)} memids in memory')
+        logger.info(f"Clearing {len(objects)} memids in memory")
         agent.memory.clear(objects)
-        ExaminedMap.clear()
+        agent.memory.place_field.clear_examined()
         # reset object id counter
         agent.perception_modules["vision"].vision.deduplicate.object_id_counter = 1
         objects = DetectedObjectNode.get_all(agent.memory)
-        logger.info(f'{len(objects)} memids in memory')
-        
-        task_data = { 
-            "goal": goal, 
-            "save_data": os.getenv('SAVE_EXPLORATION', 'False') == 'True',
+        logger.info(f"{len(objects)} memids in memory")
+
+        task_data = {
+            "goal": goal,
+            "save_data": os.getenv("SAVE_EXPLORATION", "False") == "True",
             "data_path": os.path.join(f"{os.getenv('HEURISTIC', 'default')}", str(explore_count)),
         }
-        logger.info(f'task_data {task_data}')
-        
-        if os.path.isdir(task_data['data_path']):
-            shutil.rmtree(task_data['data_path'])
+        logger.info(f"task_data {task_data}")
 
-        if os.getenv('HEURISTIC') in ('straightline', 'circle'):
-            logging.info('Default behavior: Curious Exploration')
+        if os.path.isdir(task_data["data_path"]):
+            shutil.rmtree(task_data["data_path"])
+
+        if os.getenv("HEURISTIC") in ("straightline", "circle"):
+            logging.info("Default behavior: Curious Exploration")
             agent.memory.task_stack_push(tasks.CuriousExplore(agent, task_data))
         else:
-            logging.info('Default behavior: Default Exploration')
+            logging.info("Default behavior: Default Exploration")
             agent.memory.task_stack_push(tasks.Explore(agent, task_data))
 
-        add_or_replace(agent, 'first_exploration_done', 'True')
-        add_or_replace(agent, 'explore_count', str(explore_count+1))
+        add_or_replace(agent, "first_exploration_done", "True")
+        add_or_replace(agent, "explore_count", str(explore_count + 1))
+
+
 
 def explore(agent):
-    x,y,t = agent.mover.get_base_pos()
-    goal = get_distant_goal(x,y,t)
+    x, y, t = agent.mover.get_base_pos()
+    goal = get_distant_goal(x, y, t)
     start_explore(agent, goal)
 
 def get_task_data(agent):
