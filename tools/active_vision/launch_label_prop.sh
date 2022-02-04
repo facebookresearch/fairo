670f3c9e8cca088bc7d0c8cbb156b7e09dc13e9e
Anurag <apratik@fb.com>
2022-02-04 08:01:53 -0800

    label prop

---
+++ tools/active_vision/launch_label_prop.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+set -ex
+# Runs label prop on the reexploration data.
+# Usage
+# ./launch_label_prop.sh <path that is output dir of ./launch_candidate_selection.sh> <setting for noise>
+# ./launch_label_prop.sh /checkpoint/apratik/jobs/reexplore/test2/baselinev3
+# ./launch_label_prop.sh /checkpoint/apratik/jobs/reexplore/respawnv1/baselinev3_noisy --noisy
+
+if ! source activate /private/home/apratik/miniconda3/envs/droidlet; then
+    echo "source activate not working, trying conda activate"
+    source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh || true
+    conda activate /private/home/apratik/miniconda3/envs/droidlet
+fi
+
+data_dir=$1
+# Base dir for all jobs
+base_dir=/checkpoint/${USER}/jobs/reexplore/labelprop
+
+dt=$(date '+%d-%m-%Y/%H:%M:%S');
+job_dir=$base_dir/$dt
+echo """"""""""""""""""""""""""""""
+echo Job Directory $job_dir
+mkdir -p $job_dir
+echo """"""""""""""""""""""""""""""
+
+cd /private/home/apratik/fairo/tools/active_vision
+
+chmod +x run_label_prop.py
+python run_label_prop.py --data_dir $data_dir --job_dir $job_dir
