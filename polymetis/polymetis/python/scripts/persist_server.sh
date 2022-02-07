c4465b0bf19bcb0af0379a96d6a734c43ac7fd0b
Giri Anantharaman <giriman@fb.com>
2022-02-07 18:43:03 -0500

    Update script

--- polymetis/polymetis/python/scripts/persist_server.sh
+++ polymetis/polymetis/python/scripts/persist_server.sh
@@ -4,24 +4,38 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT TERM
+
+function cleanup() {
+  echo "=== (Cleanup): Killing server and clients ==="
+  pkill -9 run_server
+  pkill -9 ".*franka.*"
+}
+
+function ctrl_c() {
+  cleanup
+  exit 130
+}
+
+. /home/box/miniconda3/etc/profile.d/conda.sh
+conda activate nuc_polymetis_env
+
 while true
 do
-        echo "Running $(which launch_robot.py)"
-        $(which launch_robot.py) robot_client=franka_hardware &
+  echo "=== Running $(which launch_robot.py) ==="
+  $(which launch_robot.py) robot_client=franka_hardware &
 
-        sleep 10
-        while true
-        do
-                $(which ping_server.py)
-                ret=$?
-                if [ $ret -ne 0 ]; then
-                    echo "=== Server died! Restarting server... ==="
-                    echo "(Cleanup): Killing server and clients"
-                    sudo pkill -9 run_server
-                    sudo pkill -9 ".*franka.*"
-
-                    break
-                fi
-                sleep 2
-        done
+  sleep 10
+  while true
+  do
+    $(which ping_server.py)
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "=== Server died! Restarting server... ==="
+        cleanup
+        break
+    fi
+    sleep 2
+  done
 done
