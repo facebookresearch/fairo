7261ee371c44947457e9ab7733e78d7a2b49766c
Giri Anantharaman <giriman@fb.com>
2022-02-09 13:46:25 -0500

    WIP

--- polymetis/polymetis/python/scripts/last_state_timestep.py
+++ polymetis/polymetis/python/scripts/last_state_timestep.py
@@ -1,5 +1,17 @@
-import a0
+#!/usr/bin/env python
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import time
+import logging
+import a0
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
 
 if __name__ == "__main__":
     s = a0.SubscriberSync("latest_robot_state", a0.INIT_MOST_RECENT)
