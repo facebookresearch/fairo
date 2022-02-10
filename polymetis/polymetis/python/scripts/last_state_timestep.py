e80a5b2dc4a81836f49dd0470179b0f5642b8673
Yixin Lin <yixin1996@gmail.com>
2022-02-09 22:48:23 -0500

    WIP

--- polymetis/polymetis/python/scripts/last_state_timestep.py
+++ polymetis/polymetis/python/scripts/last_state_timestep.py
@@ -14,7 +14,7 @@
 
 
 if __name__ == "__main__":
-    log.info("here")
+    print("here")
     # s = a0.SubscriberSync("latest_robot_state", a0.INIT_MOST_RECENT)
 
     # seconds = int(s.read().payload)
