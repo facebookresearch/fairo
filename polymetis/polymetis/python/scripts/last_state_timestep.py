a7e2b2c499d957e3d4dac5cb5d5d283a15643be4
Yixin Lin <yixin1996@gmail.com>
2022-02-09 22:45:37 -0500

    WIP

--- polymetis/polymetis/python/scripts/last_state_timestep.py
+++ polymetis/polymetis/python/scripts/last_state_timestep.py
@@ -4,13 +4,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import time
-import logging
-import a0
+# import time
+# import logging
+# import a0
 
-logging.basicConfig()
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+# logging.basicConfig()
+# log = logging.getLogger(__name__)
+# log.setLevel(logging.INFO)
 
 
 if __name__ == "__main__":
