c54a1c601061bd9d772bcaccadc65435ff075ccb
Yixin Lin <yixin1996@gmail.com>
2022-02-09 22:56:47 -0500

    WIP

--- polymetis/polymetis/python/scripts/last_state_timestep.py
+++ polymetis/polymetis/python/scripts/last_state_timestep.py
@@ -4,26 +4,16 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# import time
-# import logging
-# import a0
-
-# logging.basicConfig()
-# log = logging.getLogger(__name__)
-# log.setLevel(logging.INFO)
+import time
+import a0
 
 
 if __name__ == "__main__":
-    pass
-    # print("here")
-    # s = a0.SubscriberSync("latest_robot_state", a0.INIT_MOST_RECENT)
+    s = a0.SubscriberSync("latest_robot_state", a0.INIT_MOST_RECENT)
 
-    # seconds = int(s.read().payload)
+    seconds = int(s.read().payload)
 
-    # time_diff = time.time() - seconds
-    # log.info(f"Last robot state retrieved within {time_diff}s.")
+    time_diff = time.time() - seconds
 
-    # num_seconds_stale = 10
-    # assert (
-    #     time_diff < num_seconds_stale
-    # ), f"Robot state too stale by {time_diff}s, expected within {num_seconds_stale}s."
+    num_seconds_stale = 10
+    assert time_diff < num_seconds_stale
