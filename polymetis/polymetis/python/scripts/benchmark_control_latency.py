31986a134a8f106218c19b7c88c363011d1c44b8
ExhAustin <aswang@andrew.cmu.edu>
2022-02-23 16:18:30 -0800

    Include success rate to benchmarking script

--- polymetis/polymetis/python/scripts/benchmark_control_latency.py
+++ polymetis/polymetis/python/scripts/benchmark_control_latency.py
@@ -12,15 +12,20 @@ def output_episode_stats(episode_name, robot_states):
     latency_max = np.max(latency_arr)
     latency_min = np.min(latency_arr)
 
+    success_arr = np.array(
+        [robot_state.prev_command_successful for robot_state in robot_states]
+    )
+    success_rate = np.mean(success_arr)
+
     print(
-        f"{episode_name}: {latency_mean:.4f}/ {latency_std:.4f} / {latency_max:.4f} / {latency_min:.4f}"
+        f"{episode_name}: {latency_mean:.4f}/ {latency_std:.4f} / {latency_max:.4f} / {latency_min:.4f} / {100 * success_rate:.2f}%"
     )
 
 
 if __name__ == "__main__":
     robot = RobotInterface()
 
-    print("Control loop latency stats in milliseconds (avg / std / max / min): ")
+    print("Control loop stats in milliseconds (avg / std / max / min / success_rate): ")
 
     # Test joint PD
     robot_states = robot.move_to_joint_positions(robot.get_joint_positions())
