6422692a1f63ec4f43bf3c1f8f19c309c512a7f0
lshamis <leonid.shamis@gmail.com>
2022-02-28 19:23:29 -0800

    Update conda to use py38

--- create_conda.sh
+++ create_conda.sh
@@ -9,11 +9,11 @@ conda env remove -n droidlet -y
 
 conda install mamba -c conda-forge -y
 
-mamba create -n droidlet python=3.7 --file conda.txt --file agents/locobot/conda.txt \
+mamba create -n droidlet python=3.8 --file conda.txt --file agents/locobot/conda.txt \
       -c pytorch -c aihabitat -c open3d-admin -c conda-forge  -y
 
 conda activate droidlet
-mamba install https://anaconda.org/aihabitat/habitat-sim/0.2.1/download/linux-64/habitat-sim-0.2.1-py3.7_headless_linux_fc7fb11ccec407753a73ab810d1dbb5f57d0f9b9.tar.bz2
+mamba install https://anaconda.org/aihabitat/habitat-sim/0.2.1/download/linux-64/habitat-sim-0.2.1-py3.8_headless_linux_fc7fb11ccec407753a73ab810d1dbb5f57d0f9b9.tar.bz2
 
 python setup.py develop
 
