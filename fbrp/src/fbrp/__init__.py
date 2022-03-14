777f32c92c6b1e3e594ff29eeb0b9b8e2562f68f
Leonid Shamis <leonid.shamis@gmail.com>
2022-03-14 13:40:38 -0700

    remove debug info

--- fbrp/src/fbrp/__init__.py
+++ fbrp/src/fbrp/__init__.py
@@ -20,8 +20,6 @@ def main(*args):
     except SystemExit as sys_exit:
         if sys_exit.code == 0:
             return
-        print(sys.exc_info())
-        print(dir(sys.exc_info()[1]))
         raise RuntimeError(
             f"fbrp.main failed with exit code {sys_exit.code}"
         ) from sys_exit
