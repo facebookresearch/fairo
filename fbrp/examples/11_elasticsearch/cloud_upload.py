23f0baaebee9ebd01e614eff70a7178e620ed1c3
Tingfan <tingfan@fb.com>
2022-03-16 02:34:38 -0700

    [cloudlog] add job queue for upload

--- fbrp/examples/11_elasticsearch/cloud_upload.py
+++ fbrp/examples/11_elasticsearch/cloud_upload.py
@@ -2,6 +2,12 @@ import a0
 import boto3
 import os
 import signal
+import queue
+import threading
+import time
+import logging
+boto3.set_stream_logger('boto3.client', logging.CRITICAL)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 logpath = a0.cfg(a0.env.topic(), "/logpath", str)
@@ -21,11 +27,26 @@ with open(str(credential), 'r') as f:
             k, v=line.split()[1].split('=')[:2]
             cloud_credential[k]=v.strip('\'').strip('"')
 
-s3 = boto3.resource('s3',
-                    aws_access_key_id=cloud_credential['AWS_ACCESS_KEY_ID'],
-                    aws_secret_access_key=cloud_credential['AWS_SECRET_ACCESS_KEY'],
-                    aws_session_token=cloud_credential['AWS_SESSION_TOKEN'])
-bucket = s3.Bucket(str(bucket))
+job_queue = queue.Queue()
+
+def uploader():
+    # create individual session for each thread for threadsafety
+    session = boto3.session.Session(
+                aws_access_key_id=cloud_credential['AWS_ACCESS_KEY_ID'],
+                aws_secret_access_key=cloud_credential['AWS_SECRET_ACCESS_KEY'],
+                aws_session_token=cloud_credential['AWS_SESSION_TOKEN'])
+    s3 = session.client('s3')
+    while True:
+        (path, dst) = job_queue.get()
+        print(f'starting {path} -> {dst}')
+        s3.upload_file(Filename=path, Bucket=str(bucket), Key=dst)
+        print(f'uploaded {path} -> {dst}')
+    
+
+num_worker = 1
+for i in range(num_worker):
+    threading.Thread(target=uploader).start()
+
 
 def onlogfile(path):
     if os.path.basename(path).startswith("."):
@@ -33,9 +54,8 @@ def onlogfile(path):
 
     relpath = os.path.relpath(path, str(logpath))
     dst = os.path.join(str(prefix), relpath)
-    print(dst)
-    # Upload to AWS.
-    bucket.upload_file(Filename=path, Key=dst)
+    print(f'enque {path} -> {dst}')
+    job_queue.put((path, dst))
 
 
 def main():
@@ -43,6 +63,9 @@ def main():
 
     pattern = os.path.join(str(logpath), "**", "*.a0")
     d = a0.Discovery(pattern, onlogfile)
+    while True:
+        time.sleep(10)
+        print(f'queue size {job_queue.qsize()}')
     signal.pause()
 
 
