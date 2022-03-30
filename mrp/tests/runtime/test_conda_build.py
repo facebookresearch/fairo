5da79701e2aff95a644151c8f431cf880ab89a41
lshamis <leonid.shamis@gmail.com>
2022-03-29 17:58:25 -0700

    [mrp] setup_commands part of env (#1065)
    * [mrp] setup_commands part of env
    
    * circleci doesn't have bc
    
    * verbose test
    
    * handle early death

--- mrp/tests/runtime/test_conda_build.py
+++ mrp/tests/runtime/test_conda_build.py
@@ -1,6 +1,18 @@
 import a0
 import mrp
 import mrp.process_def
+import os
+import shutil
+import subprocess
+
+
+def reset_state(*procs):
+    mrp.process_def.defined_processes.clear()
+    for proc in procs:
+        shutil.rmtree(
+            os.path.expanduser(f"~/.config/mrp/conda/mrp_{proc}"), ignore_errors=True
+        )
+
 
 def read_logs(topic):
     output = []
@@ -12,17 +24,15 @@ def read_logs(topic):
     return output
 
 
-def test_conda_build():
-    # Reset defined processes.
-    mrp.process_def.defined_processes.clear()
+def test_conda_nobuild():
+    reset_state("proc")
+
+    proc_def = mrp.process(name="proc")
 
     # Define process to run Python 3.7.11
-    mrp.process(
-        name="proc",
-        runtime=mrp.Conda(
-            dependencies=["python=3.7.11"],
-            run_command=["python", "--version"],
-        ),
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.7.11"],
+        run_command=["python", "--version"],
     )
 
     # Run the proc and wait for it to complete.
@@ -31,16 +41,10 @@ def test_conda_build():
 
     assert read_logs("proc") == ["Python 3.7.11\n"]
 
-    # Reset defined processes.
-    mrp.process_def.defined_processes.clear()
-
     # Define process to run Python 3.8.8
-    mrp.process(
-        name="proc",
-        runtime=mrp.Conda(
-            dependencies=["python=3.8.8"],
-            run_command=["python", "--version"],
-        ),
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        run_command=["python", "--version"],
     )
 
     # Run the proc without building.
@@ -50,7 +54,109 @@ def test_conda_build():
     assert read_logs("proc") == ["Python 3.7.11\n"]
 
     # Run the proc with building.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        run_command=["python", "--version"],
+    )
+
     mrp.cmd.up("proc", reset_logs=True)
     mrp.cmd.wait("proc")
 
     assert read_logs("proc") == ["Python 3.8.8\n"]
+
+
+def test_conda_build_cache():
+    reset_state("proc")
+
+    proc_def = mrp.process(name="proc")
+
+    test_counter_path = "/tmp/test_counter"
+    test_counter_increment_command = [
+        "bash",
+        "-c",
+        "echo $(( $(cat /tmp/test_counter) + 1 )) > /tmp/test_counter",
+    ]
+    open(test_counter_path, "w").write("0")
+
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.7.11"],
+        setup_commands=[test_counter_increment_command],
+        run_command=[],
+    )
+
+    # Run the proc and wait for it to complete.
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should have run and incremented the counter.
+    assert open(test_counter_path).read() == "1\n"
+
+    # Run the proc again with no changes.
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should NOT have run and the counter should be unchanged.
+    assert open(test_counter_path).read() == "1\n"
+
+    # Defining the same process shouldn't change anything.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.7.11"],
+        setup_commands=[test_counter_increment_command],
+        run_command=[],
+    )
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should NOT have run and the counter should be unchanged.
+    assert open(test_counter_path).read() == "1\n"
+
+    # Changing the dependencies should cause a rebuild.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        setup_commands=[test_counter_increment_command],
+        run_command=[],
+    )
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should have run and the counter should be incremented.
+    assert open(test_counter_path).read() == "2\n"
+
+    # Changing the setup command should cause a rebuild.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        setup_commands=[["echo", "0"], test_counter_increment_command],
+        run_command=[],
+    )
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should have run and the counter should be incremented.
+    assert open(test_counter_path).read() == "3\n"
+
+    # Remake with identical setup.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        setup_commands=[["echo", "0"], test_counter_increment_command],
+        run_command=[],
+    )
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should NOT have run and the counter should be incremented.
+    assert open(test_counter_path).read() == "3\n"
+
+    # Update the conda history, poisoning the cache.
+    subprocess.run(["conda", "install", "-n", "mrp_proc", "-y", "pycparser"])
+
+    # Catch that the conda env has been updated.
+    proc_def.runtime = mrp.Conda(
+        dependencies=["python=3.8.8"],
+        setup_commands=[["echo", "0"], test_counter_increment_command],
+        run_command=[],
+    )
+    mrp.cmd.up("proc", reset_logs=True)
+    mrp.cmd.wait("proc")
+
+    # The setup should have run and the counter should be incremented.
+    assert open(test_counter_path).read() == "4\n"
