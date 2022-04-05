c7c08ea594720099f395e13435241a9ca3cb3720
Yixin Lin <1heart@users.noreply.github.com>
2022-04-05 17:00:18 -0400

    Custom definition of eyehandcal workspace (#1076)
    * Eye-hand calibration mods
    
    * Configurable time_to_go, num_points
    
    * Add +x for record_calibration_points.py
    
    * Add sample calibration_points.json
    
    * Add script
    
    * Update readme with instructions
    
    * Remove cartesian impedance start
    
    * Update readme.md

--- perception/sandbox/eyehandcal/scripts/collect_data_and_cal.py
+++ perception/sandbox/eyehandcal/scripts/collect_data_and_cal.py
@@ -6,27 +6,24 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-
-
 from math import pi
 import time
 import os 
 import pickle
+import json
 
 import numpy as np
 import torch
 import cv2
 
 from torchcontrol.transform import Rotation as R
-from torchcontrol.transform import Transformation as T
 from polymetis import RobotInterface
 from realsense_wrapper import RealsenseAPI
 
-from eyehandcal.utils import detect_corners, quat2rotvec, build_proj_matrix, mean_loss, find_parameter, rotmat
-   
+from eyehandcal.utils import detect_corners, quat2rotvec, build_proj_matrix, mean_loss, find_parameter, rotmat, dist_in_hull
 
 
-def realsense_images(max_pixel_diff=100):
+def realsense_images(max_pixel_diff=200):
     rs = RealsenseAPI()
     num_cameras = rs.get_num_cameras()
     assert num_cameras > 0, "no camera found"
@@ -56,24 +53,15 @@ def realsense_images(max_pixel_diff=100):
         count+=1
         yield [img.copy() for img in imgs1], intrinsics
 
+def sample_poses_from_data(xyz_points, orient_points, num_points):
+    points = dist_in_hull(points=xyz_points, n=num_points)
+    for point in points:
+        for orient in orient_points:
+            pos = torch.Tensor(point)
+            ori = R.from_quat(torch.Tensor(orient))
+            yield pos, ori
 
-
-def sample_poses(overhead_cameras=True):
-    hand_mount_yaw_offset = -pi/4
-    for x in np.linspace(0.3, 0.5, 2):
-        for y in np.linspace(-0.2, 0.2, 2):
-            for z in np.linspace(0.2, 0.4, 2):
-                for yaw in np.linspace(-pi/4, pi/4, 3):
-                    if overhead_cameras:
-                        pos_sampled = torch.Tensor([x, y, z+.1])
-                        ori_sampled = R.from_rotvec(torch.Tensor([pi/2, 0, 0])) * R.from_rotvec(torch.Tensor([0, 0, hand_mount_yaw_offset + yaw]))
-                    else:
-                        pos_sampled = torch.Tensor([x, y, z])
-                        ori_sampled = R.from_rotvec(torch.Tensor([0, 0, hand_mount_yaw_offset + yaw]))*R.from_rotvec(torch.Tensor([pi, 0, 0]))
-                    yield pos_sampled, ori_sampled
-
-
-def robot_poses(ip_address, pose_generator):
+def robot_poses(ip_address, pose_generator, time_to_go=3):
     # Initialize robot interface
     robot = RobotInterface(
         ip_address=ip_address,
@@ -83,23 +71,27 @@ def robot_poses(ip_address, pose_generator):
     # Get reference state
     robot.go_home()
     for i, (pos_sampled, ori_sampled) in enumerate(pose_generator):
-        print( f"Moving to pose ({i}): pos={pos_sampled}, quat={ori_sampled.as_quat()}")
-        state_log = robot.move_to_ee_pose(
-            position=pos_sampled,
-            orientation=ori_sampled.as_quat(),
-            time_to_go = 3
-        )
         while True:
-            pos0, quat0 = robot.get_ee_pose()
-            time.sleep(1)
-            pos1, quat1 = robot.get_ee_pose()
-            diffpos = (pos0-pos1).norm()
-            if diffpos < 0.01:
-                break
-            print(f'robot moving diffpos={diffpos}')
+            print( f"Moving to pose ({i}): pos={pos_sampled}, quat={ori_sampled.as_quat()}")
 
-        print(f"Current pose  pos={pos0}, quat={quat0}")
-        yield pos1, quat1
+            state_log = robot.move_to_ee_pose(position=pos_sampled,orientation=ori_sampled.as_quat(),time_to_go = time_to_go)
+            print(f"Length of state_log: {len(state_log)}")
+            if len(state_log) != time_to_go * robot.hz:
+                print(f"State log incorrect length. Trying again...")
+            else:
+                while True:
+                    pos0, quat0 = robot.get_ee_pose()
+                    time.sleep(1)
+                    pos1, quat1 = robot.get_ee_pose()
+                    diffpos = (pos0-pos1).norm()
+                    if diffpos < 0.01:
+                        break
+                    print(f'robot moving diffpos={diffpos}')
+
+                print(f"Current pose  pos={pos0}, quat={quat0}")
+                yield pos1, quat1
+                break
+    robot.go_home()
 
 
 # helper function
@@ -122,17 +114,18 @@ def extract_obs_data_std(data, camera_index):
     return obs_data_std, K
 
 
-
 if __name__ == '__main__':
     import argparse
     parser=argparse.ArgumentParser()
 
-    parser.add_argument('-o', '--overheadcam', default=False, action='store_true')
     parser.add_argument('--ip', default='100.96.135.68', help="robot ip address")
     parser.add_argument('--datafile', default='caldata.pkl', help="file to either load or save camera data")
     parser.add_argument('--overwrite', default=False, action='store_true', help="overwrite existing datafile, if it exists")
-    parser.add_argument('--target-marker-id', default=9, type=int, help="ID of the ARTag marker in the image")
-    parser.add_argument('--calibration-file', default='calibration.pkl', help="file to save final calibration data")
+    parser.add_argument('--marker-id', default=9, type=int, help="ID of the ARTag marker in the image")
+    parser.add_argument('--calibration-file', default='calibration.json', help="file to save final calibration data")
+    parser.add_argument('--points-file', default='calibration_points.json', help="file to load convex hull to sample points from")
+    parser.add_argument('--num-points', default=20, type=int, help="number of points to sample from convex hull")
+    parser.add_argument('--time-to-go', default=3, type=float, help="time_to_go in seconds for each movement")
     parser.add_argument('--imagedir', default=None, help="folder to save debug images")
 
     args=parser.parse_args()
@@ -148,8 +141,12 @@ if __name__ == '__main__':
 
         data = []
         img_gen=realsense_images()
-        pose_gen=sample_poses(overhead_cameras=args.overheadcam)
-        for i, (pos,ori) in enumerate(robot_poses(args.ip, pose_gen)):
+        points = json.load(open(args.points_file, 'r'))
+        xyz_points = np.array(points["xyz"])
+        orient_points = np.array(points["quat"])
+        pose_gen = sample_poses_from_data(xyz_points, orient_points, num_points=args.num_points)
+        poses = robot_poses(args.ip, pose_gen, args.time_to_go)
+        for i, (pos,ori) in enumerate(poses):
             imgs, intrinsics=next(img_gen)
 
             if args.imagedir is not None:
@@ -171,7 +168,7 @@ if __name__ == '__main__':
 
     print(f"Done. Data has {len(data)} poses.")
 
-    corner_data = detect_corners(data, target_idx=args.target_marker_id)
+    corner_data = detect_corners(data, target_idx=args.marker_id)
 
     num_of_camera=len(corner_data[0]['intrinsics'])
     params=[]
@@ -181,7 +178,10 @@ if __name__ == '__main__':
         print('number of images with keypoint', len(obs_data_std))
         param=torch.zeros(9, dtype=torch.float64, requires_grad=True)
         L = lambda param: mean_loss(obs_data_std, param, K)
-        param_star=find_parameter(param, obs_data_std, K)
+        try:
+            param_star=find_parameter(param, obs_data_std, K)
+        except:
+            continue
 
         print('found param loss (mean pixel err)', L(param_star).item())
         params.append(param_star)
@@ -198,6 +198,6 @@ if __name__ == '__main__':
             param_list.append(result)
             print(f"Camera {i} calibration: {result}")
         
-        with open(args.calibration_file, 'wb') as f:
+        with open(args.calibration_file, 'w') as f:
             print(f"Saving calibrated parameters to {args.calibration_file}")
-            pickle.dump(param_list, f)
+            json.dump(param_list, f)
