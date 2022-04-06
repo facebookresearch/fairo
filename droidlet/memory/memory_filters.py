7bb7d7ba0df4ba121b973a96c2a37b870e32a8ce
jacklanchantin <jacklanchantin@gmail.com>
2022-04-06 16:19:22 -0700

    append string directly if get_all=False

--- droidlet/memory/memory_filters.py
+++ droidlet/memory/memory_filters.py
@@ -473,10 +473,11 @@ class MemorySearcher:
                 for m in memids:
                     prop_vals = get_property_value(agent_memory, m, aname, get_all=True)
                     if prop_vals:
-                        if type(prop_vals) is str:
-                            prop_vals = [prop_vals]
-                        for prop_val in prop_vals:
-                            values_dict[m].append(prop_val)
+                        if type(prop_vals) is str: # if get_all=False, prop_vals is a str
+                            values_dict[m].append(prop_vals)
+                        else:
+                            for prop_val in prop_vals:
+                                values_dict[m].append(prop_val)
             return sum(values_dict.values(), [])
 
     def search(self, agent_memory, query=None, default_memtype="ReferenceObject"):
