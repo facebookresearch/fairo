bda773d9be0778fbf1c4fde71141c1874bd35e27
jacklanchantin <jacklanchantin@gmail.com>
2022-04-06 17:27:58 -0700

    re-add if statement

--- droidlet/memory/memory_filters.py
+++ droidlet/memory/memory_filters.py
@@ -470,14 +470,15 @@ class MemorySearcher:
                             attribute_name_list
                         )
                     )
-                for m in memids:
-                    prop_vals = get_property_value(agent_memory, m, aname, get_all=True)
-                    if prop_vals:
-                        if type(prop_vals) is list:
-                            for prop_val in prop_vals:
-                                values_dict[m].append(prop_val)
-                        else:
-                            values_dict[m].append(prop_vals)
+                if len(attribute_name_list) == 1:
+                    for m in memids:
+                        prop_vals = get_property_value(agent_memory, m, aname, get_all=True)
+                        if prop_vals:
+                            if type(prop_vals) is list:
+                                for prop_val in prop_vals:
+                                    values_dict[m].append(prop_val)
+                            else:
+                                values_dict[m].append(prop_vals)
             # TODO switch everything to dicts
             return sum(values_dict.values(), [])
 
