55d290eebabe8e9e7c2d631e4bcc3c99d3904c0a
ExhAustin <aswang@andrew.cmu.edu>
2022-04-28 16:25:39 -0700

    Add stress test file

---
+++ polymetis/scripts/jitload_stress_test.py
@@ -0,0 +1,27 @@
+from typing import Dict
+import time
+
+import torch
+import polymetis
+import torchcontrol as toco
+
+MAX_TRIES = 10
+
+
+class TmpPolicy(toco.PolicyModule):
+    def __init__(self):
+        super().__init__()
+        self.data = torch.rand([10000, 100])
+
+    def forward(self, robot_state: Dict[str, torch.Tensor]):
+        if not self.is_terminated():
+            self.set_terminated()
+        return {"joint_torques": torch.zeros(7)}
+
+
+robot = polymetis.RobotInterface()
+try:
+    for _ in range(MAX_TRIES):
+        robot.send_torch_policy(TmpPolicy())
+except KeyboardInterrupt:
+    pass
