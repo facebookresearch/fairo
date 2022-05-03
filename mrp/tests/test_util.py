cfda3b4687f273e5d4a0d3185b9c7c75a6fec2a0
ExhAustin <aswang@andrew.cmu.edu>
2022-05-03 15:04:53 -0700

    Use threading conditions instead of time.sleep

--- mrp/tests/test_util.py
+++ mrp/tests/test_util.py
@@ -1,5 +1,5 @@
 import os
-import time
+import threading
 import pytest
 
 import a0
@@ -23,24 +23,26 @@ def test_noescape():
     assert util.shell_join(["a", util.NoEscape(";b"), "c"]) == "a ;b c"
 
 
-class TestLogger:
-    def _listener_callback(self, pkt):
-        self.buffer = dict(pkt.headers)
+@pytest.mark.parametrize("logger_type", [util.stdout_logger, util.stderr_logger])
+def test_logger(logger_type):
+    test_topic = "TEST"
+    os.environ["A0_TOPIC"] = test_topic
 
-    @pytest.mark.parametrize("logger_type", [util.stdout_logger, util.stderr_logger])
-    def test_logger(self, logger_type):
-        self.buffer = None
-        test_topic = "TEST"
-        os.environ["A0_TOPIC"] = test_topic
+    # Set up listener
+    buffer = {}
+    cv = threading.Condition()
 
-        # Set up listener
-        listener = a0.LogListener(test_topic, self._listener_callback)
+    def listener_callback(pkt):
+        with cv:
+            buffer.update(dict(pkt.headers))
+            cv.notify()
 
-        log = logger_type()
-        log("test log msg")
-        time.sleep(0.1)  # wait for log to publish
+    listener = a0.LogListener(test_topic, listener_callback)
+    log = logger_type()
+    log("test log msg")
 
-        # Test packet contents to ensure content-type is included
-        assert self.buffer is not None
-        assert "content-type" in self.buffer
-        assert self.buffer["content-type"] == "text/plain"
+    # Test packet contents to ensure content-type is included
+    with cv:
+        cv.wait_for(lambda: buffer, timeout=1.0)
+    assert "content-type" in buffer
+    assert buffer["content-type"] == "text/plain"
