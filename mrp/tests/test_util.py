0aa76e44d336370542f58d86dfc1e1638fa4ce99
ExhAustin <aswang@andrew.cmu.edu>
2022-05-03 14:35:32 -0700

    Add logger test

--- mrp/tests/test_util.py
+++ mrp/tests/test_util.py
@@ -1,3 +1,9 @@
+import os
+import time
+import pytest
+
+import a0
+
 from mrp import util
 
 
@@ -15,3 +21,26 @@ def test_escape():
 
 def test_noescape():
     assert util.shell_join(["a", util.NoEscape(";b"), "c"]) == "a ;b c"
+
+
+class TestLogger:
+    def _listener_callback(self, pkt):
+        self.buffer = {h[0]: h[1] for h in pkt.headers}
+
+    @pytest.mark.parametrize("logger_type", [util.stdout_logger, util.stderr_logger])
+    def test_logger(self, log_func):
+        self.buffer = None
+        test_topic = "TEST"
+        os.environ["A0_TOPIC"] = test_topic
+
+        # Set up listener
+        listener = a0.LogListener(a0.LogTopic(test_topic), self._listener_callback)
+
+        log = logger_type()
+        log("test log msg")
+        time.sleep(0.1)  # wait for log to publish
+
+        # Test packet contents to ensure content-type is included
+        assert self.buffer is not None
+        assert "content-type" in self.buffer
+        assert self.buffer["content-type"] == "text/plain"
