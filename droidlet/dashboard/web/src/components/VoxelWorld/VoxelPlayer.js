4bdf6ad69d573e436762f24af1fb20a63af33457
Ethan Carlson <ethancarlson@fb.com>
2022-05-04 09:09:17 -0700

    VoxelPlayer initial commit

---
+++ droidlet/dashboard/web/src/components/VoxelWorld/VoxelPlayer.js
@@ -0,0 +1,67 @@
+var skin = require('minecraft-skin');  // Can change this to be an import
+
+
+class VoxelPlayer {
+    constructor (world, img, skinOpts) {
+        this.possessed;
+    
+        skinOpts.scale = skinOpts.scale || new world.THREE.Vector3(0.04, 0.04, 0.04);  // Check scale assumptions
+        this.playerSkin = new skin(world.THREE, img, skinOpts);
+        this.player = this.playerSkin.mesh;  // Returns player object
+        
+        this.player.position.set(0, 562, -20);  // Check starting position assumptions
+        world.scene.add(player);
+        
+        world.control(player);  // To think about more
+        
+        this.pov = 1;   
+    }
+
+    move(x, y, z) {  // Still need this fx?
+        var xyz = parseXYZ(x, y, z);
+        this.player.position.x += xyz.x;
+        this.player.position.y += xyz.y;
+        this.player.position.z += xyz.z;
+    }
+
+    moveTo(x, y, z) {
+        var xyz = parseXYZ(x, y, z);
+        this.player.position.x = xyz.x;
+        this.player.position.y = xyz.y;
+        this.player.position.z = xyz.z;
+    }
+
+    updatePov(type) {
+        if (type === 'first' || type === 1) {
+            this.pov = 1;
+        }
+        else if (type === 'third' || type === 3) {
+            this.pov = 3;
+        }
+        this.possess();
+    }
+
+    toggle() {
+        this.updatePov(this.pov === 1 ? 3 : 1);
+    }
+
+    possess() {  
+        if (this.possessed) this.possessed.remove(world.camera);
+        var key = this.pov === 1 ? 'cameraInside' : 'cameraOutside';
+        this.player[key].add(world.camera);
+        this.possessed = this.player[key];
+    }
+
+};
+
+function parseXYZ (x, y, z) {
+    if (typeof x === 'object' && Array.isArray(x)) {
+        return { x: x[0], y: x[1], z: x[2] };
+    }
+    else if (typeof x === 'object') {
+        return { x: x.x || 0, y: x.y || 0, z: x.z || 0 };
+    }
+    return { x: Number(x), y: Number(y), z: Number(z) };
+}
+
+export default VoxelPlayer;
