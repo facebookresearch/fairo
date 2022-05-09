hacks to fix radians/degrees bug

agents/craftassist/craftassist_agent.py
@@ -357,11 +357,12 @@ class CraftAssistAgent(DroidletAgent):
     ###FIXME!!
     #    self.get_incoming_chats = self.get_chats
 
+    # WARNING!! this is in degrees.  agent's memory stores looks in radians.
+    # FIXME: normalize, switch in DSL to radians.
     def relative_head_pitch(self, angle):
         """Converts assistant's current pitch and yaw
         into a pitch and yaw relative to the angle."""
-        # warning: pitch is flipped!
-        new_pitch = self.get_player().look.pitch - angle
+        new_pitch = np.rad2deg(self.get_player().look.pitch) - angle
         self.set_look(self.get_player().look.yaw, new_pitch)
 
     def send_chat(self, chat: str):
@@ -370,7 +371,9 @@ class CraftAssistAgent(DroidletAgent):
         chat_json = False
         try:
             chat_json = json.loads(chat)
-            chat_text = list(filter(lambda x: x["id"] == "text", chat_json["content"]))[0]["content"]
+            chat_text = list(filter(lambda x: x["id"] == "text", chat_json["content"]))[0][
+                "content"
+            ]
         except:
             chat_text = chat
 
@@ -379,7 +382,7 @@ class CraftAssistAgent(DroidletAgent):
 
         if chat_json:
             chat_json["chat_memid"] = chat_memid
-            chat_json["timestamp"] = round(datetime.timestamp(datetime.now())*1000)
+            chat_json["timestamp"] = round(datetime.timestamp(datetime.now()) * 1000)
             # Send the socket event to show this reply on dashboard
             sio.emit("showAssistantReply", chat_json)
         else:

droidlet/lowlevel/minecraft/craftassist_mover.py
@@ -28,7 +28,9 @@ def flip_x(struct, floor=False):
 
 
 def flip_look(struct):
-    return Look(-struct.yaw, -struct.pitch)
+    yaw = -np.deg2rad(struct.yaw)
+    pitch = -np.deg2rad(struct.pitch)
+    return Look(yaw, pitch)
 
 
 def maybe_flip_x_or_look(struct, floor=False):
@@ -148,12 +150,11 @@ class CraftassistMover:
 
     # FIXME!! turn_angle is broken in the cagent; should be swapping here,
     # but cagent actually has it backwards
-    # turn_angle isn't being used in other parts of agent.
     def turn_angle(self, yaw):
         self.cagent.turn_angle(yaw)
 
     def set_look(self, yaw, pitch):
-        self.cagent.set_look(-yaw, pitch)
+        self.cagent.set_look(-yaw, -pitch)
 
     def look_at(self, x, y, z):
         self.cagent.look_at(-x, y, z)
@@ -257,8 +258,8 @@ def from_droidlet_xyz_to_minecraft(xyz):
 
 
 def from_minecraft_look_to_droidlet(look):
-    return Look(-look.yaw, look.pitch)
+    return Look(-look.yaw, -look.pitch)
 
 
 def from_droidlet_look_to_craftassist(look):
-    return Look(-look.yaw, look.pitch)
+    return Look(-look.yaw, -look.pitch)