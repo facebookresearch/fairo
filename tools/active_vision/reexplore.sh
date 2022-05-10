00d555300982e124ea03e403aed45b186477700c
Anurag <apratik@fb.com>
2022-05-09 21:09:40 -0700

    user path

--- tools/active_vision/reexplore.sh
+++ tools/active_vision/reexplore.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 export NOISY_HABITAT=$3
 export ADD_HUMANS=False
-source activate /private/home/apratik/miniconda3/envs/droidlet
+source activate /private/home/${USER}/miniconda3/envs/droidlet
 ../../droidlet/lowlevel/locobot/remote/launch_pyro_habitat.sh 127.0.0.1 --scene_path /checkpoint/apratik/replica/apartment_0/habitat/mesh_semantic.ply
 
 export LOCOBOT_IP=127.0.0.1
 export SAVE_EXPLORATION=True
 export DATA_PATH=test
 export VISUALIZE_EXAMINE=True
-source activate /private/home/apratik/miniconda3/envs/droidlet
+source activate /private/home/${USER}/miniconda3/envs/droidlet
 python /private/home/soumith/fairo/agents/locobot/locobot_agent.py --data_store_path $1 --default_behavior reexplore --reexplore_json $2
