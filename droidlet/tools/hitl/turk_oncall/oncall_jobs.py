5b92478fd1aa5b19c04248830912ae8f92718458
Ethan Carlson <ethancarlson@fb.com>
2022-05-17 10:49:34 -0700

    update language, don't process S3 logs

--- droidlet/tools/hitl/turk_oncall/oncall_jobs.py
+++ droidlet/tools/hitl/turk_oncall/oncall_jobs.py
@@ -2,19 +2,12 @@
 Copyright (c) Facebook, Inc. and its affiliates.
 """
 
-import copy
 import logging
 import os
-import re
-import shutil
 import signal
 import subprocess
 import time
-
-from typing import List
-
 import boto3
-import botocore
 
 from droidlet.tools.hitl.turk_oncall.allocate_oncall_instances import (
     allocate_oncall_instances,
@@ -23,9 +16,7 @@ from droidlet.tools.hitl.turk_oncall.allocate_oncall_instances import (
 from droidlet.tools.hitl.utils.hitl_utils import (
     generate_batch_id,
     deregister_dashboard_subdomain,
-    dedup_commands,
 )
-from droidlet.tools.hitl.utils.process_s3_logs import read_s3_bucket, read_turk_logs
 
 from droidlet.tools.hitl.data_generator import DataGenerator
 from droidlet.tools.hitl.job_listener import JobListener
@@ -66,14 +57,14 @@ logger.addHandler(sh)
 
 class OnCallJob(DataGenerator):
     """
-    This Data Generator is responsible for spinning up Interaction Jobs.
+    This Data Generator is responsible for spinning up the Turk-As-Oncall version of Interaction Jobs.
 
     Each Interaction Job consists of several HITs. Turker are given dashboard sessions where they can
-    interact with craftassist agent for a period of time and annotate error commands they observed.
+    send a set of predetermined commands to the craftassist agent and mark whether the response was as expected.
 
     On a high level:
     - The input of this data generator is a request specifying how many dashboard sessions are created for turkers
-    - The output of this data generator is a list of (error) commands collected from all dashboard sessions
+    - The output of this data generator is a set of dashboard logs with the results of each session.
 
     """
 
@@ -119,7 +110,7 @@ class OnCallJob(DataGenerator):
 
         # Keep running Mephisto until timeout or job finished
         while not self.check_is_timeout() and p.poll() is None:
-            logging.debug(
+            logging.info(
                 f"[Oncall Job] Oncall Job still running...Remaining time: {self.get_remaining_time()}"
             )
             time.sleep(INTERACTION_JOB_POLL_TIME)
@@ -140,37 +131,8 @@ class OnCallJob(DataGenerator):
         logging.info(f"Free ECS instances...")
         free_ecs_instances(self.instance_ids)
 
-        logging.info(f"Processing S3 logs...")
-        self.process_s3_logs(batch_id)
-
         self.set_finished()
 
-    def process_s3_logs(self, batch_id) -> None:
-        """
-        This should be called after all dashboard sessions have completed.
-        It will read session data from S3, parse log files, collating commands into a single list
-        and finally upload the list to S3.
-        """
-        s3_logs_dir = os.path.join(HITL_TMP_DIR, f"{batch_id}/turk_logs")
-        parsed_logs_dir = os.path.join(HITL_TMP_DIR, f"{batch_id}/parsed_turk_logs")
-        os.makedirs(s3_logs_dir, exist_ok=True)
-        os.makedirs(parsed_logs_dir, exist_ok=True)
-
-        time.sleep(120)
-        rc = subprocess.call(
-            [f"aws s3 sync {S3_ROOT}/{batch_id}/interaction {s3_logs_dir}"], shell=True
-        )
-        time.sleep(120)
-
-        read_s3_bucket(s3_logs_dir, parsed_logs_dir)
-        command_list = read_turk_logs(parsed_logs_dir, NSP_OUTPUT_FNAME)
-        logging.info(f"command list from interactions: {command_list}")
-
-        logging.info(f"Uploading command list to S3...")
-        content = "\n".join(command_list)
-        s3.Object(f"{S3_BUCKET_NAME}", f"{batch_id}/collected_commands").put(Body=content)
-        content = "commands_ready"
-        s3.Object(f"{S3_BUCKET_NAME}", f"{batch_id}/commands_ready").put(Body=content)
 
     def get_batch_id(self):
         return self._batch_id
