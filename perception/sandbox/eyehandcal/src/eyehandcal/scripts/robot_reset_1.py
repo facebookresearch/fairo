57748194112d25482d7fc9939692689ea4e73242
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2022-05-18 18:34:04 -0400

    Add files via upload
    robot_reset_1.py tests the robot arm and polymetis server by moving the robot to a location and back to home.

---
+++ perception/sandbox/eyehandcal/src/eyehandcal/scripts/robot_reset_1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from ipaddress import ip_address
+from turtle import position
+from polymetis import RobotInterface#, GripperInterface
+import time
+import torch
+
+robot = RobotInterface(ip_address = "172.16.0.1")
+#gripper = GripperInterface(ip_address = "172.16.0.3")
+
+timeRobot = 5
+def robotRy():
+    robot.go_home()
+    print(" - Going home")
+    #gripper.goto(1.0,0.1,0.1)
+    #print(" - Gripper open")
+    time.sleep(2)
+    print(" - Robot moving")
+    state_log = robot.move_to_ee_pose(position=torch.Tensor([0.5,0.3,0.2]), orientation=None, time_to_go=timeRobot)
+    #print(" - Gripper close")
+    #gripper.grasp(speed=0.1, force=0.1)
+    time.sleep(2)
+    print(" - Going home")
+    robot.go_home()
+    time.sleep(2)
+    print(" - Robot moving")
+    state_log = robot.move_to_ee_pose(position=torch.Tensor([0.5,-0.3,0.2]), orientation=None, time_to_go=timeRobot)
+    #print(" - Gripper open")
+    #gripper.goto(1.0,0.1,0.1)
+    time.sleep(2)
+    print(" - Going home")
+    robot.go_home()
+count = 1
+while True:
+    print (count)
+    robotRy()
+    count += 1
