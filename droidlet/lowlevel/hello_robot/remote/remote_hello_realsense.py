Fix semantic rotation

droidlet/lowlevel/hello_robot/remote/remote_hello_realsense.py
@@ -168,8 +168,20 @@ class RemoteHelloRealsense(object):
         return color_image, depth_image
 
     def get_semantics(self, rgb, depth):
-        semantic_pred, vis = self.segmentation_model.get_prediction(rgb)
-        return semantic_pred, vis
+        """Get semantic segmentation."""
+        semantics, semantics_vis = self.segmentation_model.get_prediction(rgb)
+
+        # given RGB and depth are rotated after the point cloud creation, 
+        # we rotate them back here to align to the point cloud
+        depth = np.rot90(depth, k=1, axes=(0, 1))
+        semantics = np.rot90(semantics, k=1, axes=(0, 1))
+
+        # apply the same depth filter to semantics as we applied to the point cloud
+        semantics = semantics.reshape(-1, self.num_sem_categories)
+        valid = (depth > 0).flatten()
+        semantics = semantics[valid]
+
+        return semantics, semantics_vis
 
     def get_orientation(self):
         """Get discretized robot orientation."""
@@ -227,8 +239,11 @@ class RemoteHelloRealsense(object):
         rgb, depth = self.get_rgb_depth(rotate=False, compressed=False)
         opcd = self.get_open3d_pcd(rgb_depth=[rgb, depth])
         pcd = np.asarray(opcd.points)
+
+        # RGB and depth are rotated after the point cloud creation
         rgb = np.rot90(rgb, k=1, axes=(1, 0))
         depth = np.rot90(depth, k=1, axes=(1, 0))
+
         return pcd, rgb, depth
 
     def is_obstacle_in_front(self, return_viz=False):

droidlet/lowlevel/locobot/remote/remote_locobot.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 from scipy.spatial.transform import Rotation
 import logging
+from PIL import Image
 import quaternion
 import os
 import open3d as o3d
@@ -19,7 +20,7 @@ from droidlet.lowlevel.robot_mover_utils import (
     transform_pose,
 )
 from droidlet.dashboard.o3dviz import serialize as o3d_pickle
-from segmentation.constants import coco_categories
+from segmentation.constants import coco_categories, frame_color_palette
 from segmentation.semantic_prediction import SemanticPredMaskRCNN
 from habitat_utils import reconfigure_scene
 
@@ -461,11 +462,31 @@ class RemoteLocobot(object):
         if self.scene_contains_semantic_annotations:
             instance_segmentation = self.get_rgb_depth_segm()[2]
             semantic_segmentation = self.instance_id_to_category_id[instance_segmentation]
-            semantic_segmentation = self.one_hot_encoding[semantic_segmentation]
-            return semantic_segmentation, None
+            semantics = self.one_hot_encoding[semantic_segmentation]
+            semantics_vis = self.get_semantic_frame_vis(rgb, semantics)
         else:
-            semantic_pred, vis = self.segmentation_model.get_prediction(rgb)
-            return semantic_pred, vis
+            semantics, semantics_vis = self.segmentation_model.get_prediction(rgb)
+
+        # apply the same depth filter to semantics as we applied to the point cloud
+        semantics = semantics.reshape(-1, self.num_sem_categories)
+        valid = (depth > 0).flatten()
+        semantics = semantics[valid]
+
+        return semantics, semantics_vis
+
+    def get_semantic_frame_vis(self, rgb, semantics):
+        """Visualize first-person semantic segmentation frame."""
+        width, height = semantics.shape[:2]
+        vis_content = semantics
+        vis_content[:, :, -1] = 1e-5
+        vis_content = vis_content.argmax(-1)
+        vis = Image.new("P", (height, width))
+        vis.putpalette([int(x * 255.0) for x in frame_color_palette])
+        vis.putdata(vis_content.flatten().astype(np.uint8))
+        vis = vis.convert("RGB")
+        vis = np.array(vis)
+        vis = np.where(vis != 255, vis, rgb)
+        return vis
 
     def get_orientation(self):
         """Get discretized robot orientation."""

droidlet/lowlevel/locobot/remote/slam_service.py
@@ -7,14 +7,12 @@ import torch.nn as nn
 import numpy as np
 import Pyro4
 import select
-import cv2
 from rich import print
-from PIL import Image
 
 from slam_pkg.utils.map_builder import MapBuilder as mb
 from slam_pkg.utils import depth_util as du
 from skimage.morphology import disk, binary_dilation
-from segmentation.constants import coco_categories, frame_color_palette
+from segmentation.constants import coco_categories
 
 random.seed(0)
 torch.manual_seed(0)
@@ -117,18 +115,10 @@ class SLAM(object):
 
     def update_map(self):
         pcd, rgb, depth = self.robot.get_current_pcd()
-
-        semantics, semantics_vis = self.robot.get_semantics(rgb, depth)
-        if semantics_vis is not None:
-            self.last_semantic_frame = semantics_vis
-        else:
-            self.last_semantic_frame = self.get_semantic_frame_vis(rgb, semantics)
-        semantics = semantics.reshape(-1, self.num_sem_categories)
-        valid = (depth > 0).flatten()
-        semantics = semantics[valid]
+        semantics, self.last_semantic_frame = self.robot.get_semantics(rgb, depth)
+        pose = self.robot.get_base_state()
 
         self.map_builder.update_map(pcd)
-        pose = self.robot.get_base_state()
         self.map_builder.update_semantic_map(pcd, semantics, pose)
 
         # explore the map by robot shape
@@ -202,21 +192,6 @@ class SLAM(object):
     def get_orientation(self):
         """Get discretized robot orientation."""
         return self.robot.get_orientation()
-
-    def get_semantic_frame_vis(self, rgb, semantics):
-        """Visualize first-person semantic segmentation frame."""
-        width, height = semantics.shape[:2]
-        vis_content = semantics
-        vis_content[:, :, -1] = 1e-5
-        vis_content = vis_content.argmax(-1)
-
-        vis = Image.new("P", (height, width))
-        vis.putpalette([int(x * 255.0) for x in frame_color_palette])
-        vis.putdata(vis_content.flatten().astype(np.uint8))
-        vis = vis.convert("RGB")
-        vis = np.array(vis)
-        vis = np.where(vis != 255, vis, rgb)
-        return vis
 
     def reset_map(self, z_bins=None, obs_thr=None):
         self.map_builder.reset_map(self.map_size_cm, z_bins=z_bins, obs_thr=obs_thr)