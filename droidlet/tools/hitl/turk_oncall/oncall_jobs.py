669b7f81c4a69c981830a84fb2bbc70a13b1771b
Ethan Carlson <ethancarlson@fb.com>
2022-05-25 14:40:59 -0700

    parse local db and upload

--- droidlet/tools/hitl/turk_oncall/oncall_jobs.py
+++ droidlet/tools/hitl/turk_oncall/oncall_jobs.py
@@ -8,6 +8,7 @@ import signal
 import subprocess
 import time
 import boto3
+import yaml
 
 from droidlet.tools.hitl.turk_oncall.allocate_oncall_instances import (
     allocate_oncall_instances,
@@ -19,29 +20,35 @@ from droidlet.tools.hitl.utils.hitl_utils import (
 )
 
 from droidlet.tools.hitl.data_generator import DataGenerator
-from droidlet.tools.hitl.job_listener import JobListener
 from droidlet.tools.hitl.task_runner import TaskRunner
 
+from command_lists import COMMAND_LISTS
+
+from mephisto.abstractions.databases.local_database import LocalMephistoDB
+from mephisto.tools.data_browser import DataBrowser as MephistoDataBrowser
+from mephisto.data_model.worker import Worker
+
+db = LocalMephistoDB()
+mephisto_data_browser = MephistoDataBrowser(db=db)
 
 ECS_INSTANCE_TIMEOUT = 45
 INTERACTION_JOB_POLL_TIME = 30
-INTERACTION_LISTENER_POLL_TIME = 30
-HITL_TMP_DIR = (
-    os.environ["HITL_TMP_DIR"] if os.getenv("HITL_TMP_DIR") else f"{os.path.expanduser('~')}/.hitl"
-)
-S3_BUCKET_NAME = "droidlet-hitl"
-S3_ROOT = "s3://droidlet-hitl"
-NSP_OUTPUT_FNAME = "nsp_outputs"
-ANNOTATED_COMMANDS_FNAME = "nsp_data.txt"
 
+HITL_TMP_DIR = (
+    os.environ["HITL_TMP_DIR"]
+    if os.getenv("HITL_TMP_DIR")
+    else f"{os.path.expanduser('~')}/.hitl"
+)
+
+S3_BUCKET_NAME = "droidlet-hitl"
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
-s3 = boto3.resource(
+s3 = boto3.client(
     "s3",
-    region_name=AWS_DEFAULT_REGION,
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_DEFAULT_REGION,
 )
 
 log_formatter = logging.Formatter(
@@ -69,31 +76,56 @@ class OnCallJob(DataGenerator):
     """
 
     def __init__(
-        self, instance_num: int, image_tag: str, task_name: str, timeout: float = -1
+        self, instance_num: int, image_tag: str, timeout: float = -1
     ) -> None:
         super(OnCallJob, self).__init__(timeout)
         self._instance_num = instance_num
         self._image_tag = image_tag
-        self._task_name = task_name
         self.instance_ids = None
+        self.task_name = None
         self._batch_id = generate_batch_id()
 
     def run(self) -> None:
         batch_id = self._batch_id
 
+        # Edit Mephisto config file task name
+        with open(
+            "../../crowdsourcing/turk_as_oncall/hydra_configs/conf/run_with_qual.yaml",
+            "r",
+        ) as stream:
+            config = yaml.safe_load(stream)
+            task_name = f"ca-oncall{self._batch_id}"
+            self.task_name = task_name
+            config["mephisto"]["task"]["task_name"] = task_name
+        logging.info(
+            f"Updating Mephisto config file to have task_name: {task_name}"
+        )
+        with open(
+            "../../crowdsourcing/turk_as_oncall/hydra_configs/conf/run_with_qual.yaml",
+            "w",
+        ) as stream:
+            stream.write("#@package _global_\n")
+            yaml.dump(config, stream)
+
         # allocate AWS ECS instances and register DNS records
         logging.info(
-            f"Allocate AWS ECS instances, populate oncall data csv, and register DNS records..."
+            "Allocate AWS ECS instances, populate oncall data csv, and register DNS records..."
         )
         _, instance_ids = allocate_oncall_instances(
-            self._instance_num, batch_id, self._image_tag, self._task_name, ECS_INSTANCE_TIMEOUT
+            self._instance_num,
+            batch_id,
+            self._image_tag,
+            self.task_name,
+            ECS_INSTANCE_TIMEOUT,
         )
         self.instance_ids = instance_ids
 
         # run Mephisto to spin up & monitor turk jobs
-        logging.info(f"Start running Mephisto...")
+        logging.info("Start running Mephisto...")
         MEPHISTO_AWS_ACCESS_KEY_ID = os.environ["MEPHISTO_AWS_ACCESS_KEY_ID"]
-        MEPHISTO_AWS_SECRET_ACCESS_KEY = os.environ["MEPHISTO_AWS_SECRET_ACCESS_KEY"]
+        MEPHISTO_AWS_SECRET_ACCESS_KEY = os.environ[
+            "MEPHISTO_AWS_SECRET_ACCESS_KEY"
+        ]
         MEPHISTO_REQUESTER = os.environ["MEPHISTO_REQUESTER"]
         p = subprocess.Popen(
             [
@@ -116,7 +148,7 @@ class OnCallJob(DataGenerator):
             time.sleep(INTERACTION_JOB_POLL_TIME)
 
         # if mephisto is still running after job timeout, terminate it
-        logging.info(f"Manually terminate Mephisto after timeout...")
+        logging.info("Manually terminate Mephisto after timeout...")
         if p.poll() is None:
             os.killpg(os.getpgid(p.pid), signal.SIGINT)
             time.sleep(300)
@@ -124,22 +156,138 @@ class OnCallJob(DataGenerator):
             time.sleep(300)
             os.killpg(os.getpgid(p.pid), signal.SIGKILL)
 
-        # Deregister DNS records
-        logging.info(f"Deregister DNS records...")
+        logging.info("Deregister DNS records...")
         deregister_dashboard_subdomain(batch_id)
 
-        logging.info(f"Free ECS instances...")
+        logging.info("Free ECS instances...")
         free_ecs_instances(self.instance_ids)
+
+        logging.info(
+            "Retrieving results from local Mephisto DB and uploading to S3"
+        )
+        self.get_local_db_results()
 
         self.set_finished()
 
     def get_batch_id(self):
         return self._batch_id
 
+    def get_local_db_results(self):
+
+        NUM_CMDS = 3
+        results = [
+            "worker_name|list_id|q1_result|q1_feedback|q2_result|q2_feedback|q3_result|q3_feedback|general_feedback\n"
+        ]
+        softblock = []
+        command_dict = {}
+        feedback_dict = {}
+        summary_stats = []
+        avg_duration = 0
+        avg_usability = 0
+        units = mephisto_data_browser.get_units_for_task_name(self.task_name)
+        for unit in units:
+            data = mephisto_data_browser.get_data_from_unit(unit)
+            outputs = data["data"]["outputs"]
+
+            # General stats
+            worker_name = Worker.get(db, data["worker_id"]).worker_name
+            avg_duration += (
+                data["data"]["times"]["task_end"]
+                - data["data"]["times"]["task_start"]
+            ) / len(units)
+            avg_usability += int(outputs["usability-rating"]) / len(units)
+
+            # Task results
+            results_string = f"{worker_name}|"
+            list_id = int(outputs["listID"])
+            results_string += f"{list_id}|"
+            for i in range(NUM_CMDS):
+                # Store the command result
+                result = outputs[f"command_{i+1}"]
+                results_string += f"{result}|"
+                if command_dict.get((list_id, i), result) != result:
+                    command_dict[(list_id, i)] = "disagree"
+                else:
+                    command_dict[(list_id, i)] = result
+
+                # Store any command feedback
+                cmd_feedback = outputs[f"command_{i+1}_feedback"]
+                results_string += f"{cmd_feedback}|"
+                if result == "no":
+                    if not feedback_dict.get((list_id, i), None):
+                        feedback_dict[(list_id, i)] = []
+                    feedback_dict[(list_id, i)].append(cmd_feedback)
+
+                    # If they said the agent failed but didn't tell us why, softblock
+                    if not cmd_feedback:
+                        softblock.append(f"{worker_name}\n")
+
+            # Store general feedback
+            results_string += f"{outputs['feedback']}"
+            results.append(f"{results_string}\n")
+
+        # Build the summary stats list
+        summary_stats.append(f"Average Task Duration: {avg_duration:.2f}\n")
+        summary_stats.append(
+            f"Average Usability Rating: {avg_usability:.2f}\n"
+        )
+        summary_stats.append(
+            f"Number of Commands Passed: {list(command_dict.values()).count('yes')}\n"
+        )
+        summary_stats.append(
+            f"Number of Commands Failed: {list(command_dict.values()).count('no')}\n"
+        )
+        summary_stats.append(
+            f"Number of Commands Disagreed: {list(command_dict.values()).count('disagree')}\n\n"
+        )
+        summary_stats.append(
+            "List of failed commands (list_id|cmd_num|command_list|feedback):\n"
+        )
+        summary_stats.extend(
+            [
+                f"{key[0]}|{key[1]}|{COMMAND_LISTS[key[0]].replace('|', ',')}|{feedback_dict[key]}\n"
+                for key in command_dict.keys()
+                if command_dict[key] == "no"
+            ]
+        )
+
+        # Log locally and upload results to S3
+        local_dir = f"{HITL_TMP_DIR}/{self._batch_id}/oncall_results"
+        os.makedirs(local_dir, exist_ok=True)
+
+        with open(os.path.join(local_dir, "summary_stats.txt"), "w") as f:
+            f.writelines(summary_stats)
+        with open(os.path.join(local_dir, "results.txt"), "w") as f:
+            f.writelines(results)
+        with open(os.path.join(local_dir, "softblock.txt"), "w") as f:
+            f.writelines(softblock)
+
+        logging.info(f"Uploading job results to S3 folder: {self._batch_id}")
+        s3.upload_file(
+            f"{local_dir}/summary_stats.txt",
+            S3_BUCKET_NAME,
+            f"{self._batch_id}/summary_stats.txt",
+        )
+        s3.upload_file(
+            f"{local_dir}/results.txt",
+            S3_BUCKET_NAME,
+            f"{self._batch_id}/results.txt",
+        )
+        s3.upload_file(
+            f"{local_dir}/softblock.txt",
+            S3_BUCKET_NAME,
+            f"{self._batch_id}/softblock.txt",
+        )
+
 
 if __name__ == "__main__":
     runner = TaskRunner()
-    ocj = OnCallJob(instance_num=2, image_tag="oncall_v1", task_name="oncall_t2", timeout=30)
+    ocj = OnCallJob(
+        instance_num=2,
+        image_tag="oncall_v1",
+        task_name="oncall_t2",
+        timeout=30,
+    )
     batch_id = ocj.get_batch_id()
     runner.register_data_generators([ocj])
     runner.run()
