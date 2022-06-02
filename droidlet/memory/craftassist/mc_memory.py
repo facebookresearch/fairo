9e7f4ec251ac6fc5bf8aa4f759b59233d6dfa00b
Aqib M Mahfuz <aqib@devfair0830.h2.fair>
2022-06-02 16:32:34 -0700

    Bugfixes + MC Obstacle Mapping Implementation

--- droidlet/memory/craftassist/mc_memory.py
+++ droidlet/memory/craftassist/mc_memory.py
@@ -434,6 +434,8 @@ class MCAgentMemory(AgentMemory):
             count = c[0] + dn
             self.db_write("UPDATE ReferenceObjects SET voxel_count=? WHERE uuid=?", count, memid)
             return count
+        elif dn > 0:
+            self.db_write("INSERT INTO ReferenceObjects (voxel_count, uuid) VALUES (?, ?)", dn, memid)
         else:
             return None
 
