c15fbdfd1fcb9030493eebf5ef2aa66f09f4fc5b
Igor Molybog <igormolybog@fb.com>
2022-06-13 15:31:55 -0700

    test waiting times reduced
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- polymetis/tests/scripts/7_joint_velocities.py
+++ polymetis/tests/scripts/7_joint_velocities.py
@@ -11,7 +11,7 @@ from polymetis import RobotInterface
 
 def test_new_joint_vel(state_log, joint_vel_desired):
     for i, state in enumerate(state_log):
-        assert torch.allclose(torch.Tensor(state.joint_velocities), joint_vel_desired, atol=0.01), f"""iteration {i}:
+        assert torch.allclose(torch.Tensor(state.joint_velocities), joint_vel_desired, rtol=0.01, atol=0.01), f"""iteration {i}:
                         measured velocities {torch.Tensor(state.joint_velocities)},
                         desired velocities {joint_vel_desired}"""
 
@@ -27,19 +27,19 @@ if __name__ == "__main__":
     # Joint velocity control
     print("=== RobotInterface.start_joint_velocity_control ===")
 
-    joint_vel_desired = torch.Tensor([0.1, -0.1, -0.1, 0.0, 0.0, 0.1, 0.1])
+    joint_vel_desired = torch.Tensor([0.01, -0.01, -0.01, 0.0, 0.0, 0.01, 0.01])
 
     robot.start_joint_velocity_control(joint_vel_desired)
-    time.sleep(3)
+    time.sleep(0.6)
     state_log = robot.terminate_current_policy()
 
     hz = robot.metadata.hz
-    test_new_joint_vel(state_log[2*hz:], joint_vel_desired)
+    test_new_joint_vel(state_log[int(hz/2):], joint_vel_desired)
 
-    new_joint_vel_desired = torch.Tensor([-0.1, 0.1, 0.0, -0.1, 0.1, -0.1, -0.1])
+    new_joint_vel_desired = torch.Tensor([-0.01, 0.01, 0.0, -0.01, 0.01, -0.01, -0.01])
     robot.start_joint_velocity_control(joint_vel_desired)
-    time.sleep(1)
+    time.sleep(0.1)
     robot.update_desired_joint_velocities(new_joint_vel_desired)
-    time.sleep(5)
+    time.sleep(1)
     state_log = robot.terminate_current_policy()
-    test_new_joint_vel(state_log[5*hz:], new_joint_vel_desired)
+    test_new_joint_vel(state_log[int(1*hz):], new_joint_vel_desired)
