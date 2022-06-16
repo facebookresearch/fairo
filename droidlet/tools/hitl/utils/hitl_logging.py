5e6ad4c4b34c8e321cb4af6e647d6dca37ce1345
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-15 17:02:44 -0700

    update hitl logging, added a unit test of hitl logging

--- droidlet/tools/hitl/utils/hitl_logging.py
+++ droidlet/tools/hitl/utils/hitl_logging.py
@@ -1,27 +1,47 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+The hitl_logging.py include a HitlLogging class for logging in HiTL module.
+"""
+
+from datetime import datetime, timezone
 import logging
 import os
-
-"""
-Logger need:
-- Log file saved location 
-    - S3_BUCKET_NAME
-    - S3_ROOT
-- Class name
-- Format
-
-Logging - same as python logging
-"""
+import inspect
 
 HITL_TMP_DIR = (
     os.environ["HITL_TMP_DIR"] if os.getenv("HITL_TMP_DIR") else f"{os.path.expanduser('~')}/.hitl"
 )
 
-log_formatter = logging.Formatter(
+DEFAULT_LOG_FORMATTER = logging.Formatter(
     "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() %(levelname)s]: %(message)s"
 )
 
-class HitlLogger():
-    def __init__(self, logger_name: str, batch_id: int, formatter=log_formatter, level=logging.WARNING):
+class HitlLogging():
+    """
+    The HitlLogging class is a wrapper for the python basic logging, 
+    allows the caller class to registering for a logger name and logs into seperate files.
+
+    The log would be output to both console and a log file, the log file is located under the HiTL temporary directory 
+    following the below format:
+        {Hitl Tmp Dir}/{Batch Id}/{Logger Name}{Timestamp}.log
+
+    Parameters:
+        - batch_id:     required - batch_id of the hitl jobs
+        - logger_name:  optional, default is set to caller class name
+        - formatter:    optional, default is DEFAULT_LOG_FORMATTER 
+        - level:        optional, default is logging.WARNING (same as python logging module)
+    """
+    def __init__(self, batch_id: int, logger_name=None, formatter=DEFAULT_LOG_FORMATTER, level=logging.WARNING):
+        # Get caller class to use as logger name if logger name is not specified
+        if logger_name is None:
+            logger_name = inspect.stack()[1][0].f_locals["self"].__class__.__name__
+
+        # get timestamp to differentiate different instance 
+        timestamp = datetime.now(timezone.utc).isoformat()
+
+        logger_name = f"{logger_name}{timestamp}"
+
         log_dir = os.path.join(HITL_TMP_DIR, f"{batch_id}/pipeline_logs")
         os.makedirs(log_dir, exist_ok=True)
 
@@ -39,8 +59,12 @@ class HitlLogger():
         logger.addHandler(sh)
 
         self.__logger__ = logger
+        self.__log_file__ = log_file
     
     def get_logger(self):
         return self.__logger__
+
+    def get_log_file(self):
+        return self.__log_file__
 
 
---
+++ droidlet/tools/hitl/utils/tests/hitl_logging_test.py
@@ -0,0 +1,57 @@
+from droidlet.tools.hitl.utils.hitl_logging import HitlLogging
+from droidlet.tools.hitl.utils.hitl_utils import generate_batch_id
+import unittest
+import os
+
+# Generate a batch id        
+batch_id = generate_batch_id()
+msg = "I am a message"
+
+class TestLoggerClass1:
+    def __init__(self) -> None:
+        pass
+
+    def call_logging(self):
+        # Log a message
+        hl = HitlLogging(batch_id)
+        hl_logger = hl.get_logger()
+        hl_logger.error(msg)
+        return hl
+
+class TestLoggerClass2:
+    def __init__(self) -> None:
+        pass
+
+    def call_logging(self):
+        # Log a message
+        hl = HitlLogging(batch_id)
+        hl_logger = hl.get_logger()
+        hl_logger.warning(msg)
+        return hl
+
+class TestHitlLogging(unittest.TestCase):
+    def test_hitl_logging_3instances(self):
+        test_cl1 = TestLoggerClass1()
+        test_cl2 = TestLoggerClass2()
+        test_cl2_instance2 = TestLoggerClass2()
+
+        hl1 = test_cl1.call_logging()
+        hl2 = test_cl2.call_logging()
+        hl3 = test_cl2_instance2.call_logging()
+
+        # should generate 3 log files
+        log_files = []
+        log_files.append(hl1.get_log_file())
+        log_files.append(hl2.get_log_file())
+        log_files.append(hl3.get_log_file())
+
+
+        for log_fname in log_files:
+            self.assertTrue(os.path.exists(log_fname))
+            log_f = open(log_fname, 'r')
+            for line in log_f:
+                self.assertTrue(msg in line)
+            log_f.close()
+
+if __name__ == "__main__":
+    unittest.main()
