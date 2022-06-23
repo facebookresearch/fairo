ef2a89b62d9eeb18f7ea688c2b9956d69413b7d8
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-23 16:14:46 -0700

    ad impl for server to get log and get job info

--- droidlet/tools/hitl/dashboard_app/backend/dashboard_aws_helper.py
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_aws_helper.py
@@ -1,9 +1,14 @@
+import json
 import boto3
+import botocore
 import os
 import re
 
 S3_BUCKET_NAME = "droidlet-hitl"
 S3_ROOT = "s3://droidlet-hitl"
+HITL_TMP_DIR = (
+    os.environ["HITL_TMP_DIR"] if os.getenv("HITL_TMP_DIR") else f"{os.path.expanduser('~')}/.hitl"
+)
 
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
@@ -17,6 +22,26 @@ s3 = boto3.resource(
 
 bucket = s3.Bucket(S3_BUCKET_NAME)
 
+def _dowload_file(fname: str):
+    # check if exists on local tmp directory
+    local_file_name = os.path.join(HITL_TMP_DIR, fname)
+
+    if not os.path.exists(local_file_name): 
+        # reterive from s3
+        local_folder_name = local_file_name[: local_file_name.rindex("/")]
+        os.makedirs(local_folder_name, exist_ok=True)
+        try:    
+            s3.meta.client.download_file(S3_BUCKET_NAME, fname, local_file_name)
+        except botocore.exceptions.ClientError as e:
+            print("file not exists")
+
+    return local_file_name if os.path.exists(local_file_name) else None
+
+def _read_file(fname: str):
+    f = open(fname, "r")
+    content = f.read()
+    f.close()
+    return content
 
 def get_job_list():
     job_list = []
@@ -30,6 +55,17 @@ def get_job_list():
             job_list.append(int(prefix.get("Prefix")[:-1]))
     return job_list
 
+def get_traceback_by_id(job_id: int):
+    local_fname = _dowload_file(f"{job_id}/log_traceback.csv")
+    if local_fname is None:
+        return f"cannot find traceback with id {job_id}"
+    return _read_file(local_fname)
 
-def get_job_info(job_id: int):
-    pass
+def get_run_info_by_id(job_id: int):
+    local_fname = _dowload_file(f"job_management_records/{job_id}.json")
+    if local_fname is None:
+        return f"cannot find run info with id {job_id}"
+    f = open(local_fname)
+    json_data = json.load(f)
+    f.close()
+    return json_data
--- droidlet/tools/hitl/dashboard_app/backend/dashboard_server.py
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_server.py
@@ -1,5 +1,6 @@
+from enum import Enum
 import logging
-from droidlet.tools.hitl.dashboard_app.backend.dashboard_aws_helper import get_job_list
+from droidlet.tools.hitl.dashboard_app.backend.dashboard_aws_helper import get_job_list, get_run_info_by_id, get_traceback_by_id
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
@@ -7,14 +8,33 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+class DASHBOARD_EVENT(Enum):
+    """
+    server supported event types
+    """
+    GET_JOBS = "get_job_list"
+    GET_TRACEBACK = "get_traceback_by_id"
+    GET_RUN_INFO = "get_run_info_by_id"
 
-@socketio.on("get_job_list")
-def handle_message():
-    print("Request received: get_job_list")
+
+@socketio.on(DASHBOARD_EVENT.GET_JOBS.value)
+def get_jobs():
+    print(f"Request received: {DASHBOARD_EVENT.GET_JOBS.value}")
     job_list = get_job_list()
-    print(job_list)
-    emit("get_job_list", job_list)
+    print(f"Job list reterived from aws, sending job list (length:{len(job_list)}) to client")
+    emit(DASHBOARD_EVENT.GET_JOBS.value, job_list)
 
+@socketio.on(DASHBOARD_EVENT.GET_TRACEBACK.value)
+def get_traceback(job_id):
+    print(f"Request received: {DASHBOARD_EVENT.GET_TRACEBACK.value}")
+    log_content = get_traceback_by_id(int(job_id))
+    emit(DASHBOARD_EVENT.GET_TRACEBACK.value, log_content)
+
+@socketio.on(DASHBOARD_EVENT.GET_RUN_INFO.value)
+def get_info(job_id):
+    print(f"Request received: {DASHBOARD_EVENT.GET_RUN_INFO.value}")
+    run_info = get_run_info_by_id(int(job_id))
+    emit(DASHBOARD_EVENT.GET_RUN_INFO.value, run_info)
 
 if __name__ == "__main__":
     socketio.run(app)
