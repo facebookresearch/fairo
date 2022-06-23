92cd5463f42c0576443fffbfd7f11f0f2a8cd045
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-23 16:14:46 -0700

    ad impl for server to get log and get job info

--- droidlet/tools/hitl/dashboard_app/backend/dashboard_server.py
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_server.py
@@ -21,6 +21,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app, cors_allowed_origins="*")  # allow cors
 
+class DASHBOARD_EVENT(Enum):
+    """
+    server supported event types
+    """
+    GET_JOBS = "get_job_list"
+    GET_TRACEBACK = "get_traceback_by_id"
+    GET_RUN_INFO = "get_run_info_by_id"
 
 class DASHBOARD_EVENT(Enum):
     """
@@ -113,6 +120,17 @@ def get_interaction_session_log(id_info_json):
     emit(DASHBOARD_EVENT.GET_INTERACTION_SESSION_LOG.value, log)
 
 
+@socketio.on(DASHBOARD_EVENT.GET_TRACEBACK.value)
+def get_traceback(job_id):
+    print(f"Request received: {DASHBOARD_EVENT.GET_TRACEBACK.value}")
+    log_content = get_traceback_by_id(int(job_id))
+    emit(DASHBOARD_EVENT.GET_TRACEBACK.value, log_content)
+
+@socketio.on(DASHBOARD_EVENT.GET_RUN_INFO.value)
+def get_info(job_id):
+    print(f"Request received: {DASHBOARD_EVENT.GET_RUN_INFO.value}")
+    run_info = get_run_info_by_id(int(job_id))
+    emit(DASHBOARD_EVENT.GET_RUN_INFO.value, run_info)
 
 if __name__ == "__main__":
     socketio.run(app)
