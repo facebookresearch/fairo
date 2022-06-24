5739a9bc24e0fee3a9f05fd76af445e80ebd5673
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-24 15:35:48 -0700

    updated documentation

--- droidlet/tools/hitl/utils/job_management.py
+++ droidlet/tools/hitl/utils/job_management.py
@@ -1,5 +1,18 @@
 """
-Job management utils
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This file include job management utils for recording hitl job run statistics,
+including meta data like name, batch id, and job specific statistics like 
+failed/sucess command in interaction jobs, model accuracy in the nsp retrain jobs etc.
+
+This util class is extendable, if you want to add a new statistics for recording, 
+please do the following:
+    - To add a new meta data - please add a new entry in the MetaData enum.
+    - To add a new job type - please add a new entry in the Job enum.
+    - To add a new job statistic:
+        1. add a new entry in the JobStat enum.
+        2a. if this job statistic is shared by all jobs, add it to the STAT_FOR_ALL set.
+        2b. if this job belongs to a specific job, add it to the STAT_JOB_PAIR dict (key - corresonding job, val - a job statistics set).
 """
 import logging
 import boto3
@@ -117,6 +130,9 @@ DEFAULT_STAT_UPDATE_INTERVAL = 60
 
 
 def get_dashboard_version(image_tag: str):
+    """
+    get sha256 value of the input image_tag
+    """
     response = ecr.batch_get_image(
         registryId=AWS_ECR_REGISTRY_ID,
         repositoryName=AWS_ECR_REPO_NAME,
@@ -129,10 +145,19 @@ def get_dashboard_version(image_tag: str):
 
 
 def get_s3_link(batch_id: int):
+    """
+    get s3 link for the batch with batch_id input
+    """
     return f"https://s3.console.aws.amazon.com/s3/buckets/droidlet-hitl?region={AWS_DEFAULT_REGION}&prefix={batch_id}"
 
 
 class JobManagementUtil:
+    """
+    Job Management Util.
+
+    Each hitl pipeline run corresponds to a JobManagementUtil instance.
+    A stat_update_interal (in seconds) can be specificed for the minimal time to be wait for a repeatly update of the same statistic.
+    """
     def __init__(self, stat_update_interval=DEFAULT_STAT_UPDATE_INTERVAL):
         # prepare dict for recording the data
         rec_dict = {}
@@ -162,6 +187,9 @@ class JobManagementUtil:
         self._stat_update_interval = stat_update_interval
 
     def _set_time(self, time_type, job_type=None):
+        """
+        record corrent time for the time_type of specified job_type or metadata if job type is None.
+        """
         time_now = str(datetime.datetime.now())
 
         rec_dict = self._record_dict
@@ -184,19 +212,35 @@ class JobManagementUtil:
         self._save_tmp()
 
     def _save_tmp(self):
+        """
+        save json to local temporary folder.
+        """
         json.dump(self._record_dict, open(self._local_path, "w"))
 
     def set_meta_start(self):
+        """
+        record start time of the entire run
+        """
         self._set_time(MetaData.START_TIME)
 
     def set_meta_end(self):
+        """
+        record end time of the entire run
+        """
         self._set_time(MetaData.END_TIME)
 
     def set_meta_data(self, meta_data: MetaData, val):
+        """
+        set metadata to the value provided in the input parameter
+        """
         self._record_dict[meta_data._name_] = val
         self._save_tmp()
 
     def set_job_stat(self, job_type: Job, job_stat: JobStat, val, force_update=False):
+        """
+        set job statistic for the input job type to the value provided in the input parameter.
+        you can do a force update regardless of the stat_update_interal with the force_update set to True
+        """
         jname = job_type._name_
         sname = job_stat._name_
         curr_timestamp = datetime.datetime.now()
