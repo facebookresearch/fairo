2d0ee2ce79f1f5cd8a685f0846ad65ba13d78b35
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-28 14:15:18 -0700

    add backbone for pipeline metainfo and detail

--- droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/navbar.js
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/navbar.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import 'antd/dist/antd.css';
-import { Menu } from 'antd';
+import '../index.css';
+import { Menu, Typography } from 'antd';
 import { useLocation, useNavigate } from "react-router-dom";
 import { SUBPATHS } from "../constants/subpaths";
 
 const menuItems = Object.values(SUBPATHS);
+const {Title} = Typography;
 
 const NavBar = (props) => {
     const [, setCurrent] = useState('');
@@ -16,7 +18,20 @@ const NavBar = (props) => {
         navigate(`../${evt.key}`, {replace: true});
     }
 
-    return <Menu onClick={onClick} selectedKeys={location.pathname} mode="horizontal" items={menuItems} />;    
+    return (
+    <Menu theme="dark" onClick={onClick} selectedKeys={location.pathname} mode="horizontal"> 
+        <div className="logo">
+            <Title level={4} style={{"color": "white"}}>HITL Dashboard</Title>
+        </div>
+         {  
+            // navigation bar tabs
+             menuItems.map(item => 
+                <Menu.Item key={item.key}> 
+                    <div className="navbar-title-content">{item.label}</div>
+                </Menu.Item>
+            )
+         }
+    </Menu>);    
 }
 
 export default NavBar;
