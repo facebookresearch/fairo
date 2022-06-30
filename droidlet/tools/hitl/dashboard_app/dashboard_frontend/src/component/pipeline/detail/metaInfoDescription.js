bee2ddd3c39881524de0594cb9223f6de7b6328f
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-30 15:53:52 -0700

    add more navigation

---
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/metaInfoDescription.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MetaInfoDescription = (metaInfo) => {
+    return <div>{metaInfo}</div>
+}
+
+export default MetaInfoDescription;
---
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/utils/textUtils.js
@@ -0,0 +1,4 @@
+export const toFirstCapital = (word) => (
+    `${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}`
+);
+
