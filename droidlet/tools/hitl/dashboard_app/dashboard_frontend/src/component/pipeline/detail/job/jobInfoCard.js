fae8fcba702a4d48de70a5789b75955d3b896060
Chuxi Wang <wangchuxi94@gmail.com>
2022-06-30 16:47:47 -0700

    add info display

--- droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/job/jobInfoCard.js
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/job/jobInfoCard.js
@@ -1,15 +1,15 @@
-import { Button, Card, Typography } from "antd";
+import { Button, Card, Descriptions } from "antd";
 import React from "react";
 import { Link, useOutletContext, useParams } from "react-router-dom";
 import { toFirstCapital } from "../../../../utils/textUtils";
 
-const { Title } = Typography;
-
 const JobInfoCard = (props) => {
     const batchId = useParams().batch_id;
     const job = useParams().job;
-    const metaInfo = useOutletContext().metaInfo;
-    console.log(metaInfo);
+    const jobInfo = Object.entries(useOutletContext().metaInfo)
+        .filter((o) =>
+            o[0].toLowerCase() === job)[0][1];
+    console.log(jobInfo)
 
     // TODO: add backend call
     // TODO: add log display
@@ -20,6 +20,16 @@ const JobInfoCard = (props) => {
             title={`${toFirstCapital(job)} Jobs`}
             extra={<Button type="link"><Link to="../">Close</Link></Button>}
         >
+            <Descriptions 
+            >
+                {Object.entries(jobInfo).map(
+                    (o) => (
+                        <Descriptions.Item label={o[0]} >
+                            {o[1]}
+                        </Descriptions.Item>
+                    )
+                )}
+            </Descriptions>
 
         </Card>
     </div>;
