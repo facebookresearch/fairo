49a179883c3214ac4437e9a593acad1bbf3d8408
Chuxi Wang <wangchuxi94@gmail.com>
2022-07-06 14:13:01 -0700

    revert change to droidlet dashboard, fixed typo in readme

--- droidlet/dashboard/web/config/webpack.config.js
+++ droidlet/dashboard/web/config/webpack.config.js
@@ -53,10 +53,6 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function(webpackEnv) {
-  watchOptions: {
-    ignored: /node_modules/
-  }
-  
   const isEnvDevelopment = webpackEnv === 'development';
   const isEnvProduction = webpackEnv === 'production';
 
