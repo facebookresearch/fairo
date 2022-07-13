000272688a716983e36fc9d3983f584180691c4e
Chuxi Wang <wangchuxi94@gmail.com>
2022-07-13 16:19:41 -0700

    add backend api

---
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_model_utils.py
@@ -0,0 +1,22 @@
+import torch
+
+def load_model(model_fpath: str):
+    """
+    generates a model instance from the model file
+    """
+    return torch.load(model_fpath)
+
+def get_keys(model):
+    """
+    helper method to get all keys for the model
+    """
+    return model.keys()
+
+def get_value_by_key(model, key):
+    """
+    helper method for getting a value in the model dict
+    """
+    return model[key]
+
+
+
