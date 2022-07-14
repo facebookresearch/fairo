d645d399b32b447fe21a0aaff0477b5c627a94c3
Chuxi Wang <wangchuxi94@gmail.com>
2022-07-14 11:08:33 -0700

    add other attributes from the model, fixed server problem - send list instead of obj_key now

--- droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/asset/modelCard.js
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/asset/modelCard.js
@@ -7,7 +7,7 @@ The card showing Model infomation of a run.
 Usage:
 <ModelCard />
 */
-import { Card, Descriptions } from "antd";
+import { Button, Card, Descriptions, Divider, Tooltip } from "antd";
 import React, { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../../../../context/socket";
 
@@ -17,17 +17,24 @@ const ModelCard = (props) => {
     const batchId = props.batchId;
     const pipelineType = props.pipelineType;
     const [modelArgs, setModelArgs] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [modelKeys, setModelKeys] = useState(null);
+    const [loadingArgs, setLoadingArgs] = useState(true);
+    const [loadingKeys, setLoadingKeys] = useState(true);
 
     const socket = useContext(SocketContext);
 
     const handleReceivedModelArgs = (data) => {
-        setLoading(false);
-        console.log(typeof (JSON.parse(data)));
-        console.log(Object.keys(JSON.parse(data)))
+        setLoadingArgs(false);
 
         if (data !== 404) {
             setModelArgs(JSON.parse(data));
+        }
+    }
+
+    const handleRecievedModelKeys = (data) => {
+        setLoadingKeys(false);
+        if (data !== 404) {
+            setModelKeys(data);
         }
     }
 
@@ -35,34 +42,54 @@ const ModelCard = (props) => {
         socket.emit("get_model_value_by_id_n_key", batchId, "args");
     }
 
+    const getModelKeys = () => {
+        socket.emit("get_model_keys_by_id", batchId);
+    }
     useEffect(() => {
         socket.on("get_model_value_by_id_n_key", (data) => handleReceivedModelArgs(data));
+        socket.on("get_model_keys_by_id", (data) => handleRecievedModelKeys(data));
     }, [socket, handleReceivedModelArgs]);
 
     useEffect(() => {
-        getModelArgs();
+        loadingArgs && getModelArgs();
+        loadingKeys && getModelKeys();
     }, []); // component did mount
 
     const processModelArg = (arg) => {
-        if (typeof(arg) === "object") {
+        if (typeof (arg) === "object") {
             return Object.keys(arg).map((key) => (`${key}: ${arg[key]}`));
-        } else if (!arg || (typeof(arg) === "string" && arg.length === 0)){ 
+        } else if (!arg || (typeof (arg) === "string" && arg.length === 0)) {
             return "NA";
-        } else{
+        } else {
             return arg;
-        } 
+        }
     }
 
     return (
         <div style={{ width: '70%' }}>
-            <Card title="Model" loading={loading}>
+            <Card title="Model" loading={loadingKeys || loadingArgs}>
                 <Meta />
-                <Descriptions title="Model Args" bordered column={2}>
-                    {modelArgs && Object.keys(modelArgs).map((key) =>
-                        <Descriptions.Item label={key}>{processModelArg(modelArgs[key])}</Descriptions.Item>
-                    )}
-                </Descriptions>
-
+                <div style={{ textAlign: "left" }}>
+                    <Descriptions title="Model Args" bordered column={2}>
+                        {modelArgs && Object.keys(modelArgs).map((key) =>
+                            <Descriptions.Item label={key}>{processModelArg(modelArgs[key])}</Descriptions.Item>
+                        )}
+                    </Descriptions>
+                    <Divider />
+                    <Descriptions title="Other Attributes" bordered column={2}>
+                        {modelKeys && modelKeys.map((modelKey) =>
+                            modelKey !== "args"
+                            &&
+                            <Descriptions.Item>
+                                <Tooltip title={`View ${modelKey}`}>
+                                    <Button type="link" onClick={() => console.log(modelKey)}>
+                                        {modelKey}
+                                    </Button>
+                                </Tooltip>
+                            </Descriptions.Item>
+                        )}
+                    </Descriptions>
+                </div>
             </Card>
         </div>);
 }
