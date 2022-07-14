edf921f5c0572633f821e129a0e8bbf261d06eb6
Chuxi Wang <wangchuxi94@gmail.com>
2022-07-14 16:43:32 -0700

    Update thedocumentation

--- droidlet/tools/hitl/dashboard_app/backend/dashboard_model_utils.py
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_model_utils.py
@@ -1,3 +1,11 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+Utils for loading a model and preparing model infomation for transporting via socket.
+"""
+import argparse
+import collections
+import json
 import torch
 
 
@@ -20,14 +28,14 @@ def get_value_by_key(model, key):
     """
     helper method for getting a value in the model dict
     """
-
     value = model[key]
 
     # logic for preparing json
     if isinstance(value, argparse.Namespace) or isinstance(value, collections.OrderedDict):
-        # for args and state_dict
+        # for args and state_dict, need to dump the __dict__ field
         return json.dumps(value.__dict__)
     else:
+        # otherwise, dump the whole value object
         return json.dumps(value)
 
 
@@ -36,6 +44,7 @@ def get_complete_model(model):
     helper method to get the complete model
     """
     model_dict = {}
+    # get all fields
     for key in model.keys():
         model_dict[key] = get_value_by_key(model, key)
     return json.dumps(model_dict)
--- droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/asset/modelCard.js
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/pipeline/detail/asset/modelCard.js
@@ -2,10 +2,9 @@
 Copyright (c) Facebook, Inc. and its affiliates.
 
 The card showing Model infomation of a run. 
-**Note: this is just a placeholder for the model card. TODO: add real content of the model card**
 
 Usage:
-<ModelCard />
+<ModelCard batchId = {batchId} pipelineType={pipelineType}/>
 */
 import { Button, Card, Descriptions, Divider, Tooltip } from "antd";
 import React, { useContext, useEffect, useState } from "react";
@@ -25,6 +24,11 @@ const ModelCard = (props) => {
 
     const handleReceivedModelArgs = (data) => {
         setLoadingArgs(false);
+        // data other than args are rendered in the ModelAttributeDetailModal component
+        if (data !== 404 && data[0] === "args") {
+            setModelArgs(JSON.parse(data[1]));
+        }
+    });
 
         if (data !== 404) {
             setModelArgs(JSON.parse(data));
@@ -39,6 +43,7 @@ const ModelCard = (props) => {
     }
 
     const getModelArgs = () => {
+        // get args for the model
         socket.emit("get_model_value_by_id_n_key", batchId, "args");
     }
 
@@ -65,31 +70,54 @@ const ModelCard = (props) => {
         }
     }
 
+    const handleOnClickViewModelAttibute = (modelKey) => {
+        setCurrentModelKey(modelKey);
+        setModalOpen(true);
+    }
+
     return (
         <div style={{ width: '70%' }}>
             <Card title="Model" loading={loadingKeys || loadingArgs}>
                 <Meta />
-                <div style={{ textAlign: "left" }}>
-                    <Descriptions title="Model Args" bordered column={2}>
-                        {modelArgs && Object.keys(modelArgs).map((key) =>
-                            <Descriptions.Item label={key}>{processModelArg(modelArgs[key])}</Descriptions.Item>
-                        )}
-                    </Descriptions>
-                    <Divider />
-                    <Descriptions title="Other Attributes" bordered column={2}>
-                        {modelKeys && modelKeys.map((modelKey) =>
-                            modelKey !== "args"
-                            &&
-                            <Descriptions.Item>
-                                <Tooltip title={`View ${modelKey}`}>
-                                    <Button type="link" onClick={() => console.log(modelKey)}>
-                                        {modelKey}
-                                    </Button>
-                                </Tooltip>
-                            </Descriptions.Item>
-                        )}
-                    </Descriptions>
-                </div>
+                {
+                    !loadingKeys && !loadingArgs && (
+                        modelKeys ?
+                            <div style={{ textAlign: "left" }}>
+                                <Descriptions title="Model Args" bordered column={2}>
+                                    {modelArgs && Object.keys(modelArgs).map((key) =>
+                                        <Descriptions.Item label={key}>{processModelArg(modelArgs[key])}</Descriptions.Item>
+                                    )}
+                                </Descriptions>
+                                <Divider />
+                                <Descriptions title="Other Attributes" bordered column={2}>
+                                    {modelKeys && modelKeys.map((modelKey) =>
+                                        modelKey !== "args"
+                                        &&
+                                        <Descriptions.Item>
+                                            <Tooltip title={`View ${modelKey}`}>
+                                                <Button type="link" onClick={() => handleOnClickViewModelAttibute(modelKey)}>
+                                                    {modelKey}
+                                                </Button>
+                                            </Tooltip>
+                                        </Descriptions.Item>
+                                    )}
+                                </Descriptions>
+                            </div>
+                            :
+                            <div>NA</div>
+                    )
+                }
+                {/* modal showing a specific model attribute's field (anything other than args) */}
+                {currentModelKey
+                    &&
+                    <ModelAtrributeModal
+                        modelKey={currentModelKey}
+                        setModelKey={setCurrentModelKey}
+                        modalOpen={modalOpen}
+                        setModalOpen={setModalOpen}
+                        batchId={batchId} 
+                    />
+                }
             </Card>
         </div>);
 }
