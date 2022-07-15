582f5685439fdce6f8b19cdde9d0ea6e80050db1
lshamis <leonid.shamis@gmail.com>
2022-07-15 16:17:26 -0700

    MRP example for habitat (#1289)
    * MRP example for habitat
    
    * format
    
    * responding to comments

---
+++ mrp/examples/14_habitat/msetup.py
@@ -0,0 +1,28 @@
+import mrp
+
+mrp.process(
+    name="viewer",
+    runtime=mrp.Conda(
+        channels=["conda-forge", "aihabitat"],
+        dependencies=["habitat-sim"],
+        setup_commands=[
+            [
+                "python3",
+                "-m",
+                "habitat_sim.utils.datasets_download",
+                "--uids",
+                "replica_cad_dataset",
+                "--no-replace",
+            ],
+        ],
+        run_command=[
+            "habitat-viewer",
+            "--dataset",
+            "data/replica_cad/replicaCAD.scene_dataset_config.json",
+            "--",
+            "apt_1",
+        ],
+    ),
+)
+
+mrp.main()
