2d5be4034d7aaf0c7dc253131657c0dfd3c62574
Aqib M Mahfuz <aqib@devfair0830.h2.fair>
2022-07-18 21:29:37 -0700

    Extend map views, center to bot features, beautification of Memory2D.js

--- droidlet/dashboard/web/src/components/Memory2D/Memory2DMenu.js
+++ droidlet/dashboard/web/src/components/Memory2D/Memory2DMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
@@ -37,108 +38,151 @@ const groupingHelpText =
  *                            toggleDynamicPositioning: handler to toggle DP
  *                            showTriples: bool for if MemoryMapTable should show triples assoc with object
  *                            toggleShowTriples: handler to toggle showTriples
+ *                            mapView: the plane which the map is currently displaying
+ *                            toggleMapView: handler to change mapView in Memory2D
+ *                            centerToBot: handler that centers the stage to the bot
  */
 export default function Memory2DMenu(props) {
   const [groupName, setGroupName] = React.useState("");
   const [groupingCount, setGroupingCount] = React.useState(0);
+  const [mapView, setMapView] = React.useState(props.mapView);
 
   return (
     <ThemeProvider theme={menuTheme}>
       <Drawer anchor="right" open={props.showMenu} onClose={props.onMenuClose}>
         <div style={{ width: 450 }}>
           <Grid container direction="column" spacing={3}>
-            <Grid item>
+            <Grid item desc="close-menu">
               <IconButton onClick={props.onMenuClose}>
                 <CloseIcon />
               </IconButton>
               <Divider />
             </Grid>
-            <Grid item xs="auto">
-              <Card variant="outlined">
-                <CardContent>
-                  <Grid
-                    container
-                    justifyContent="space-between"
-                    alignItems="center"
-                    spacing={2}
-                  >
-                    <Grid item xs={6}>
-                      <TextField
-                        value={groupName}
-                        error={Object.keys(props.selected_objects).length <= 1}
-                        helperText={
-                          Object.keys(props.selected_objects).length <= 1
-                            ? "Select multiple objects"
-                            : Object.keys(props.selected_objects).length +
-                              " objects selected"
-                        }
-                        placeholder={
-                          "Group Name " +
-                          (groupingCount > 0 ? groupingCount + 1 : "")
-                        }
-                        onChange={(e) => {
-                          setGroupName(e.target.value);
-                        }}
-                      />
+            <Grid
+              item
+              desc="menu-items"
+              container
+              direction="column"
+              spacing={3}
+              style={{ marginLeft: 3 }}
+            >
+              <Grid item xs="auto" container>
+                <Card variant="outlined">
+                  <CardContent>
+                    <Grid item container alignItems="center" spacing={6}>
+                      <Grid item xs={6}>
+                        <TextField
+                          value={groupName}
+                          error={
+                            Object.keys(props.selected_objects).length <= 1
+                          }
+                          helperText={
+                            Object.keys(props.selected_objects).length <= 1
+                              ? "Select multiple objects"
+                              : Object.keys(props.selected_objects).length +
+                                " objects selected"
+                          }
+                          placeholder={
+                            "Group Name " +
+                            (groupingCount > 0 ? groupingCount + 1 : "")
+                          }
+                          onChange={(e) => {
+                            setGroupName(e.target.value);
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Button
+                          variant="contained"
+                          disabled={
+                            Object.keys(props.selected_objects).length <= 1 ||
+                            !groupName
+                          }
+                          onClick={() => {
+                            props.onGroupSubmit({
+                              selected_objects: props.selected_objects,
+                              group_name: groupName,
+                            });
+                            setGroupingCount((prev) => prev + 1);
+                            setGroupName("");
+                          }}
+                        >
+                          Group
+                        </Button>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Tooltip
+                          title={groupingHelpText}
+                          interactive
+                          leaveDelay={500}
+                        >
+                          <HelpIcon />
+                        </Tooltip>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                      <Button
-                        variant="contained"
-                        disabled={
-                          Object.keys(props.selected_objects).length <= 1 ||
-                          !groupName
-                        }
-                        onClick={() => {
-                          props.onGroupSubmit({
-                            selected_objects: props.selected_objects,
-                            group_name: groupName,
-                          });
-                          setGroupingCount((prev) => prev + 1);
-                          setGroupName("");
-                        }}
-                      >
-                        Group
-                      </Button>
-                    </Grid>
-                    <Grid item xs={3}>
-                      <Tooltip
-                        title={groupingHelpText}
-                        interactive
-                        leaveDelay={500}
-                      >
-                        <HelpIcon />
-                      </Tooltip>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item container direction="column" alignItems="flex-start">
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      size="small"
-                      checked={props.dynamicPositioning}
-                      onChange={props.toggleDynamicPositioning}
-                    />
-                  }
-                  label="Dynamic Positioning"
-                  labelPlacement="end"
-                />
+                  </CardContent>
+                </Card>
               </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      size="small"
-                      checked={props.showTriples}
-                      onChange={props.toggleShowTriples}
-                    />
-                  }
-                  label="Show triples"
-                  labelPlacement="end"
-                />
+              <Grid item container direction="column" alignItems="flex-start">
+                <Grid item>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        size="small"
+                        checked={props.dynamicPositioning}
+                        onChange={props.toggleDynamicPositioning}
+                      />
+                    }
+                    label="Dynamic Positioning"
+                    labelPlacement="end"
+                  />
+                </Grid>
+                <Grid item>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        size="small"
+                        checked={props.showTriples}
+                        onChange={props.toggleShowTriples}
+                      />
+                    }
+                    label="Show triples"
+                    labelPlacement="end"
+                  />
+                </Grid>
+                <Grid item>
+                  {"View:   "}
+                  <ButtonGroup variant="contained">
+                    <Button
+                      color={"ZX" === mapView ? "secondary" : "default"}
+                      onClick={() => {
+                        props.toggleMapView("ZX");
+                        setMapView("ZX");
+                      }}
+                    >
+                      ZX
+                    </Button>
+                    <Button
+                      color={"XY" === mapView ? "secondary" : "default"}
+                      onClick={() => {
+                        props.toggleMapView("XY");
+                        setMapView("XY");
+                      }}
+                    >
+                      XY
+                    </Button>
+                    <Button
+                      color={"YZ" === mapView ? "secondary" : "default"}
+                      onClick={() => {
+                        props.toggleMapView("YZ");
+                        setMapView("YZ");
+                      }}
+                    >
+                      YZ
+                    </Button>
+                  </ButtonGroup>
+                  <Button onClick={props.centerToBot}>CENTER TO BOT</Button>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
--- droidlet/dashboard/web/src/components/Memory2D/MemoryMapTable.js
+++ droidlet/dashboard/web/src/components/Memory2D/MemoryMapTable.js
@@ -247,6 +247,8 @@ export default function MemoryMapTable(props) {
                 <Tooltip
                   title="restores values to what they were before any manual edits"
                   placement="bottom"
+                  interactive
+                  leaveDelay={500}
                 >
                   <IconButton
                     onClick={() => {
@@ -280,6 +282,8 @@ export default function MemoryMapTable(props) {
                 <Tooltip
                   title="refreshes values to what they were when table was opened"
                   placement="bottom"
+                  interactive
+                  leaveDelay={500}
                 >
                   <IconButton
                     onClick={() => {
