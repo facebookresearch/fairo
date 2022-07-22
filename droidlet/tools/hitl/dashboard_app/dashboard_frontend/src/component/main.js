a0afc347e128862e16b117dc113073ac8e5db3fa
Chuxi Wang <wangchuxi94@gmail.com>
2022-07-21 18:37:49 -0700

    add frontend card

--- droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/main.js
+++ droidlet/tools/hitl/dashboard_app/dashboard_frontend/src/component/main.js
@@ -1,99 +1,49 @@
-import React, {useState, useContext, useEffect, useCallback} from 'react';
-import Button from '@mui/material/Button';
 /*
 Copyright (c) Facebook, Inc. and its affiliates.
 */
-import {SocketContext} from '../context/socket';
-import { List, ListSubheader, ListItem, ListItemText, Divider } from '@mui/material';
+import React from 'react';
+import { Layout, Typography, Card, Divider } from 'antd';
+import { SearchOutlined, ToolOutlined } from '@ant-design/icons';
+import { Content } from 'antd/lib/layout/layout';
 
 const Main = () => {
-    const socket = useContext(SocketContext);
-    
-    const [jobList, setJobList] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [jobInfo, setJobInfo] = useState('');
-    const [jobTraceback, setJobTraceback] = useState('');
+    return <div>
+        <Layout>
+            <Content style={{
+                padding: '24px 50px 50px 50px',
+            }}>
+                <Typography.Title>Welcome to Droidlet HITL Dashboard.</Typography.Title>
+                <Card style={{ height: '60vh', position: 'relative' }}>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                    </div>
+                    <Divider>
+                        <Typography.Title level={5}>
+                            <SearchOutlined style={{ paddingRight: '4px' }} />
+                            Quick View Run
+                        </Typography.Title>
+                    </Divider>
 
-    const handleReceivedJobList = useCallback((data) => {
-        setJobList(data);
-        setLoading(false);
-    }, []);
+                    <Divider>
+                        <Typography.Title level={5}>
+                            <ToolOutlined style={{ paddingRight: '4px' }} />
+                            Tools
+                        </Typography.Title>
+                    </Divider>
+                    <Card
+                        title="Checksum"
+                        type="inner"
+                    >
+                        Checksum 
 
-    const getJobList = () => {
-        socket.emit("get_job_list");
-        setLoading(true);
-    } 
+                    </Card>
+                </Card>
+            </Content>
+        </Layout>
+    </div>
 
-    const handleReciveTraceback = useCallback((data) => {
-        setJobTraceback(data);
-    }, []);
-
-    const getJobTraceback = () => {
-        // hardcoded id for checking backend api connection
-        socket.emit("get_traceback_by_id", 20220519194922);
-    }
-
-    const handleReciveInfo = useCallback((data) => {
-        setJobInfo(JSON.stringify(data));
-    }, []);
-
-    const getJobInfo = () => {
-        // hardcoded id for checking backend api connection
-        socket.emit("get_run_info_by_id", 20211209154235);
-    }
-
-    useEffect(() => {
-        socket.on("get_job_list", (data) => handleReceivedJobList(data));
-        socket.on("get_traceback_by_id", (data) => handleReciveTraceback(data));
-        socket.on("get_run_info_by_id", (data) => handleReciveInfo(data));
-    }, [socket, handleReceivedJobList, handleReciveTraceback, handleReciveInfo]);
-
-    return (
-        <div>
-            <Button variant="contained" onClick={() => {console.log(socket)}}>console log</Button>
-            <div style={{"display": "flex"}}>
-                <div style={{"width" : "400px"}}>
-                    <h1>
-                    Test list Jobs
-                    </h1>
-                    <Button variant="contained" onClick={getJobList}>List Jobs</Button>
-                    <List       
-                        sx={{ width: '100%', maxWidth: 360, border: 'solid gray'}}
-                        subheader={
-                            <ListSubheader>
-                                Job List
-                            </ListSubheader>
-                    }>  
-                        {
-                            loading && <div>loading</div>
-                        }
-                        {
-                            jobList.length !== 0 
-                            && jobList.map(job => 
-                                (<li>
-                                    <Divider light />
-                                    <ListItem>
-                                        <ListItemText>{job}</ListItemText>
-                                    </ListItem>
-                                </li>
-                            ))
-                        }
-                    </List>
-                </div>
-                <div  style={{"width" : "400px"}}>
-                    <h1>Test get log</h1>
-                    <Button variant="contained" onClick={getJobTraceback}>get log</Button>
-                    <div>{jobTraceback.length !== 0 && jobTraceback}</div>
-                </div>
-                <div  style={{"width" : "400px"}}>
-                    <h1>test get info</h1>
-                    <Button variant="contained" onClick={getJobInfo}>get info</Button>
-                    <div>{jobInfo.length !== 0 && jobInfo}</div>
-                </div>   
-            </div>
-        </div>
-        );
-    
 }
 
 export default Main;
