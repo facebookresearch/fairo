6a06f36082eae4a9f6004bf7653e4d6d6c585f34
Chuxi Wang <51009396+mialsy@users.noreply.github.com>
2022-07-29 17:52:20 -0700

    Hitl dashboard model viz rebased (#1305)
    * add dashboard server backbone
    
    * Automatic style fix for droidlet
    
    * ad impl for server to get log and get job info
    
    * add test for aws helper, fix problem for setting json
    
    * Automatic style fix for droidlet
    
    * add documentation
    
    * Automatic style fix for droidlet
    
    * Automatic style fix for droidlet
    
    * add backbone for pipeline metainfo and detail
    
    * added list view skeleton, TODO: backend interaction
    
    * add a status column
    
    * updated panel to re-usable
    
    * add date columns
    
    * update naming, detail page, and the documentation
    
    * removed redundant file created at create react app
    
    * HiTL dashboard app - added search & filter on run list (#1270)
    
    * add serch comp
    
    * add search and filter
    
    * clean up
    
    * updated readme
    
    * Update README.MD
    
    * update documentation
    
    * updated parameter name to be more descriptive
    
    * HiTl Dashboard - Detail Page for a Run (#1271)
    
    * added a basic layout for run detail page
    
    * add more navigation
    
    * add info display
    
    * add more impl for get job detail
    
    * fixed bugs
    
    * Automatic style fix for droidlet
    
    * add error handling
    
    * added a not found page
    
    * add ui touch up
    
    * rebase
    
    * rebase
    
    * fixed getting session list when there is already sesssion list
    
    * Hitl dashboard session detail (#1272)
    
    * rebase readme
    
    * rebase
    
    * re arrange update note in readme
    
    * Automatic style fix for droidlet
    
    * Update README.MD
    
    * add styling to the dashboard session log
    
    * fixed name issue
    
    * Update README.MD
    
    Co-authored-by: bot <bot@fb.com>
    
    Co-authored-by: bot <bot@fb.com>
    
    * rebase
    
    * rebase
    
    * add more impl for get job detail
    
    * Automatic style fix for droidlet
    
    * add ui touch up
    
    * rebase
    
    * reabse
    
    * add dataset viz impl
    
    * add documentation for newly added files
    
    * Automatic style fix for droidlet
    
    * update display for when meta.txt cannot be found
    
    * fixed state npe
    
    * updated readme
    
    * update readme
    
    * fixed typo
    
    * fixed readme typo
    
    * add backend api
    
    * manully converrt to json
    
    * add component to show model args
    
    * Automatic style fix for droidlet
    
    * add other attributes from the model, fixed server problem - send list instead of obj_key now
    
    * Automatic style fix for droidlet
    
    * add more navigation
    
    * add info display
    
    * add more impl for get job detail
    
    * Automatic style fix for droidlet
    
    * add error handling
    
    * add ui touch up
    
    * Update README.MD
    
    * Update README.MD
    
    * add dataset viz impl
    
    * Automatic style fix for droidlet
    
    * update display for when meta.txt cannot be found
    
    * updated readme
    
    * Added demo figure
    
    * fixed typo
    
    * add backend api
    
    * manully converrt to json
    
    * Automatic style fix for droidlet
    
    * add other attributes from the model, fixed server problem - send list instead of obj_key now
    
    * Automatic style fix for droidlet
    
    * Automatic style fix for droidlet
    
    * updat homepage to show a welcome message
    
    * Update thedocumentation
    
    * update document
    
    * Update README.MD
    
    * revert files in droidlet dashboard
    
    * fixed issue due to merge
    
    * Automatic style fix for droidlet
    
    * fixed test
    
    Co-authored-by: bot <bot@fb.com>

--- droidlet/tools/hitl/dashboard_app/backend/dashboard_aws_helper.py
+++ droidlet/tools/hitl/dashboard_app/backend/dashboard_aws_helper.py
@@ -13,6 +13,13 @@ import botocore
 import os
 import re
 
+from droidlet.tools.hitl.dashboard_app.backend.dashboard_model_utils import load_model
+
+
+PIPELINE_DATASET_MAPPING = {
+    "NLU": "nsp_data",
+}
+
 S3_BUCKET_NAME = "droidlet-hitl"
 S3_ROOT = "s3://droidlet-hitl"
 HITL_TMP_DIR = (
@@ -34,32 +41,6 @@ def _download_file(fname: str):
     """
     download file from s3 if it does not exists in local tmp storage
     """
-    # check if exists on local tmp directory
-    local_file_name = os.path.join(HITL_TMP_DIR, fname)
-
-    if not os.path.exists(local_file_name):
-        # reterive from s3
-        local_folder_name = local_file_name[: local_file_name.rindex("/")]
-        os.makedirs(local_folder_name, exist_ok=True)
-        try:
-            s3.meta.client.download_file(S3_BUCKET_NAME, fname, local_file_name)
-        except botocore.exceptions.ClientError as e:
-            print(f"file not exists {fname}")
-
-    return local_file_name if os.path.exists(local_file_name) else None
-
-
-def _read_file(fname: str):
-    """
-    read file into a string
-    """
-    f = open(fname, "r")
-    content = f.read()
-    f.close()
-    return content
-
-
-def _dowload_file(fname: str):
     # check if exists on local tmp directory
     local_file_name = os.path.join(HITL_TMP_DIR, fname)
 
@@ -113,16 +94,6 @@ def get_traceback_by_id(batch_id: int):
     return _read_file(local_fname), None
 
 
-def get_traceback_by_id(batch_id: int):
-    """
-    helper method for preparing get_traceback_by_id api's response
-    """
-    local_fname = _download_file(f"{batch_id}/log_traceback.csv")
-    if local_fname is None:
-        return f"cannot find traceback with id {batch_id}"
-    return _read_file(local_fname)
-
-
 def get_run_info_by_id(batch_id: int):
     """
     helper method for preparing get_run_info_by_id api's response
@@ -166,3 +137,54 @@ def get_interaction_session_log_by_id(batch_id: int, session_id: str):
     log_file = open(log_fname, "r")
     log = log_file.readlines()
     return log, None
+
+
+def get_dataset_indices_by_id(batch_id: int):
+    """
+    For a run specied by the batch id,
+    get the newly added dataset data points' start index and end index from meta.txt
+    """
+    local_fname = _download_file(f"{batch_id}/meta.txt")
+    if local_fname is None:
+        return f"cannot find meta.txt with id {batch_id}", 404
+    meta_txt_content = _read_file(local_fname)
+    meta_txt_splitted = meta_txt_content.split("\n")
+    return [int(meta_txt_splitted[0]), int(meta_txt_splitted[-1])], None
+
+
+def get_dataset_version_list_by_pipeline(pipeline: str):
+    """
+    Get dataset's version list
+    """
+    # get dataset name prefix and search pattern
+    dataset_prefix = PIPELINE_DATASET_MAPPING[pipeline]
+    pattern_str = f"{dataset_prefix}_v[0-9]" + "{1,}.txt"
+    pattern = re.compile(pattern_str)
+
+    dataset_list = []
+    # list object from s3 bucket
+    for obj in s3.Bucket(S3_BUCKET_NAME).objects.all():
+        if re.match(pattern, obj.key):
+            dataset_list.append(obj.key)
+    return dataset_list
+
+
+def get_dataset_by_name(dataset_name: str):
+    """
+    Reterive a specified dataset's content
+    """
+    local_fname = _download_file(dataset_name)
+    if local_fname is None:
+        return f"cannot find {dataset_name}", 404
+    return _read_file(local_fname), None
+
+
+def get_model_by_id(batch_id: int):
+    """
+    Download best model from aws, return the model if the model file exists
+    """
+    local_fname = _download_file(f"{batch_id}/best_model/best_model.pth")
+    if local_fname is None:
+        return f"cannot find best_model file related to {batch_id}", 404
+    else:
+        return load_model(local_fname), None
