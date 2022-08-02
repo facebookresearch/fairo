94bafb0061e9da2379d93d83b5d6672e11c020bc
ExhAustin <aswang@andrew.cmu.edu>
2022-08-02 16:20:14 -0500

    Remove unused method

--- polymetis/polymetis/python/scripts/benchmark_ee_tracking.py
+++ polymetis/polymetis/python/scripts/benchmark_ee_tracking.py
@@ -45,37 +45,6 @@ def generate_trajectory(center_pose, radius, hz, num_loops, time_to_go):
     return ee_pose_traj
 
 
-def run_offline_policy_from_traj(robot, traj):
-    """Executes offline policy that executes trajectories in a similar fashion to robot.move_to_ee_pose"""
-    num_steps = traj.shape[0]
-
-    ee_pose_traj = []
-    for i in range(num_steps):
-        ee_pose = T.from_rot_xyz(
-            rotation=R.from_quat(traj[i, 3:]),
-            translation=traj[i, :3],
-        )
-        ee_pose_traj.append(ee_pose)
-
-    ee_twist_traj = []
-    for i in range(num_steps):
-        pose0 = ee_pose_traj[max(i - 1, 0)]
-        pose1 = ee_pose_traj[min(i + 1, num_steps - 1)]
-        ee_twist = (pose1 * pose0.inv()).as_twist() * robot.metadata.hz
-        ee_twist_traj.append(ee_twist)
-
-    policy = toco.policies.EndEffectorTrajectoryExecutor(
-        ee_pose_trajectory=ee_pose_traj,
-        ee_twist_trajectory=ee_twist_traj,
-        Kp=robot.Kx_default,
-        Kd=robot.Kxd_default,
-        robot_model=robot.robot_model,
-        ignore_gravity=True,
-    )
-
-    return robot.send_torch_policy(policy)
-
-
 def compare_traj(ref_traj, states_reached, robot_model, experiment_name=""):
     """Compute and print error between state log and the reference trajectory"""
     # Compute error
