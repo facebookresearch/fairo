cb363d342ffdee73c2eeb8252adee7c295f49c06
theophilee <theophile.gervet@gmail.com>
2022-08-22 23:41:13 +0900

    dev

--- agents/locobot/teleop.py
+++ agents/locobot/teleop.py
@@ -296,6 +296,8 @@ if __name__ == "__main__":
 
         # this goes from 21ms to 120ms
         rgb_depth = mover.get_rgb_depth()
+        print("rgb_depth.depth.min()", rgb_depth.depth.min())
+        print("rgb_depth.depth.max()", rgb_depth.depth.max())
 
         points, colors = rgb_depth.ptcloud.reshape(-1, 3), rgb_depth.rgb.reshape(-1, 3)
         colors = colors / 255.
