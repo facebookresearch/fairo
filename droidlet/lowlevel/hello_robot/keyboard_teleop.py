d4efc3ba74d4ecf371e6e3607df7c169ca936107
ExhAustin <aswang@andrew.cmu.edu>
2022-08-26 14:17:54 -0700

    Fix teleop script

--- droidlet/lowlevel/hello_robot/keyboard_teleop.py
+++ droidlet/lowlevel/hello_robot/keyboard_teleop.py
@@ -1,6 +1,7 @@
+import sys
 import time
 
-from pynput import keyboard 
+from pynput import keyboard
 import numpy as np
 
 from droidlet.lowlevel.hello_robot.hello_robot_mover import HelloRobotMover
@@ -12,23 +13,26 @@ RIGHT = keyboard.Key.right
 
 
 class RobotController:
-    def __init__(self,
+    def __init__(
+        self,
         ip,
         hz=10,
         acc=1.2,
-        racc=2.,
+        racc=2.0,
         vel_max=0.6,
         rvel_max=0.5,
     ):
         # Params
-        self.dt = 1 / hz.
+        self.dt = 1.0 / hz
         self.acc = acc
         self.racc = racc
         self.vel_max = vel_max
         self.rvel_max = rvel_max
 
         # Robot
+        print("Connecting to robot...")
         self.robot = HelloRobotMover(ip=ip)
+        print("Connected.")
 
         # Keyboard
         self.key_states = {key: 0 for key in [UP, DOWN, LEFT, RIGHT]}
@@ -38,12 +42,13 @@ class RobotController:
         self.rvel = 0
 
     def on_press(self, key):
-        self.key_states[key] = 1 
+        self.key_states[key] = 1
 
     def on_release(self, key):
         self.key_states[key] = 0
 
     def run(self):
+        print("Teleoperation started.")
         while True:
             # Map keystrokes
             vert_val = self.key_states[UP] - self.key_states[DOWN]
@@ -63,13 +68,13 @@ class RobotController:
             time.sleep(self.dt)
 
 
-if __name__ == '__main__':
-    robot_controller = RobotController()
+if __name__ == "__main__":
+    robot_controller = RobotController(sys.argv[1])
 
     listener = keyboard.Listener(
         on_press=robot_controller.on_press,
         on_release=robot_controller.on_release,
     )
     listener.start()
-    
+
     robot_controller.run()
