2f15f6bc6ba2dcea687ab4dafc890551869aeb9d
theophilee <theophile.gervet@gmail.com>
2022-09-19 15:57:43 -0400

    dev

--- droidlet/lowlevel/locobot/remote/slam_service.py
+++ droidlet/lowlevel/locobot/remote/slam_service.py
@@ -249,7 +249,7 @@ with Pyro4.Daemon(ip) as daemon:
         max_z = robot_height + 5  # cm
         obj = SLAM(
             robot,
-            obstacle_threshold=5,  # 300 on robot
+            obstacle_threshold=300,  # 300 on robot, 5 in sim
             agent_min_z=min_z,
             agent_max_z=max_z,
         )
