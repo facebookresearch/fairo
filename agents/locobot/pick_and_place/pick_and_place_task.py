1c2cc3d5081e49e7ecdca00d1b48ecdb70ff2ffb
theophilee <theophile.gervet@gmail.com>
2022-10-04 15:12:21 -0400

    dev

--- agents/locobot/pick_and_place/pick_and_place_task.py
+++ agents/locobot/pick_and_place/pick_and_place_task.py
@@ -142,7 +142,14 @@ class PickAndPlaceTask:
         # This should correct for the length of the Stretch gripper and the gripper upon which
         # Graspnet was trained
         grasp_offset = np.eye(4)
-        grasp_offset[2, 3] = (-1 * STRETCH_STANDOFF_DISTANCE) + 0.12
+
+        # Magic number 1
+        # grasp_offset[2, 3] = (-1 * STRETCH_STANDOFF_DISTANCE) + 0.12
+
+        # Magic number 2
+        grasp_offset[2, 3] = (-1 * STRETCH_STANDOFF_DISTANCE) + 0.22
+        grasp_offset[1, 3] = (-1 * STRETCH_STANDOFF_DISTANCE) + 0.10
+
         for i, grasp in enumerate(grasps):
            grasps[i] = grasp @ grasp_offset
 
