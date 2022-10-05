ddf1e9e65602f773142ed6842d0575030c098857
theophilee <theophile.gervet@gmail.com>
2022-10-05 17:15:59 -0400

    dev

--- agents/locobot/pick_and_place/scratch.py
+++ agents/locobot/pick_and_place/scratch.py
@@ -1,8 +1,21 @@
 import numpy as np
-
+from utils import get_pcd_in_cam
+from home_robot.ros.camera import RosCamera
 
 depth = np.load("depth.npy")
 rgb = np.load("rgb.npy")
+intrinsic_mat = np.array([
+    [603.00891113, 0., 323.16085815],
+    [0., 601.89398193, 245.71160889],
+    [0., 0., 1.]
+])
 
 print(depth.shape, depth.min(), depth.max())
 print(rgb.shape, rgb.min(), rgb.max())
+
+pcd1 = get_pcd_in_cam(depth, intrinsic_mat)
+print(pcd1.shape, pcd1.min(), pcd1.max())
+
+dpt_cam = RosCamera('/camera/aligned_depth_to_color', buffer_size=1)
+pcd2 = dpt_cam.depth_to_xyz(dpt_cam.fix_depth(depth))
+print(pcd2.shape, pcd2.min(), pcd2.max())
