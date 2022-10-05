afc6ee2db489f6a0a8e92dfbb4fbc365ee0a2aeb
theophilee <theophile.gervet@gmail.com>
2022-10-05 14:39:17 -0400

    dev

--- droidlet/lowlevel/hello_robot/remote/remote_hello_realsense.py
+++ droidlet/lowlevel/hello_robot/remote/remote_hello_realsense.py
@@ -216,8 +216,7 @@ class RemoteHelloRealsense(object):
         return color_image, depth_image
 
     def get_pcd_from_depth(self, depth):
-        if depth.shape[0] == CW:
-            depth = np.rot90(depth, k=1, axes=(0, 1))
+        depth = np.rot90(depth, k=1, axes=(0, 1))
         return self.dpt_cam.depth_to_xyz(self.dpt_cam.fix_depth(depth))
 
     def get_rgb_depth_optimized_for_habitat_transfer(self, rotate=True, compressed=False):
