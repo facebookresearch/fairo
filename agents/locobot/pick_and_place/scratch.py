4985446cc78ea445465adec094de7b81cde68b71
theophilee <theophile.gervet@gmail.com>
2022-10-06 10:41:09 -0400

    dev

--- agents/locobot/pick_and_place/scratch.py
+++ agents/locobot/pick_and_place/scratch.py
@@ -16,9 +16,11 @@ print(depth.shape, depth.min(), depth.max())
 print(rgb.shape, rgb.min(), rgb.max())
 
 pcd1 = get_pcd_in_cam(depth, intrinsic_mat)
+print(pcd1.shape)
 show_point_cloud(pcd1, rgb, orig=np.zeros(3))
 
 rospy.init_node('debug')
 dpt_cam = RosCamera('/camera/aligned_depth_to_color', buffer_size=1)
 pcd2 = np.rot90(dpt_cam.depth_to_xyz(np.rot90(depth, k=1, axes=(0, 1))), k=1, axes=(1, 0))
+print(pcd2.shape)
 show_point_cloud(pcd2, rgb, orig=np.zeros(3))
