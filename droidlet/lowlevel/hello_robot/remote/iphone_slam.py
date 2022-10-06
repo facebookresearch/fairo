7e9ac657b9eab563a2c94ba1aa8d3ea0ecbad678
ExhAustin <aswang@andrew.cmu.edu>
2022-10-06 16:36:25 -0700

    Debug node

--- droidlet/lowlevel/hello_robot/remote/iphone_slam.py
+++ droidlet/lowlevel/hello_robot/remote/iphone_slam.py
@@ -30,32 +30,42 @@ class Record3dSLAM:
         self.T_b2i[:3, :3] = R.from_rotvec([np.pi / 2.0, 0.0, 0.0]).as_matrix()
         self.T_i2b = np.linalg.pinv(self.T_b2i)
 
+        # Init ros node
+        rospy.init_node("iphone_slam")
+
     def _on_new_frame(self, frame):
         # Extract camera pose
         pose_i = frame.pose_mat
 
         # Compute & update base pose
         pose_b = self.T_b2i @ pose_i @ self.T_i2b
+        pose_pos = pose_b[:3, 3]
+        pose_ori = R.from_matrix(pose_b[:3, :3])
+        pose_quat = pose_ori.as_quat()
 
         with self.pose_lock:
-            self.base_pose[:2] = pose_b[:2, 3]
-            self.base_pose[2] = R.from_matrix(pose_b[:3, :3]).as_rotvec()[2]
+            self.base_pose[:2] = pose_pos[:2]
+            self.base_pose[2] = pose_ori.as_rotvec()[2]
 
         # Publish to rostopic
         camera_pose_ros = Pose()
-        camera_pose_ros.linear.x = frame.pose_pos[0]
-        camera_pose_ros.linear.y = frame.pose_pos[1]
-        camera_pose_ros.linear.z = frame.pose_pos[2]
-        camera_pose_ros.angular.x = frame.pose_quat[0]
-        camera_pose_ros.angular.y = frame.pose_quat[1]
-        camera_pose_ros.angular.z = frame.pose_quat[2]
-        camera_pose_ros.angular.w = frame.pose_quat[3]
+        camera_pose_ros.position.x = frame.pose_pos[0]
+        camera_pose_ros.position.y = frame.pose_pos[1]
+        camera_pose_ros.position.z = frame.pose_pos[2]
+        camera_pose_ros.orientation.x = frame.pose_quat[0]
+        camera_pose_ros.orientation.y = frame.pose_quat[1]
+        camera_pose_ros.orientation.z = frame.pose_quat[2]
+        camera_pose_ros.orientation.w = frame.pose_quat[3]
         self.c_pose_pub.publish(camera_pose_ros)
 
         base_pose_ros = Pose()
-        base_pose_ros.linear.x = self.base_pose[0]
-        base_pose_ros.linear.y = self.base_pose[1]
-        base_pose_ros.angular.z = self.base_pose[2]
+        base_pose_ros.position.x = self.base_pose[0]
+        base_pose_ros.position.y = self.base_pose[1]
+        base_pose_ros.position.z = 0.0
+        base_pose_ros.orientation.x = pose_quat[0]
+        base_pose_ros.orientation.y = pose_quat[1]
+        base_pose_ros.orientation.z = pose_quat[2]
+        base_pose_ros.orientation.w = pose_quat[3]
         self.b_pose_pub.publish(base_pose_ros)
 
     def get_base_pose(self):
@@ -65,6 +75,7 @@ class Record3dSLAM:
 
     def start(self):
         self.reader.start(frame_callback=self._on_new_frame)
+        rospy.spin()
 
 
 if __name__ == "__main__":
