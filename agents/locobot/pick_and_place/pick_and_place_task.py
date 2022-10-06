3fa9244996959c5e8f691e9c43513bd4af2d6de7
theophilee <theophile.gervet@gmail.com>
2022-10-06 11:04:18 -0400

    dev

--- agents/locobot/pick_and_place/pick_and_place_task.py
+++ agents/locobot/pick_and_place/pick_and_place_task.py
@@ -363,17 +363,18 @@ class PickAndPlaceTask:
                 print("Too few predicted grasps; trying to segment again...")
                 continue
 
-            if debug:
-                rotated_grasps = [self.R_stretch_camera.T @ grasp for grasp in predicted_grasps]
-                show_point_cloud(flat_pcd, image_rgb, orig=np.zeros(3), grasps=rotated_grasps)
+            # if debug:
+            #     rotated_grasps = [self.R_stretch_camera.T @ grasp for grasp in predicted_grasps]
+            #     show_point_cloud(flat_pcd, image_rgb, orig=np.zeros(3), grasps=rotated_grasps)
 
             world_grasps = [camera_pose @ grasp for grasp in predicted_grasps]
+            # Was needed with flat_pcd = get_pcd_in_cam(depth, self.intrinsic_mat):
             # world_pcd = trimesh.transform_points(flat_pcd, self.R_stretch_camera)
             world_pcd = flat_pcd
             world_pcd = trimesh.transform_points(world_pcd, camera_pose)
 
-            if debug:
-                show_point_cloud(world_pcd, image_rgb, orig=np.zeros(3), grasps=world_grasps)
+            # if debug:
+            #     show_point_cloud(world_pcd, image_rgb, orig=np.zeros(3), grasps=world_grasps)
 
             #self.manip.goto_static_grasp(world_grasps, scores, pause=True)
             self.goto_static_grasp(world_grasps, scores, world_pcd, image_rgb, pause=False, debug=debug)
