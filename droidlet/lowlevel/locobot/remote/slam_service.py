634e01002e547c3fcf97cd8e274815e3e583749e
theophilee <theophile.gervet@gmail.com>
2022-10-06 20:24:45 -0400

    dev

--- droidlet/lowlevel/locobot/remote/slam_service.py
+++ droidlet/lowlevel/locobot/remote/slam_service.py
@@ -141,11 +141,6 @@ class SLAM(object):
             t1 = time.time()
             print("get_semantics()", t1 - t0)
 
-            print("depth.shape", depth.shape)
-            print("depth.min(), depth.max()", depth.min(), depth.max())
-            print("pcd.shape", pcd.shape)
-            print("semantics.shape", semantics.shape)
-
             self.map_builder.update_semantic_map(pcd, semantics, pose)
             t2 = time.time()
             print("update_semantic_map()", t2 - t1)
