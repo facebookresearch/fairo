99e2e152d9b6a5aff8ba3464f5077a2dbec67e48
ExhAustin <aswang@andrew.cmu.edu>
2022-12-01 18:12:41 -0600

    Add docstring

--- polymetis/polymetis/python/polymetis/gripper_interface.py
+++ polymetis/polymetis/python/polymetis/gripper_interface.py
@@ -94,10 +94,22 @@ class GripperInterface:
         epsilon_outer: float = -1.0,
         blocking: bool = True,
     ):
-        """Commands the gripper to a certain width
+        """Commands the gripper to a close
+
+        For Robotiq grippers, this is equivalent to calling `goto` with grasp_width (0 by default).
+
+        For the Franka Hand, see documentation for franka::Gripper::move in libfranka.
+        Basically the gripper will perform the grasp if grasp_width - epsilon_inner < final_width < grasp_width + epsilon_outer.
+        Else the gripper will not continue to exert force.
+        The default width and epsilon values ensures that the gripper closes to the minimum width possible and continues to exert force.
+
+
         Args:
             vel: Velocity of the movement
             force: Maximum force the gripper will exert
+            grasp_width: Target width of the grasp
+            epsilon_inner: Maximum tolerated deviation when the actual grasped width is smaller than the commanded grasp width
+            epsilon_outer: Maximum tolerated deviation when the actual grasped width is larger than the commanded grasp width
         """
         cmd = polymetis_pb2.GripperCommand(
             width=grasp_width,
